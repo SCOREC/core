539cc659ecaef7322bc6580bfc4b19b934e2585a
Dan Ibanez <ibaned@rpi.edu>
2014-05-16 14:10:15 -0400

    adding meshmodel header

---
+++ gmi/gmi_mesh.h
@@ -0,0 +1,16 @@
+/****************************************************************************** 
+
+  (c) 2011-2014 Scientific Computation Research Center, 
+      Rensselaer Polytechnic Institute. All rights reserved.
+  
+  This work is open source software, licensed under the terms of the
+  BSD license as described in the LICENSE file in the top-level directory.
+
+*******************************************************************************/
+#ifndef GMI_MESH_H
+#define GMI_MESH_H
+
+void gmi_register_mesh(void);
+
+#endif
+
