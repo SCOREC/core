ba46f260f0ef832b2fd976540b1fd39f42f9cf0f
Dan Ibanez <ibaned@rpi.edu>
2014-05-16 13:50:06 -0400

    license headers in gmi

--- gmi/gmi.h
+++ gmi/gmi.h
@@ -1,3 +1,12 @@
+/****************************************************************************** 
+
+  (c) 2011-2014 Scientific Computation Research Center, 
+      Rensselaer Polytechnic Institute. All rights reserved.
+  
+  This work is open source software, licensed under the terms of the
+  BSD license as described in the LICENSE file in the top-level directory.
+
+*******************************************************************************/
 #ifndef GMI_H
 #define GMI_H
 
