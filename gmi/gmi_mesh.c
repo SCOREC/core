5f9efa5571fb5a2fb4d3b9747698fe8ba4cbeb77
Dan Ibanez <dan.a.ibanez@gmail.com>
2014-05-17 13:45:55 -0400

    added missing banner to gmi_mesh.c

--- gmi/gmi_mesh.c
+++ gmi/gmi_mesh.c
@@ -1,3 +1,12 @@
+/****************************************************************************** 
+
+  Copyright 2014 Scientific Computation Research Center, 
+      Rensselaer Polytechnic Institute. All rights reserved.
+  
+  This work is open source software, licensed under the terms of the
+  BSD license as described in the LICENSE file in the top-level directory.
+
+*******************************************************************************/
 #include "gmi_mesh.h"
 #include <stdlib.h>
 #include <stdio.h>
