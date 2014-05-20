122f9788303bf1d2876fcaf679a1799eee96900f
Dan Ibanez <ibaned@rpi.edu>
2014-05-20 09:59:22 -0400

    adding separate verify and render exes

---
+++ test/verify.cc
@@ -0,0 +1,22 @@
+#include <gmi_mesh.h>
+#include <apfMDS.h>
+#include <apfMesh2.h>
+#include <apf.h>
+#include <PCU.h>
+
+int main(int argc, char** argv)
+{
+  assert(argc==3);
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  gmi_register_mesh();
+  apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
+  m->verify();
+  m->destroyNative();
+  apf::destroyMesh(m);
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
+
+
+
