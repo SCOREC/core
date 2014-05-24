f6c5987efc73d1ef2d9188a985f3fa5087f60ea4
Dan Ibanez <dan.a.ibanez@gmail.com>
2014-05-24 12:09:15 -0400

    config.sh uses mpi wrappers

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 cmake .. \
-  -DCMAKE_C_COMPILER=$CC \
-  -DCMAKE_CXX_COMPILER=$CXX \
+  -DCMAKE_C_COMPILER=mpicc \
+  -DCMAKE_CXX_COMPILER=mpicxx \
   -DCMAKE_C_FLAGS="-Wall -g -O2" \
   -DCMAKE_CXX_FLAGS="-Wall -g -O2" \
   -DENABLE_THREADS=ON \
