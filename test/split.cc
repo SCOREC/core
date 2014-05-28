4e956bfa4c8d977cf42118384a9fcaf9e51353bf
Dan Ibanez <ibaned@rpi.edu>
2014-05-28 17:31:36 -0400

    removed timers and verify from split, timers moved

--- test/split.cc
+++ test/split.cc
@@ -12,16 +12,6 @@ const char* meshFile = 0;
 const char* outFile = 0;
 int partitionFactor = 1;
 
-apf::Mesh2* readMesh()
-{
-  double t0 = MPI_Wtime();
-  apf::Mesh2* m = apf::loadMdsMesh(modelFile,meshFile);
-  double t1 = MPI_Wtime();
-  if ( ! PCU_Comm_Self())
-    printf("time to load %s and %s: %f seconds\n",modelFile,meshFile,t1-t0);
-  return m;
-}
-
 void freeMesh(apf::Mesh* m)
 {
   m->destroyNative();
@@ -31,25 +21,16 @@ void freeMesh(apf::Mesh* m)
 apf::Migration* getPlan(apf::Mesh* m)
 {
   apf::Splitter* splitter = Parma_MakeRibSplitter(m);
-  double t0 = MPI_Wtime();
   apf::MeshTag* weights = Parma_WeighByMemory(m);
   apf::Migration* plan = splitter->split(weights, 1.10, partitionFactor);
   m->destroyTag(weights);
-  double t1 = MPI_Wtime();
   delete splitter;
-  if ( ! PCU_Comm_Self())
-    printf("time to run RIB: %f seconds\n",t1-t0);
   return plan;
 }
 
 void runAfter(apf::Mesh2* m)
 {
-  m->verify();
-  double t2 = MPI_Wtime();
   m->writeNative(outFile);
-  double t3 = MPI_Wtime();
-  if ( ! PCU_Comm_Self())
-    printf("time to write %s: %f seconds\n",outFile,t3-t2);
   freeMesh(m);
 }
 
@@ -73,7 +54,7 @@ int main(int argc, char** argv)
   gmi_register_mesh();
   PCU_Protect();
   getConfig(argc,argv);
-  apf::Mesh2* m = readMesh();
+  apf::Mesh2* m = apf::loadMdsMesh(modelFile,meshFile);
   splitMdsMesh(m, getPlan(m), partitionFactor, runAfter);
   PCU_Comm_Free();
   MPI_Finalize();
