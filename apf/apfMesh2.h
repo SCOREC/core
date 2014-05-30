c670c4c9d23211e3de06bec8e8af537ecf011ca4
Dan Ibanez <ibaned@rpi.edu>
2014-05-30 10:39:39 -0400

    some 80-char line enforcement

--- apf/apfMesh2.h
+++ apf/apfMesh2.h
@@ -25,10 +25,12 @@ class Mesh2 : public Mesh
     virtual bool canSnap() = 0;
     virtual void setParam(MeshEntity* e, Vector3 const& p) = 0;
     virtual void getParamOn(ModelEntity* g, MeshEntity* e, Vector3& p) = 0;
-    virtual bool getPeriodicRange(ModelEntity* g, int axis, double range[2]) = 0;
+    virtual bool getPeriodicRange(ModelEntity* g, int axis,
+        double range[2]) = 0;
     void setPoint(MeshEntity* e, int node, Vector3 const& p);
     virtual void setPoint_(MeshEntity* e, int node, Vector3 const& p) = 0;
-    MeshEntity* createVertex(ModelEntity* c, Vector3 const& point, Vector3 const& param);
+    MeshEntity* createVertex(ModelEntity* c, Vector3 const& point,
+        Vector3 const& param);
     void requireUnfrozen()
     {
       if (hasFrozenFields)
@@ -40,7 +42,8 @@ class Mesh2 : public Mesh
       requireUnfrozen();
       return createVert_(c);
     }
-    virtual MeshEntity* createEntity_(int type, ModelEntity* c, MeshEntity** down) = 0;
+    virtual MeshEntity* createEntity_(int type, ModelEntity* c,
+        MeshEntity** down) = 0;
     MeshEntity* createEntity(int type, ModelEntity* c, MeshEntity** down)
     {
       requireUnfrozen();
@@ -54,7 +57,8 @@ class Mesh2 : public Mesh
     }
     virtual void addMatch(MeshEntity* e, int peer, MeshEntity* match) = 0;
     virtual void clearMatches(MeshEntity* e) = 0;
-    virtual void repartition(MeshTag* elementWeights, double maximumImbalance) = 0;
+    virtual void repartition(MeshTag* elementWeights,
+        double maximumImbalance) = 0;
     virtual void acceptChanges() = 0;
 };
 
