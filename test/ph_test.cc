0e9bf252719e554ad658eac69259b42daecfcb4a
Dan Ibanez <ibaned@rpi.edu>
2014-06-03 13:44:57 -0400

    properly initialize libraries in ph_test, it works

--- test/ph_test.cc
+++ test/ph_test.cc
@@ -4,9 +4,14 @@
 #include <apfMDS.h>
 #include <apfMesh2.h>
 #include <apf.h>
+#include <gmi_mesh.h>
+#include <PCU.h>
 
 int main(int argc, char** argv)
 {
+  MPI_Init(&argc, &argv);
+  PCU_Comm_Init();
+  gmi_register_mesh();
   ph::Input in("adapt.inp");
   apf::Mesh2* m = apf::loadMdsMesh(
       in.modelFileName.c_str(), in.meshFileName.c_str());
@@ -16,4 +21,6 @@ int main(int argc, char** argv)
     ph::readAndAttachSolution(in, m);
   m->destroyNative();
   apf::destroyMesh(m);
+  PCU_Comm_Free();
+  MPI_Finalize();
 }
