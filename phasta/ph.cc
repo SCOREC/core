87fe6dcfe9b2afae8daf8c1e0104973eaec8e591
Dan Ibanez <ibaned@rpi.edu>
2014-06-05 16:39:39 -0400

    added some mkdir-related phasta helpers

--- phasta/ph.cc
+++ phasta/ph.cc
@@ -3,13 +3,14 @@
 #include <cstdlib>
 #include <stdarg.h>
 #include <cassert>
+#include <sstream>
+#include <iostream>
 
-/* both of these are for
-   goToStepDir */
+/* OS-specific things try to stay here */
 #include <sys/stat.h>
 #include <unistd.h>
 
-#include <sstream>
+#include <PCU.h>
 
 namespace ph {
 
@@ -23,15 +24,52 @@ void fail(const char* format, ...)
   abort();
 }
 
+enum {
+  DIR_MODE = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH
+};
+
 void goToStepDir(int step)
 {
   std::stringstream ss;
   ss << step;
   std::string s = ss.str();
-  int err = mkdir(s.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  int err = mkdir(s.c_str(), DIR_MODE);
   assert(!err);
   err = chdir(s.c_str());
   assert(!err);
+}
+
+enum {
+  DIR_FANOUT = 2048
+};
+
+std::string setupOutputDir(int parts)
+{
+  std::stringstream ss;
+  ss << parts << "-procs_case";
+  std::string s = ss.str();
+  if (!PCU_Comm_Self())
+    mkdir(s.c_str(), DIR_MODE);
+  PCU_Barrier();
+  return s;
+}
+
+void setupOutputSubdir(int parts, std::string& path)
+{
+  if (PCU_Comm_Peers() <= DIR_FANOUT)
+    return;
+  int self = PCU_Comm_Self();
+  int subSelf = self % DIR_FANOUT;
+  std::stringstream ss(path);
+  ss << '/' << subSelf << '/';
+  path = ss.str();
+  if (!subSelf) {
+    if (mkdir(path.c_str(), DIR_MODE)) {
+      std::cerr << "overwriting directory " << path
+          << " -Rank " << PCU_Comm_Self() << '\n';
+    }
+  }
+  PCU_Barrier();
 }
 
 }
--- phasta/ph.h
+++ phasta/ph.h
@@ -1,11 +1,15 @@
 #ifndef PH_H
 #define PH_H
 
+#include <string>
+
 namespace ph {
 
 void fail(const char* format, ...) __attribute__((noreturn,format(printf,1,2)));
 
 void goToStepDir(int step);
+std::string setupOutputDir(int parts);
+void setupOutputSubdir(int parts, std::string& path);
 
 }
 
