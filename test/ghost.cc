3c464049cfda4e7adb83434bfe8393501b71b737
Cameron Smith <smithc11@rpi.edu>
2014-06-10 17:02:24 -0400

    ghost - high surface area selector added, formatting fixes to driver

--- test/ghost.cc
+++ test/ghost.cc
@@ -6,43 +6,41 @@
 #include <parma.h>
 
 namespace {
+  const char* modelFile = 0;
+  const char* meshFile = 0;
 
-const char* modelFile = 0;
-const char* meshFile = 0;
+  void freeMesh(apf::Mesh* m)
+  {
+    m->destroyNative();
+    apf::destroyMesh(m);
+  }
 
-void freeMesh(apf::Mesh* m)
-{
-  m->destroyNative();
-  apf::destroyMesh(m);
-}
+  void getConfig(int argc, char** argv)
+  {
+    assert(argc==3);
+    modelFile = argv[1];
+    meshFile = argv[2];
+  }
 
-void getConfig(int argc, char** argv)
-{
-  assert(argc==3);
-  modelFile = argv[1];
-  meshFile = argv[2];
-}
+  apf::MeshTag* applyUnitVtxWeight(apf::Mesh* m) {
+    apf::MeshTag* wtag = m->createDoubleTag("ghostUnitWeight",1);
+    apf::MeshEntity* e;
+    apf::MeshIterator* itr = m->begin(0);
+    double w = (PCU_Comm_Self() % 2);
+    while( (e = m->iterate(itr)) )
+      m->setDoubleTag(e, wtag, &w);
+    m->end(itr);
+    return wtag;
+  }
 
-apf::MeshTag* applyUnitVtxWeight(apf::Mesh* m) {
-  apf::MeshTag* wtag = m->createDoubleTag("ghostUnitWeight",1);
-  apf::MeshEntity* e;
-  apf::MeshIterator* itr = m->begin(0);
-  double w = 1;
-  while( (e = m->iterate(itr)) )
-    m->setDoubleTag(e, wtag, &w);
-  m->end(itr);
-  return wtag;
-}
-
-void runParma(apf::Mesh* m) {
-  apf::MeshTag* weights = applyUnitVtxWeight(m);
-  const int layers = 3;
-  const int bridgeDim = 1;
-  apf::Balancer* ghost = Parma_MakeGhostDiffuser(m, layers, bridgeDim);
-  ghost->balance(weights, 1.05);
-  delete ghost;
-}
-
+  void runParma(apf::Mesh* m) {
+    apf::MeshTag* weights = applyUnitVtxWeight(m);
+    const int layers = 3;
+    const int bridgeDim = 1;
+    apf::Balancer* ghost = Parma_MakeGhostDiffuser(m, layers, bridgeDim);
+    ghost->balance(weights, 1.05);
+    delete ghost;
+  }
 }
 
 int main(int argc, char** argv)
