117ea530ebb7970e37c9c2752d1a4bee8a61b812
Dan Ibanez <ibaned@rpi.edu>
2014-06-12 18:41:27 -0400

    got some (cryptic) documentation from the old source

--- phasta/phOutput.h
+++ phasta/phOutput.h
@@ -35,23 +35,36 @@ struct EnsaArrays
    number k in [0,1] of
    element j of
    boundary block i */
+/* ibcb (part 0) has these bits:
+   MF NP TV HF TW F1 F2 F3 F4
+   0  1  2  3  4  5  6  7  8
+   part 1 is just the value of SID */
   int*** ibcb;
 /* bcb[i][j][k] is the natural boundary condition
    value for
    boundary condition k of
    element j of
    boundary block i */
+/* bcb is organized as follows:
+   MF NP TV     HF F1 F2 F3 F4
+   0  1  2 3 4  5  6  7  8  9 */
   double*** bcb;
 /* nbc[i] is the index into essential boundary condition
    arrays of local node i (probably ;) */
   int* nbc;
 /* ibc[i] is the essential boundary condition
    status code of essential BC node i */
+/* ibc bits are as follows:
+var: rho t p u v w sc1 sc2 sc3 sc4 perio spebc
+bit:  0  1 2 3 4 5  6   7   8   9    10    11 */
   int* ibc;
 /* bc[i][j] is the essential boundary condition
    value of 
    essential boundary condition j of
    essential BC node i */
+/* bc is organized as follows:
+var:  rho t p c11 c12 c13 m1 c21 c22 c23 m2 theta sc_j
+idx:   0  1 2  3   4   5  6   7   8   9  10   11   12  */
   double** bc;
 };
 
