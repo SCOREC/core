130178d97c2acfbf3b3a3bd28ea9acdc75d8ae56
Dan Ibanez <ibaned@rpi.edu>
2014-06-16 14:48:48 -0400

    adding Zoltan-based split exe

---
+++ test/zsplit.cc
@@ -0,0 +1,66 @@
+#include <gmi_mesh.h>
+#include <apf.h>
+#include <apfMesh2.h>
+#include <apfMDS.h>
+#include <PCU.h>
+#include <apfZoltan.h>
+#include <parma.h>
+
+namespace {
+
+const char* modelFile = 0;
+const char* meshFile = 0;
+const char* outFile = 0;
+int partitionFactor = 1;
+
+void freeMesh(apf::Mesh* m)
+{
+  m->destroyNative();
+  apf::destroyMesh(m);
+}
+
+apf::Migration* getPlan(apf::Mesh* m)
+{
+  apf::Splitter* splitter = apf::makeZoltanSplitter(
+      m, apf::GRAPH, apf::PARTITION, false);
+  apf::MeshTag* weights = Parma_WeighByMemory(m);
+  apf::Migration* plan = splitter->split(weights, 1.10, partitionFactor);
+  apf::removeTagFromDimension(m, weights, m->getDimension());
+  m->destroyTag(weights);
+  delete splitter;
+  return plan;
+}
+
+void runAfter(apf::Mesh2* m)
+{
+  m->writeNative(outFile);
+  freeMesh(m);
+}
+
+void getConfig(int argc, char** argv)
+{
+  assert(argc==5);
+  modelFile = argv[1];
+  meshFile = argv[2];
+  outFile = argv[3];
+  partitionFactor = atoi(argv[4]);
+}
+
+}
+
+int main(int argc, char** argv)
+{
+  int provided;
+  MPI_Init_thread(&argc,&argv,MPI_THREAD_MULTIPLE,&provided);
+  assert(provided==MPI_THREAD_MULTIPLE);
+  PCU_Comm_Init();
+  gmi_register_mesh();
+  PCU_Protect();
+  getConfig(argc,argv);
+  apf::Mesh2* m = apf::loadMdsMesh(modelFile,meshFile);
+  splitMdsMesh(m, getPlan(m), partitionFactor, runAfter);
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
+
+
