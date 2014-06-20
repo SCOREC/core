74c26af8c5cc2168973c7c174d23ccf9430ad973
Cameron Smith <smithc11@rpi.edu>
2014-06-20 11:50:45 -0400

    usage stmt for gmi_test

--- test/gmi_test.cc
+++ test/gmi_test.cc
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <SimUtil.h>
 #include <SimModel.h>
 #include "gmi_mesh.h"
@@ -5,6 +6,11 @@
 
 int main(int argc, char** argv)
 {
+  if (argc != 3) {
+    printf("Convert simmetrix smd model to a gmi dmg model\n");
+    printf("Usage: %s <simmetrix smd model> <gmi dmg model>\n", argv[0]);
+    return 0;
+  }
   Sim_readLicenseFile(0);
   SimModel_start();
   gmi_register_mesh();
