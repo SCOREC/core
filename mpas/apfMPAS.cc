7ebb8d708fe22ae3f060691cf48cbc26aa089fc2
Dan Ibanez <ibaned@rpi.edu>
2014-07-09 17:48:56 -0400

    updated FindNetCDF, changed names, skeleton builds

---
+++ mpas/apfMPAS.cc
@@ -0,0 +1,10 @@
+#include "apfMPAS.h"
+#include <apfMesh2.h>
+
+namespace apf {
+
+void loadMpasMesh(apf::Mesh2* m, const char* filename)
+{
+}
+
+}
---
+++ mpas/apfMPAS.h
@@ -0,0 +1,12 @@
+#ifndef APF_MPAS_H
+#define APF_MPAS_H
+
+namespace apf {
+
+class Mesh2;
+
+void loadMpasMesh(apf::Mesh2* m, const char* filename);
+
+}
+
+#endif
