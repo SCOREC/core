6ddcd92701ccb96bdc3899cc31ff9feed2341eaf
Dan Ibanez <dan.a.ibanez@gmail.com>
2014-07-10 11:40:43 -0400

    filled in mpas_write

--- test/mpas_write.cc
+++ test/mpas_write.cc
@@ -1,6 +1,23 @@
-#include <cstdio>
+#include <apfMDS.h>
+#include <gmi_mesh.h>
+#include <apfMPAS.h>
+#include <apfMesh2.h>
+#include <apf.h>
+#include <PCU.h>
 
 int main(int argc, char** argv)
 {
-  printf("Hello, world\n");
+  if (argc != 4) {
+    printf("usage: %s <model (.dmg)>  <mesh (.smb)> <NetCDF file>", argv[0]);
+    return 0;
+  }
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  gmi_register_mesh();
+  apf::Mesh2* m = apf::loadMdsMesh(argv[1], argv[2]);
+  apf::writeMpasAssignments(m, argv[3]);
+  m->destroyNative();
+  apf::destroyMesh(m);
+  PCU_Comm_Free();
+  MPI_Finalize();
 }
