9fdf8ce0a28b164ac369f6942362148e27c2b21e
Dan Ibanez <dan.a.ibanez@gmail.com>
2014-08-06 09:31:54 -0400

    starting dashboard scripts

---
+++ cdash/nightly.cmake
@@ -0,0 +1,131 @@
+cmake_minimum_required(VERSION 2.8)
+
+SET(CTEST_DO_SUBMIT OFF)
+SET(CTEST_TEST_TYPE Nightly)
+
+set(CTEST_SITE             "twister.scorec.rpi.edu" )
+set(CTEST_DASHBOARD_ROOT   "/lore/dibanez/cdash" )
+set(CTEST_CMAKE_GENERATOR  "Unix Makefiles" )
+set(CTEST_BUILD_CONFIGURATION  RelWithDebInfo)
+
+set(CTEST_PROJECT_NAME "SCOREC")
+set(CTEST_SOURCE_NAME repos)
+set(CTEST_BUILD_NAME  "linux-gcc-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_BINARY_NAME build)
+
+set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/${CTEST_SOURCE_NAME}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/${CTEST_BINARY_NAME}")
+
+if(not exists "${CTEST_SOURCE_DIRECTORY}")
+  file(make_directory "${CTEST_SOURCE_DIRECTORY}")
+endif()
+if(not exists "${CTEST_BINARY_DIRECTORY}")
+  file(make_directory "${CTEST_BINARY_DIRECTORY}")
+endif()
+
+configure_file(${CTEST_SCRIPT_DIRECTORY}/CTestConfig.cmake
+               ${CTEST_SOURCE_DIRECTORY}/CTestConfig.cmake COPYONLY)
+
+set(CTEST_BUILD_FLAGS -j4)
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=SCOREC")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
+find_program(CTEST_GIT_COMMAND NAMES git)
+set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
+
+SET(SCOREC_REPO https://github.com/SCOREC/core.git)
+
+ctest_empty_binary_directory( "${CTEST_BINARY_DIRECTORY}" )
+
+if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}/publicTrilinos/SCOREC")
+  EXECUTE_PROCESS(COMMAND "${CTEST_GIT_COMMAND}" 
+    clone ${SCOREC_REPO} ${CTEST_SOURCE_DIRECTORY}/core
+    OUTPUT_VARIABLE _out
+    ERROR_VARIABLE _err
+    RESULT_VARIABLE HAD_ERROR)
+  
+  message(STATUS "out: ${_out}")
+  message(STATUS "err: ${_err}")
+  message(STATUS "res: ${HAD_ERROR}")
+  if(HAD_ERROR)
+    message(FATAL_ERROR "Cannot checkout core repository!")
+  endif()
+endif()
+
+ctest_start(${CTEST_TEST_TYPE})
+
+if(CTEST_DO_SUBMIT)
+  ctest_submit(FILES "${CTEST_SCRIPT_DIRECTORY}/Project.xml"
+      RETURN_VALUE HAD_ERROR)
+  if(HAD_ERROR)
+    message(FATAL_ERROR "Cannot submit SCOREC Project.xml!")
+  endif()
+endif()
+
+set_property(GLOBAL PROPERTY SubProject core)
+set_property(GLOBAL PROPERTY Label core)
+
+ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}/core" RETURN_VALUE count)
+message("Found ${count} changed files")
+
+if(CTEST_DO_SUBMIT)
+  ctest_submit(PARTS Update RETURN_VALUE HAD_ERROR)
+  if(HAD_ERROR)
+    message(FATAL_ERROR "Cannot update core!")
+  endif()
+endif()
+
+SET(CONFIGURE_OPTIONS
+  "-DCMAKE_C_COMPILER=mpicc" \
+  "-DCMAKE_CXX_COMPILER=mpicxx" \
+  "-DCMAKE_C_FLAGS=-O2 -g" \
+  "-DCMAKE_CXX_FLAGS=-O2 -g" \
+  "-DENABLE_THREADS=ON" \
+  "-DENABLE_ZOLTAN=ON" \
+  "-DENABLE_MPAS=ON"
+)
+
+ctest_configure(
+  BUILD "${CTEST_BINARY_DIRECTORY}" 
+  SOURCE "${CTEST_SOURCE_DIRECTORY}/core"
+  OPTIONS "${CONFIGURE_OPTIONS}"
+  RETURN_VALUE HAD_ERROR)
+
+if(HAD_ERROR)
+	message(FATAL_ERROR "Cannot configure core build!")
+endif()
+
+if(CTEST_DO_SUBMIT)
+  ctest_submit(PARTS Configure RETURN_VALUE HAD_ERROR)
+  if(HAD_ERROR)
+    message(FATAL_ERROR "Cannot submit core configure results!")
+  endif()
+endif()
+
+ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" RETURN_VALUE HAD_ERROR)
+
+if(HAD_ERROR)
+	message(FATAL_ERROR "Cannot build core!")
+endif()
+
+if(CTEST_DO_SUBMIT)
+  ctest_submit(PARTS Build RETURN_VALUE HAD_ERROR)
+  if(HAD_ERROR)
+    message(FATAL_ERROR "Cannot submit core build results!")
+  endif()
+endif()
+
+# ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}")
+
+# if(CTEST_DO_SUBMIT)
+#   ctest_submit(PARTS Build RETURN_VALUE HAD_ERROR)
+#   if(HAD_ERROR)
+#     message(FATAL_ERROR "Cannot submit core test results!")
+#   endif()
+# endif()
+
+message("DONE")
+
---
+++ cdash/nightly.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /usr/local/etc/bash_profile
+module load cmake/latest
+module load openmpi/1.6.5-ib
+module load parmetis/OpenMPI-1.6.5/4.0.2
+module load zoltan/OpenMPI-1.6.5/3.8
+module load simmetrix/simModSuite
+module load netcdf
+
+export PATH=/lore/dibanez/bin:$PATH
+
+cd /lore/dibanez/cdash
+
+svn up meshes/
+ctest -VV -D Nightly -S /lore/dibanez/core/cdash/nightly.cmake &> log
+rm -rf build/ repos/
+
