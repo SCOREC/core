505708658a78b58d47cf6615a9726e8a54e9abb4
Dan Ibanez <dan.a.ibanez@gmail.com>
2014-08-07 10:28:21 -0400

    use the simmetrix model for all pipe tests

--- test/testing.cmake
+++ test/testing.cmake
@@ -2,49 +2,49 @@ set(MESHES /lore/dibanez/meshes)
 set(MDIR ${MESHES}/pipe)
 add_test(verify_serial
   verify
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "${MDIR}/pipe.smb")
 add_test(uniform_serial
   uniform
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "${MDIR}/pipe.smb"
   "pipe.smb")
 add_test(ma_serial
   ma_test
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "pipe.smb")
 add_test(tet_serial
   tetrahedronize
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "pipe.smb"
   "tet.smb")
 add_test(split_2
   split
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "pipe.smb"
   "pipe_2_.smb"
   2)
 add_test(split_4
   ${MPIRUN} ${MPIRUN_PROCFLAG} 2
   zsplit
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "pipe_2_.smb"
   "pipe_4_.smb"
   2)
 add_test(verify_parallel
   ${MPIRUN} ${MPIRUN_PROCFLAG} 4
   ma_test
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "pipe_4_.smb")
 add_test(ma_parallel
   ${MPIRUN} ${MPIRUN_PROCFLAG} 4
   ma_test
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "pipe_4_.smb")
 add_test(tet_parallel
   ${MPIRUN} ${MPIRUN_PROCFLAG} 4
   tetrahedronize
-  "${MDIR}/pipe.dmg"
+  "${MDIR}/pipe.smd"
   "pipe_4_.smb"
   "tet.smb")
 set(MDIR ${MESHES}/mpas)
