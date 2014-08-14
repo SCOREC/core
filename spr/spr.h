f5eff41a2f78c98e0702c5b21526ddb122bf9d4c
Brian Granzow <brian.granzow@gmail.com>
2014-08-14 18:00:40 -0400

    begin spr header clean up

--- spr/spr.h
+++ spr/spr.h
@@ -21,26 +21,16 @@ namespace apf {
   * @param name (In) name of integration point field
   * @param order (In) integration order of accuracy
   */
-Field* getGradIPField(Field* f, const char* name, int order);
+apf::Field* getGradIPField(apf::Field* f,
+                           const char* name, 
+                           int order);
 
-/** \brief Computes an isotropic size field from a gradient field.
-  *
-  * \details This function carries out the majority of the SPR-based
-  * error estimator. The input is a gradient-like linear field, which
-  * could be strain, stress, or a vector gradient.
-  * The output is the linear scalar size field to be used for mesh
-  * adaptation.
-  *
-  * \param eps The gradient-like input field
-  * \param adaptRatio The maximum acceptable relative error
-  *                   (|err|/|eps|) which determines how fine
-  *                   the size field is.
-  */
-Field* getSPRSizeField(Field* eps, double adaptRatio);
-
-/** \brief Recovers a linear matrix field from an integration point sampling
+/** @brief recover a nodal field using patch recovery
+  * @param f (In) integration point field
   */
 Field* recoverField(Field* ipMatrixField);
+
+Field* getSPRSizeField(Field* eps, double adaptRatio);
 
 /** @brief solve linear least squares problem Ax=b
   * @param A (In) mxn matrix
