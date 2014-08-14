7c88fb4ece2d9e57b77a70125b7e84c478611667
Brian Granzow <brian.granzow@gmail.com>
2014-08-14 16:45:07 -0400

    clean up PatchOp

--- spr/apfRecoverField.cc
+++ spr/apfRecoverField.cc
@@ -168,56 +168,59 @@ void getSprPoints(Field* f,
   }
 }
 
-void runSpr(Field* eps,
-            Field* eps_star,
+/** @brief run patch recovery
+  * @param f (In) integration point field
+  * @param f_star (In) recovered nodal field
+  * @elements (In) elements in patch
+  * @param entity (In) central entity of patch
+  */
+void runSpr(Field* f,
+            Field* f_star,
             std::set<MeshEntity*>& elements,
             MeshEntity* entity)
 {
-  SprPoints sprPoints;
-  getSprPoints(eps,elements,sprPoints);
-  NewArray<double> componentValues(sprPoints.count);
+  SprPoints spr_points;
+  getSprPoints(f,elements,spr_points);
+  NewArray<double> component_values(spr_points.count);
   Vector3 point;
-  getMesh(eps)->getPoint(entity,0,point);
+  getMesh(f)->getPoint(entity,0,point);
   Matrix3x3 value;
-  Mesh* mesh = getMesh(eps);
+  Mesh* mesh = getMesh(f);
   int order = mesh->getShape()->getOrder();
   for (int i=0; i < 3; ++i)
     for (int j=0; j < 3; ++j)
     {
-      for (int p=0; p < sprPoints.count; ++p)
-        componentValues[p] = sprPoints.values[p][i][j];
-      DynamicVector polynomial;
-      DynamicVector coefficients;
-      evalPolynomialCoeffs(order,sprPoints.count,
-          sprPoints.points,componentValues,coefficients);
-      evalPolynomialTerms(order,point,polynomial);
-      value[i][j] = coefficients*polynomial;
+      for (int p=0; p < spr_points.count; ++p)
+        component_values[p] = spr_points.values[p][i][j];
+      DynamicVector coeffs;
+      evalPolynomialCoeffs(order,spr_points.count,
+          spr_points.points,component_values,coeffs);
+      DynamicVector terms;
+      evalPolynomialTerms(order,point,terms);
+      value[i][j] = coeffs*terms;
     }
-  /* Need to set a matrix value to each node on a mesh
-     entity in order to generalize. OK for 1st and 2nd
-     order for now */
-  setMatrix(eps_star,entity,0,value);
+  setMatrix(f_star,entity,0,value);
 }
 
 class PatchOp : public CavityOp
 {
   public:
-    PatchOp(Field* e, Field* es):
-      CavityOp(getMesh(e)),
-      eps(e),
-      eps_star(es),
-      pointCount(0)
+    PatchOp(Field* f_, Field* f_star_):
+      CavityOp(getMesh(f_)),
+      f(f_),
+      f_star(f_star_),
+      num_points(0)
     {
     }
     void reset(MeshEntity* e)
     {
-      pointCount = 0;
+      num_points = 0;
       elements.clear();
       entity = e;
     }
     virtual Outcome setEntity(MeshEntity* e)
     {
-      if (hasEntity(eps_star,e))
+      if (hasEntity(f_star,e))
         return SKIP;
       reset(e);
       if ( ! buildPatch())
@@ -226,7 +229,7 @@ class PatchOp : public CavityOp
     }
     virtual void apply()
     {
-      runSpr(eps,eps_star,elements,entity);
+      runSpr(f,f_star,elements,entity);
     }
     bool buildPatch()
     {
@@ -244,47 +247,47 @@ class PatchOp : public CavityOp
     }
     bool expandAsNecessary()
     {
-      const int desiredCount = 4;
-      if (pointCount >= desiredCount)
+      const int num_desired_points = 4;
+      if (num_points >= num_desired_points)
         return true;
-      std::set<MeshEntity*> oldSet = elements;
+      std::set<MeshEntity*> old_set = elements;
       int d = mesh->getDimension();
       for (int shared_dim=d-1; shared_dim >= 0; --shared_dim)
       {
-        if (!addElementsThatShare(shared_dim,oldSet))
+        if (!addElementsThatShare(shared_dim,old_set))
           return false;
-        if (pointCount >= desiredCount)
+        if (num_points >= num_desired_points)
           return true;
       }
-      bool hope = elements.size() > oldSet.size();
+      bool hope = elements.size() > old_set.size();
       if (hope)
         return expandAsNecessary();
       else
       {
-        fail("SPR Patch Builder: all hope is lost.");
+        fail("SPR: patch construction: all hope is lost.");
         return false;
       }
     }
     bool addElementsThatShare(int dimension,
-                              std::set<MeshEntity*>& oldSet)
+                              std::set<MeshEntity*>& old_set)
     {
       std::set<MeshEntity*> bridges;
-      for (std::set<MeshEntity*>::iterator it = oldSet.begin();
-          it != oldSet.end(); ++it)
+      for (std::set<MeshEntity*>::iterator it = old_set.begin();
+          it != old_set.end(); ++it)
       {
         Downward down;
         int nd = mesh->getDownward(*it,dimension,down);
         for (int i=0; i < nd; ++i)
           bridges.insert(down[i]);
       }
-      std::vector<MeshEntity*> bridgeArray(bridges.begin(),bridges.end());
+      std::vector<MeshEntity*> bridge_array(bridges.begin(),bridges.end());
       bridges.clear();
-      if ( ! requestLocality(&(bridgeArray[0]),bridgeArray.size()))
+      if ( ! requestLocality(&(bridge_array[0]),bridge_array.size()))
         return false;
-      for (size_t i=0; i < bridgeArray.size(); ++i)
+      for (size_t i=0; i < bridge_array.size(); ++i)
       {
         Adjacent candidates;
-        mesh->getAdjacent(bridgeArray[i],mesh->getDimension(),candidates);
+        mesh->getAdjacent(bridge_array[i],mesh->getDimension(),candidates);
         add(candidates);
       }
       return true;
@@ -300,29 +303,29 @@ class PatchOp : public CavityOp
         return;
       elements.insert(e);
       MeshElement* element = createMeshElement(mesh,e);
-      pointCount += countIntPoints(element,1);
+      num_points += countIntPoints(element,1);
       destroyMeshElement(element);
     }
-    Field* eps;
-    Field* eps_star;
-    int pointCount;
+    Field* f;
+    Field* f_star;
+    int num_points;
     std::set<MeshEntity*> elements;
     MeshEntity* entity;
 };
 
-Field* recoverField(Field* eps)
+Field* recoverField(Field* f)
 {
   std::string name = "spr_";
-  name += getName(eps);
-  Mesh* m = getMesh(eps);
-  Field* eps_star = createFieldOn(m,name.c_str(),MATRIX);
-  PatchOp op(eps,eps_star);
+  name += getName(f);
+  Mesh* m = getMesh(f);
+  Field* f_star = createFieldOn(m,name.c_str(),MATRIX);
+  PatchOp op(f,f_star);
   for (int i=0; i <= 3; ++i)
   {
     if (m->getShape()->countNodesOn(i) != 0)
       op.applyToDimension(i);
   }
-  return eps_star;
+  return f_star;
 }
 
 }
