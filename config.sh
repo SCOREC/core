9dab25e8e343fbe4133a73851aff1a726f1d6f30
Dan Ibanez <ibaned@rpi.edu>
2014-08-20 11:02:07 -0400

    hardcode mpi wrappers in config.sh instead of $CC

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 cmake .. \
-  -DCMAKE_C_COMPILER="$CC" \
-  -DCMAKE_CXX_COMPILER="$CXX" \
+  -DCMAKE_C_COMPILER="mpicc" \
+  -DCMAKE_CXX_COMPILER="mpicxx" \
   -DCMAKE_C_FLAGS="-O2 -g -Wall" \
   -DCMAKE_CXX_FLAGS="-O2 -g -Wall" \
   -DENABLE_THREADS=ON \
