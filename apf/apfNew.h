98d5ad311bf4e3159d9baf7884d1721fe11ddcf5
Dan Ibanez <ibaned@rpi.edu>
2014-08-21 14:26:44 -0400

    documented apfNew.h

--- apf/apfNew.h
+++ apf/apfNew.h
@@ -8,31 +8,55 @@
 #ifndef APFNEW_H
 #define APFNEW_H
 
+/** \file apfNew.h
+    \brief wrapper for operator new/delete [] */
+
 #include <cstddef>
 #include <algorithm>
 #include <new>
 
 namespace apf {
 
-/* this wraps a new [] array to at least prevent memory leaks,
-   double free() errors, and the like.
-   lacking size data, it has a limited but important niche */
+/** \brief wrapper over operator new/delete []
+  \details this wrapper is used to automatically
+  call delete [] on an array created with new [],
+  preventing memory leaks and forming the base
+  class for other containers.
+  Ideally, all usage of operator new [] should be
+  replaced with this.
+  However, since this class does not store its
+  own size, it cannot be copied and its niche
+  is limited. see apf::DynamicArray for the
+  next step */
 template <class T>
 class NewArray
 {
   public:
+    /** \brief default initialize pointer to zero */
     NewArray():elements(0) {}
+    /** \brief construct with (n) elements */
     NewArray(std::size_t n):elements(0) {allocate(n);}
+    /** \brief destructor automatically frees memory */
     ~NewArray() {deallocate();}
+    /** \brief return true if memory has been allocated */
     bool allocated() const {return elements;}
+    /** \brief mutable index operator */
     T& operator[](std::size_t i) {return elements[i];}
+    /** \brief immutable index operator */
     T const& operator[](std::size_t i) const {return elements[i];}
+    /** \brief user-callable deallocation helper
+        \details remember that operator delete [] is
+                 a no-op on a zero pointer */
     void deallocate() {delete [] elements; elements=0;}
+    /** \brief user-callable allocation helper
+      \details note that no mix of allocate/deallocate
+      calls can cause a memory leak */
     void allocate(std::size_t n)
     {
       deallocate();
       elements = new T[n];
     }
+    /** \brief swap pointers with another NewArray */
     void swap(NewArray<T>& other)
     {
       std::swap(elements,other.elements);
