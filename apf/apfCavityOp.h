0025ada2bf0bc30cd1ce54625bc0420884b444a4
Dan Ibanez <ibaned@rpi.edu>
2014-08-21 20:16:49 -0400

    documented apfCavityOp.h

--- apf/apfCavityOp.h
+++ apf/apfCavityOp.h
@@ -8,20 +8,24 @@
 #ifndef APFCAVITYOP_H
 #define APFCAVITYOP_H
 
+/** \file apfCavityOp.h
+  \brief Parallel mesh cavity operator system */
+
 #include "apfMesh.h"
 #include <vector>
 #include <cstring>
 
 namespace apf {
 
-/* A cavity operation is a generalization for
+/** \page cavity Cavity Operator
+   A cavity operation is a generalization for
    anything operating on a mesh entity
    that needs access to a surrounding cavity
    comprised of adjacent elements and their
    closures, either to read or write attached
    data or even to modify the mesh.
 
-   A user-provided CavityOp should define two
+   A user-provided apf::CavityOp should define two
    methods: setEntity() and apply().
    setEntity() should proceed as follows:
      1) check if the operator should be applied.
@@ -59,18 +63,35 @@ namespace apf {
    mesh modifying operators should call preDeletion(e) before
    actually deleting an entity to prevent a crash due to
    iterator invalidation.
- */
+*/
 
+/** \brief user-defined mesh cavity operator */
 class CavityOp
 {
   public:
+    /** \brief constructor
+      \param canModify true iff the operator can create or
+                       destroy mesh entities */
     CavityOp(Mesh* m, bool canModify = false);
-    enum Outcome { SKIP, OK, REQUEST };
+    /** \brief outcome of a setEntity call */
+    enum Outcome {
+      /** \brief skip the given entity */
+      SKIP,
+      /** \brief apply to the given entity */
+      OK,
+      /** \brief request more elements around the entity */
+      REQUEST };
+    /** \brief evaluate whether what to do with this entity */
     virtual Outcome setEntity(MeshEntity* e) = 0;
+    /** \brief apply the operator on the (now local) cavity */
     virtual void apply() = 0;
+    /** \brief parallel collective operation over entities of one dimension */
     void applyToDimension(int d);
+    /** \brief within setEntity, require that entities be made local */
     bool requestLocality(MeshEntity** entities, int count);
+    /** \brief call before deleting a mesh entity during the operation */
     void preDeletion(MeshEntity* e);
+    /** \brief mesh pointer for convenience */
     Mesh* mesh;
   private:
     typedef std::vector<MeshEntity*> Requests;
