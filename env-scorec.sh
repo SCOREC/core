47a507a88c3fb7cfa3ce6b3daef973c8f04868b8
Cameron Smith <smithc11@rpi.edu>
2014-09-12 14:21:27 -0400

    use git module

--- env-scorec.sh
+++ env-scorec.sh
@@ -2,6 +2,4 @@ module load cmake/latest
 module load mpich3/3.1.2-thread-multiple
 module load parmetis/mpich3.1.2/4.0.3
 module load zoltan/mpich3.1.2/3.8
-#Dan's install of Git v3 !
-#make a module of this soon !
-export PATH=/lore/dibanez:$PATH
+module load git
