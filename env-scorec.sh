893218e7ce39db9a5bad1b78f8b62ec345b5c56a
Cameron Smith <smithc11@rpi.edu>
2014-09-13 07:49:31 -0400

    use gcc and simmetrix module

--- env-scorec.sh
+++ env-scorec.sh
@@ -1,5 +1,7 @@
 module load cmake/latest
+module load gcc
 module load mpich3/3.1.2-thread-multiple
 module load parmetis/mpich3.1.2/4.0.3
 module load zoltan/mpich3.1.2/3.8
+module load simmetrix/simModSuite
 module load git
