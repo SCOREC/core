SCOREC/core

commit a3518cc9516a3c5767c671b9dc41ccb0720f5529

trying to fix pgi compiler warning
std::map::at is a C++11 construct

apf/apfConstruct.cc
@@ -234,7 +234,7 @@ void setCoords(Mesh2* m, const double* coords, int nverts,
     double v[3];
     PCU_Comm_Unpack(v, sizeof(v));
     Vector3 vv(v);
-    m->setPoint(globalToVert.at(gid), 0, vv);
+    m->setPoint(globalToVert[gid], 0, vv);
   }
 
   delete [] c;

commit 553be02d1d60b779c69b23c725675e885ef7fa43

fix compile error
caused by a3518cc
apparently the std::map::at
was used to access a const std::map.
Since C++98 doesn't give us nice
accessor functions for const maps,
lets forget about const correctness.

apf/apfConstruct.cc
@@ -158,7 +158,7 @@ void construct(Mesh2* m, int* conn, int nelem, int etype,
 }
 
 void setCoords(Mesh2* m, const double* coords, int nverts,
-    const GlobalToVert& globalToVert)
+    GlobalToVert& globalToVert)
 {
   Gid max = getMax(globalToVert);
   Gid total = max + 1;

apf/apfConvert.h
@@ -56,7 +56,7 @@ void construct(Mesh2* m, int* conn, int nelem, int etype,
   * coordinates assigned.
   */
 void setCoords(Mesh2* m, const double* coords, int nverts,
-    const GlobalToVert& globalToVert);
+    GlobalToVert& globalToVert);
 
 /** \brief convert an apf::Mesh2 object into a connectivity array
   \details this is useful for debugging the apf::convert function */