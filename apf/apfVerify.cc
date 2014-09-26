7cb18c64e4221d6cda0b57dcb6b75c3f2e1b148c
Dan Ibanez <ibaned@rpi.edu>
2014-09-26 16:05:36 -0400

    raising the severity of coordinate mismatch

--- apf/apfVerify.cc
+++ apf/apfVerify.cc
@@ -386,8 +386,10 @@ void verify(Mesh* m)
   verifyConnectivity(m);
   verifyOrder(m);
   long n = verifyCoords(m);
-  if (n && (!PCU_Comm_Self()))
-    fprintf(stderr,"apf::verify warning: %ld coordinate mismatches\n", n);
+  if (n && (!PCU_Comm_Self())) {
+    fprintf(stderr,"apf::verify fail: %ld coordinate mismatches\n", n);
+    abort();
+  }
   n = verifyVolumes(m);
   if (n && (!PCU_Comm_Self()))
     fprintf(stderr,"apf::verify warning: %ld negative simplex elements\n", n);
