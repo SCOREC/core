25707b76eda8947e77be1419588bb101f3698bae
Cameron Smith <smithc11@rpi.edu>
2014-10-09 13:48:27 -0400

    usage stmt for render

--- test/render.cc
+++ test/render.cc
@@ -6,9 +6,14 @@
 
 int main(int argc, char** argv)
 {
-  assert(argc==4);
   MPI_Init(&argc,&argv);
   PCU_Comm_Init();
+  if ( argc != 4 ) {
+    if ( !PCU_Comm_Self() )
+      printf("Usage: %s <model> <mesh> <out prefix>\n", argv[0]);
+    MPI_Finalize();
+    exit(EXIT_FAILURE);
+  }
   gmi_register_mesh();
   apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
   apf::writeVtkFiles(argv[3], m);
