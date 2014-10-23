d3ff26a25a853ead1f633147739a69e9566954b2
Dan Ibanez <ibaned@rpi.edu>
2014-10-23 19:00:10 -0400

    added missing DynamicMatrix print operator
    this also meant creating a new
    .cc file for DynamicMatrix.

---
+++ apf/apfDynamicMatrix.cc
@@ -0,0 +1,10 @@
+#include "apfDynamicMatrix.h"
+
+std::ostream& operator<<(std::ostream& s, apf::DynamicMatrix const& A)
+{
+  for (std::size_t i = 0; i < A.getRows(); ++i) {
+    for (std::size_t j = 0; j < A.getRows(); ++j)
+      s << A(i,j) << ' ';
+    s << '\n';
+  }
+}
