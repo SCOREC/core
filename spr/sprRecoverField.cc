7a205a195a65c3d9902d03206299667c10b4ce89
Dan Ibanez <dan.a.ibanez@gmail.com>
2014-10-24 09:54:22 -0400

    refactored field recovery, debug looks ok
    There were previously many points
    where the same thing was calculated,
    such as the order or the number of points.
    I think the main bug was in taking the
    order from the field "f", which is now
    an IP field.
    Either way, this refactor makes it
    clear where everything comes from
    and computes important things just once.

--- spr/sprRecoverField.cc
+++ spr/sprRecoverField.cc
@@ -6,16 +6,190 @@
  */
 
 #include "spr.h"
-#include "apfMesh.h"
-#include "apfShape.h"
-#include "apfField.h"
-#include "apfCavityOp.h"
+
+#include <PCU.h>
+
+#include <apfMesh.h>
+#include <apfShape.h>
+#include <apfField.h>
+#include <apfCavityOp.h>
+
 #include <set>
 
 namespace spr {
 
-struct SprPoints {
-  SprPoints():num_points(0) {}
+/* overall information useful during recovery */
+struct Recovery {
+  apf::Mesh* mesh;
+  /* order of output field and fit polynomial.
+     so far limited to 1 or 2,
+     and determined by the order of the mesh's
+     coordinate field. */
+  int order;
+  /* the number of terms in a polynomial of the above order
+     defined over 3D space: f(x,y,z) */
+  int polynomial_terms;
+  /* the number of integration points per element.
+     this immediately assumes the mesh has one element type.
+     warning: the number of points per element is not necessarily
+     determined by the order of the output field !
+     users sometimes give us "too much" information, i.e. 5 integration
+     points per tet to derive a linear field. this is ok, the
+     way this is programmed it should handle any nonzero number of points
+     per element regardless of output order (by growing bigger patches) */
+  int points_per_element;
+  /* input field containing integration point data for all elements */
+  apf::Field* f;
+  /* output field containing recovered nodal data */
+  apf::Field* f_star;
+};
+
+static int determinePointsPerElement(apf::Field* f)
+{
+  apf::Mesh* m = apf::getMesh(f);
+  int element_type = apf::getFirstType(m, m->getDimension());
+  apf::FieldShape* s = apf::getShape(f);
+  return s->countNodesOn(element_type);
+}
+
+static apf::Field* makeRecoveredField(Recovery* r)
+{
+  std::string name = "spr_";
+  name += apf::getName(r->f);
+  return apf::createLagrangeField(
+        r->mesh, name.c_str(), apf::getValueType(r->f), r->order);
+}
+
+static int countPolynomialTerms(int order)
+{
+  return ((order + 1) * (order + 2) * (order + 3)) / 6;
+}
+
+static void setupRecovery(Recovery* r, apf::Field* f)
+{
+  r->mesh = apf::getMesh(f);
+  r->order = r->mesh->getShape()->getOrder();
+  r->polynomial_terms = countPolynomialTerms(r->order);
+  r->points_per_element = determinePointsPerElement(f);
+  r->f = f;
+  r->f_star = makeRecoveredField(r);
+}
+
+typedef std::set<apf::MeshEntity*> EntitySet;
+
+struct Patch {
+  apf::Mesh* mesh;
+  Recovery* recovery;
+  /* the entity around which the patch
+     is centered. a patch collects elements
+     around this entity and then their integration
+     points will be used to recover values for
+     all nodes on this entity */
+  apf::MeshEntity* entity;
+  EntitySet elements;
+};
+
+static void setupPatch(Patch* p, Recovery* r)
+{
+  p->mesh = r->mesh;
+  p->recovery = r;
+  p->entity = 0;
+}
+
+static void startPatch(Patch* p, apf::MeshEntity* e)
+{
+  p->elements.clear();
+  p->entity = e;
+}
+
+static int countPatchPoints(Patch* p)
+{
+  return p->recovery->points_per_element * p->elements.size();
+}
+
+static bool hasEnoughPoints(Patch* p)
+{
+  return countPatchPoints(p) >= p->recovery->polynomial_terms;
+}
+
+void addElementToPatch(Patch* p, apf::MeshEntity* e)
+{
+  p->elements.insert(e);
+}
+
+void addElementsToPatch(Patch* p, apf::DynamicArray<apf::MeshEntity*>& es)
+{
+  for (std::size_t i=0; i < es.getSize(); ++i)
+    addElementToPatch(p, es[i]);
+}
+
+static bool getInitialPatch(Patch* p, apf::CavityOp* o)
+{
+  if ( ! o->requestLocality(&p->entity,1))
+    return false;
+  apf::DynamicArray<apf::MeshEntity*> adjacent;
+  p->mesh->getAdjacent(p->entity, p->mesh->getDimension(), adjacent);
+  addElementsToPatch(p, adjacent);
+  return true;
+}
+
+static bool addElementsThatShare(Patch* p, int dim,
+    EntitySet& old_elements, apf::CavityOp* o)
+{
+  EntitySet bridges;
+  APF_ITERATE(EntitySet, old_elements, it)
+  {
+    apf::Downward down;
+    int nd = p->mesh->getDownward(*it, dim, down);
+    for (int i=0; i < nd; ++i)
+      bridges.insert(down[i]);
+  }
+  std::vector<apf::MeshEntity*> 
+    bridge_array(bridges.begin(),bridges.end());
+  bridges.clear();
+  if ( ! o->requestLocality(&(bridge_array[0]),bridge_array.size()))
+    return false;
+  for (size_t i=0; i < bridge_array.size(); ++i)
+  {
+    apf::Adjacent candidates;
+    p->mesh->getAdjacent(bridge_array[i], p->mesh->getDimension(), candidates);
+    addElementsToPatch(p, candidates);
+  }
+  return true;
+}
+
+static bool expandAsNecessary(Patch* p, apf::CavityOp* o)
+{
+  if (hasEnoughPoints(p))
+    return true;
+  EntitySet old_set = p->elements;
+  int d = p->mesh->getDimension();
+  for (int shared_dim = d-1; shared_dim >= 0; --shared_dim)
+  {
+    if (!addElementsThatShare(p, shared_dim, old_set, o))
+      return false;
+    if (hasEnoughPoints(p));
+      return true;
+  }
+  bool hope = p->elements.size() > old_set.size();
+  if (hope)
+    return expandAsNecessary(p, o);
+  else
+  {
+    apf::fail("SPR: patch construction: all hope is lost.");
+    return false;
+  }
+}
+
+static bool buildPatch(Patch* p, apf::CavityOp* o)
+{
+  if (!getInitialPatch(p, o)) return false;
+  if (!expandAsNecessary(p, o)) return false;
+  return true;
+}
+
+struct Samples {
+  Samples():num_points(0) {}
   void allocate(int np, int nc)
   {
     num_points = np;
@@ -28,6 +202,29 @@ struct SprPoints {
   apf::NewArray<apf::Vector3> points;
   apf::NewArray<apf::NewArray<double> > values;
 };
+
+/** @brief get spr point data from element patch
+  * @details assumes constant #IP/element
+  */
+void getSamples(Patch* p, Samples* s)
+{
+  Recovery* r = p->recovery;
+  int np = countPatchPoints(p);
+  int nc = r->f->countComponents();
+  s->allocate(np,nc);
+  std::size_t i = 0;
+  APF_ITERATE(EntitySet, p->elements, it) {
+    apf::MeshElement* me = apf::createMeshElement(r->mesh, *it);
+    for (int l = 0; l < r->points_per_element; ++l) {
+      apf::Vector3 param;
+      apf::getIntPoint(me, r->order, l, param);
+      apf::mapLocalToGlobal(me, param, s->points[i]);
+      apf::getComponents(r->f, *it, l, &(s->values[i][0]));
+      ++i;
+    }
+    apf::destroyMeshElement(me);
+  }
+}
 
 void evalPolynomialTerms(int order,
                          apf::Vector3 const& point,
@@ -67,14 +264,14 @@ void evalPolynomialTerms(int order,
   * @param values (In) data value associated with integration points
   * @param coeffs (Out) coefficients of fit polynomial
   */
-void evalPolynomialCoeffs(int order,
-                          int num_points,
-                          apf::NewArray<apf::Vector3> const& points,
-                          apf::NewArray<double> const& values,
-                          apf::DynamicVector& coeffs)
+void fitPolynomial(int order,
+                   int num_points,
+                   apf::NewArray<apf::Vector3> const& points,
+                   apf::NewArray<double> const& values,
+                   apf::DynamicVector& coeffs)
 {
   int m = num_points;
-  int n = (order+1)*(order+2)*(order+3)/6;
+  int n = countPolynomialTerms(order);
   assert(m >= n);
   apf::DynamicMatrix P(m,n);
   apf::DynamicVector p;
@@ -99,214 +296,75 @@ void evalPolynomialCoeffs(int order,
     coeffs[i] = a[i];
 }
 
-/** @brief get spr point data from element patch
-  * @param f (In) integration point field
-  * @param elements (In) elements in patch
-  * @param spr_points (Out) coord and data at points
-  * @details assumes constant #IP/element
-  */
-void getSprPoints(apf::Field* f,
-                  std::set<apf::MeshEntity*>& elements,
-                  SprPoints& spr_points)
+double evalPolynomial(int order, apf::Vector3& point, apf::DynamicVector& coeffs)
 {
-  apf::Mesh* mesh = apf::getMesh(f);
-  std::set<apf::MeshEntity*>::iterator elem = elements.begin();
-  apf::MeshElement* me = apf::createMeshElement(mesh,*elem);
-  int order = mesh->getShape()->getOrder();
-  int num_points_elem = apf::countIntPoints(me,order);
-  apf::destroyMeshElement(me);
-  int np = elements.size()*num_points_elem;
-  int nc = f->countComponents();
-  spr_points.allocate(np,nc);
-  std::size_t p = 0;
-  for (std::set<apf::MeshEntity*>::iterator it = elements.begin();
-      it != elements.end(); ++it)
-  {
-    apf::MeshElement* mesh_element = apf::createMeshElement(mesh,*it);
-    for (int l=0; l < num_points_elem; ++l)
-    {
-      apf::Vector3 param;
-      apf::getIntPoint(mesh_element,order,l,param);
-      apf::mapLocalToGlobal(mesh_element,param,spr_points.points[p]);
-      apf::getComponents(f,*it,l,&(spr_points.values[p])[0]);
-      ++p;
-    }
-    apf::destroyMeshElement(mesh_element);
-  }
+  apf::DynamicVector terms;
+  evalPolynomialTerms(order, point, terms);
+  return coeffs * terms;
 }
 
 /** @brief run patch recovery
-  * @param f (In) integration point field
-  * @param f_star (In) recovered nodal field
-  * @elements (In) elements in patch
-  * @param entity (In) central entity of patch
   */
-void runSpr(apf::Field* f,
-            apf::Field* f_star,
-            std::set<apf::MeshEntity*>& elements,
-            apf::MeshEntity* entity)
+void runSpr(Patch* p)
 {
-  SprPoints spr_points;
-  getSprPoints(f,elements,spr_points);
-  apf::Mesh* mesh = apf::getMesh(f);
-  int order = mesh->getShape()->getOrder();
-  int num_spr_points = spr_points.num_points;
-  int num_field_components = f->countComponents();
-  int num_nodes_entity = f_star->countNodesOn(entity);
-  apf::NewArray<double> component_values(num_spr_points);
-  apf::NewArray<apf::Vector3> nodal_points(num_nodes_entity);
-  apf::NewArray<apf::NewArray<double> > recovered_values(num_nodes_entity);
-  for (int n=0; n < num_nodes_entity; ++n)
-  {
-    recovered_values[n].allocate(num_field_components);
-    mesh->getPoint(entity,n,nodal_points[n]);
+  Recovery* r = p->recovery;
+  Samples samples;
+  getSamples(p, &samples);
+  int num_components = apf::countComponents(r->f_star);
+  int num_nodes = r->f_star->countNodesOn(p->entity);
+  apf::NewArray<double> values(samples.num_points);
+  apf::NewArray<apf::Vector3> nodal_points(num_nodes);
+  apf::NewArray<apf::NewArray<double> > recovered_values(num_nodes);
+  for (int n = 0; n < num_nodes; ++n) {
+    recovered_values[n].allocate(num_components);
+    p->mesh->getPoint(p->entity, n, nodal_points[n]);
   }
-  for (int i=0; i < num_field_components; ++i)
-  {
-    for (int p=0; p < num_spr_points; ++p)
-      component_values[p] = spr_points.values[p][i];
+  for (int i = 0; i < num_components; ++i) {
+    for (int p = 0; p < samples.num_points; ++p)
+      values[p] = samples.values[p][i];
     apf::DynamicVector coeffs;
-    evalPolynomialCoeffs(order,num_spr_points,
-        spr_points.points,component_values,coeffs);
-    apf::DynamicVector terms;
-    for (int n=0; n < num_nodes_entity; ++n)
-    {
-      evalPolynomialTerms(order,nodal_points[n],terms);
-      recovered_values[n][i] = coeffs*terms;
-    }
+    fitPolynomial(r->order, samples.num_points,
+        samples.points, values, coeffs);
+    for (int n = 0; n < num_nodes; ++n)
+      recovered_values[n][i] = evalPolynomial(r->order, nodal_points[n], coeffs);
   }
-  for (int n=0; n < num_nodes_entity; ++n)
-    setComponents(f_star,entity,n,&(recovered_values[n])[0]);
+  for (int n = 0; n < num_nodes; ++n)
+    setComponents(r->f_star, p->entity, n, &(recovered_values[n][0]));
 }
 
 class PatchOp : public apf::CavityOp
 {
-  public:
-    PatchOp(apf::Field* f_, apf::Field* f_star_):
-      apf::CavityOp(apf::getMesh(f_)),
-      f(f_),
-      f_star(f_star_),
-      num_points(0)
-    {
-    }
-    void reset(apf::MeshEntity* e)
-    {
-      num_points = 0;
-      elements.clear();
-      entity = e;
-    }
-    virtual Outcome setEntity(apf::MeshEntity* e)
-    {
-      if (hasEntity(f_star,e))
-        return SKIP;
-      reset(e);
-      if ( ! buildPatch())
-        return REQUEST;
-      return OK;
-    }
-    virtual void apply()
-    {
-      runSpr(f,f_star,elements,entity);
-    }
-    bool buildPatch()
-    {
-      if (!getInitialPatch()) return false;
-      if (!expandAsNecessary()) return false;
-      return true;
-    }
-    bool getInitialPatch()
-    {
-      if ( ! requestLocality(&entity,1)) return false;
-      apf::DynamicArray<apf::MeshEntity*> adjacent;
-      mesh->getAdjacent(entity,mesh->getDimension(),adjacent);
-      add(adjacent);
-      return true;
-    }
-    bool expandAsNecessary()
-    {
-      int o = mesh->getShape()->getOrder();
-      int num_desired_points = (o+1)*(o+2)*(o+3)/6;
-      if (num_points >= num_desired_points)
-        return true;
-      std::set<apf::MeshEntity*> old_set = elements;
-      int d = mesh->getDimension();
-      for (int shared_dim=d-1; shared_dim >= 0; --shared_dim)
-      {
-        if (!addElementsThatShare(shared_dim,old_set))
-          return false;
-        if (num_points >= num_desired_points)
-          return true;
-      }
-      bool hope = elements.size() > old_set.size();
-      if (hope)
-        return expandAsNecessary();
-      else
-      {
-        apf::fail("SPR: patch construction: all hope is lost.");
-        return false;
-      }
-    }
-    bool addElementsThatShare(int dimension,
-                              std::set<apf::MeshEntity*>& old_set)
-    {
-      std::set<apf::MeshEntity*> bridges;
-      for (std::set<apf::MeshEntity*>::iterator it = old_set.begin();
-          it != old_set.end(); ++it)
-      {
-        apf::Downward down;
-        int nd = mesh->getDownward(*it,dimension,down);
-        for (int i=0; i < nd; ++i)
-          bridges.insert(down[i]);
-      }
-      std::vector<apf::MeshEntity*> 
-        bridge_array(bridges.begin(),bridges.end());
-      bridges.clear();
-      if ( ! requestLocality(&(bridge_array[0]),bridge_array.size()))
-        return false;
-      for (size_t i=0; i < bridge_array.size(); ++i)
-      {
-        apf::Adjacent candidates;
-        mesh->getAdjacent(bridge_array[i],mesh->getDimension(),candidates);
-        add(candidates);
-      }
-      return true;
-    }
-    void add(apf::DynamicArray<apf::MeshEntity*>& es)
-    {
-      for (std::size_t i=0; i < es.getSize(); ++i)
-        add(es[i]);
-    }
-    void add(apf::MeshEntity* e)
-    {
-      if (elements.count(e))
-        return;
-      elements.insert(e);
-      apf::MeshElement* element = apf::createMeshElement(mesh,e);
-      int integration_order = f->getShape()->getOrder();
-      num_points += apf::countIntPoints(element,integration_order);
-      apf::destroyMeshElement(element);
-    }
-    apf::Field* f;
-    apf::Field* f_star;
-    int num_points;
-    std::set<apf::MeshEntity*> elements;
-    apf::MeshEntity* entity;
+public:
+  PatchOp(Recovery* r):
+    apf::CavityOp(r->mesh)
+  {
+    setupPatch(&patch, r);
+  }
+  virtual Outcome setEntity(apf::MeshEntity* e)
+  {
+    if (hasEntity(patch.recovery->f_star, e))
+      return SKIP;
+    startPatch(&patch, e);
+    if ( ! buildPatch(&patch, this))
+      return REQUEST;
+    return OK;
+  }
+  virtual void apply()
+  {
+    runSpr(&patch);
+  }
+  Patch patch;
 };
 
 apf::Field* recoverField(apf::Field* f)
 {
-  std::string name = "spr_";
-  name += getName(f);
-  apf::Mesh* m = apf::getMesh(f);
-  apf::Field* f_star =
-    apf::createFieldOn(m,name.c_str(),apf::getValueType(f));
-  PatchOp op(f,f_star);
+  Recovery recovery;
+  setupRecovery(&recovery, f);
+  PatchOp op(&recovery);
   for (int i=0; i <= 3; ++i)
-  {
-    if (m->getShape()->countNodesOn(i) != 0)
+    if (recovery.mesh->getShape()->countNodesOn(i) != 0)
       op.applyToDimension(i);
-  }
-  return f_star;
+  return recovery.f_star;
 }
 
 }
