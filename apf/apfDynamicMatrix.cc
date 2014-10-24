ac445cff9937a9f4585e189af9de99005f240442
Dan Ibanez <ibaned@rpi.edu>
2014-10-23 20:04:01 -0400

    yet another fix to DynamicMatrix print

--- apf/apfDynamicMatrix.cc
+++ apf/apfDynamicMatrix.cc
@@ -3,7 +3,7 @@
 std::ostream& operator<<(std::ostream& s, apf::DynamicMatrix const& A)
 {
   for (std::size_t i = 0; i < A.getRows(); ++i) {
-    for (std::size_t j = 0; j < A.getRows(); ++j)
+    for (std::size_t j = 0; j < A.getColumns(); ++j)
       s << A(i,j) << ' ';
     s << '\n';
   }
