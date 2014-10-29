402b61ac3238d1937d621fa7be69cfd2d4a37211
Marissa Barbato <barbam@othello.scorec.rpi.edu>
2014-10-29 13:36:42 -0400

    Beginning QR decomposition and added unit qr_test

---
+++ spr/sprQR.cc
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2011 Scientific Computation Research Center
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#include "spr.h"
+
+namespace spr {
+
+void backSubstitution(apf::DynamicMatrix& R,
+                      apf::DynamicVector& b,
+                      apf::DynamicVector& x)
+{
+  
+}
+
+void solveQR(apf::DynamicMatrix& A,
+              apf::DynamicVector& x,
+              apf::DynamicVector& b)
+{
+
+}
+
+}
