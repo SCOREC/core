e2df086351f81781fc09d9b9cd3878d03a45207a
Cameron Smith <smithc11@rpi.edu>
2014-11-20 11:49:41 -0600

    adding test for fixing disconnected componets
    also includes a little cleanup in dcpart.cc

---
+++ test/fixDisconnected.cc
@@ -0,0 +1,31 @@
+#include <apf.h>
+#include <apfMDS.h>
+#include <apfMesh2.h>
+#include <gmi_mesh.h>
+#include <parma.h>
+#include <PCU.h>
+
+int main(int argc, char** argv)
+{
+  assert(argc == 4);
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  if ( argc != 4 ) {
+    if ( !PCU_Comm_Self() )
+      printf("Usage: %s <model> <mesh> <out mesh>\n", argv[0]);
+    MPI_Finalize();
+    exit(EXIT_FAILURE);
+  }
+  gmi_register_mesh();
+  //load model and mesh
+  apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
+  Parma_PrintPtnStats(m, "initial");
+  Parma_ProcessDisconnectedParts(m);
+  Parma_PrintPtnStats(m, "final");
+  m->writeNative(argv[3]);
+  // destroy mds
+  m->destroyNative();
+  apf::destroyMesh(m);
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
