efba5a12ae6ef78755ea225680384d0ec317e3e7
Cameron Smith <smithc11@rpi.edu>
2014-11-26 10:34:42 -0500

    test driver for zoltan balancing

---
+++ test/zbalance.cc
@@ -0,0 +1,29 @@
+#include <apf.h>
+#include <apfMDS.h>
+#include <apfMesh2.h>
+#include <apfZoltan.h>
+#include <gmi_mesh.h>
+#include <parma.h>
+#include <PCU.h>
+
+int main(int argc, char** argv)
+{
+  assert(argc == 4);
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  gmi_register_mesh();
+  //load model and mesh
+  apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
+  apf::MeshTag* weights = Parma_WeighByMemory(m);
+  apf::Balancer* balancer = makeZoltanBalancer(m, apf::GRAPH, apf::REPARTITION);
+  balancer->balance(weights, 1.10);
+  delete balancer;
+  apf::removeTagFromDimension(m, weights, m->getDimension());
+  m->destroyTag(weights);
+  m->writeNative(argv[3]);
+  // destroy mds
+  m->destroyNative();
+  apf::destroyMesh(m);
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
