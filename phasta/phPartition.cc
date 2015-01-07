29bea056c3502a04cf1dafc1bd96e200237d9f26
Michel Rasquin <mrasquin@alcf.anl.gov>
2015-01-07 17:20:08 +0000

    balance vertices after splitting if mesh is not mixed

--- phasta/phPartition.cc
+++ phasta/phPartition.cc
@@ -31,19 +31,92 @@ void split(Input& in, apf::Mesh2* m, void (*runAfter)(apf::Mesh2*))
   apf::splitMdsMesh(m, plan, factor, runAfter);
 }
 
+bool isMixed(apf::Mesh2* m) {
+  int mixed = 0;
+  apf::MeshEntity* e;
+  apf::MeshIterator* it = m->begin(m->getDimension());
+  while ((e = m->iterate(it)))
+    if ( m->getType(e) != apf::Mesh::TET ) {
+      mixed = 1;
+      break;
+    }
+  m->end(it);
+  PCU_Max_Ints(&mixed, 1);
+  return mixed;
+}
+
+void setWeight(apf::Mesh* m, apf::MeshTag* tag, int dim) {
+  double w = 1.0;
+  apf::MeshEntity* e;
+  apf::MeshIterator* it = m->begin(dim);
+  while ((e = m->iterate(it)))
+    m->setDoubleTag(e, tag, &w);
+  m->end(it);
+}
+
+apf::MeshTag* setWeights(apf::Mesh* m) {
+  apf::MeshTag* tag = m->createDoubleTag("parma_weight", 1);
+  setWeight(m, tag, 0);
+  setWeight(m, tag, m->getDimension());
+  return tag;
+}
+
+void clearTags(apf::Mesh* m, apf::MeshTag* t) {
+  apf::removeTagFromDimension(m, t, 0);
+  apf::removeTagFromDimension(m, t, m->getDimension());
+}
+
 void balance(apf::Mesh2* m)
 {
-  Parma_PrintPtnStats(m, "preRefine");
-  apf::MeshTag* weights = Parma_WeighByMemory(m);
-  double tolerance = 1.05;
-  const double step = 0.2;
-  const int verbose = 0;
-  apf::Balancer* balancer = Parma_MakeElmBalancer(m, step, verbose);
-  balancer->balance(weights, tolerance);
-  delete balancer;
-  apf::removeTagFromDimension(m, weights, m->getDimension());
-  m->destroyTag(weights);
-  Parma_PrintPtnStats(m, "postRefine");
+  bool fineStats=true;
+  Parma_PrintPtnStats(m, "preRefine", fineStats);
+  if ( isMixed(m) ) {
+
+    apf::MeshTag* weights = Parma_WeighByMemory(m);
+    double tolerance = 1.05;
+    const double step = 0.2;
+    const int verbose = 0;
+    apf::Balancer* balancer = Parma_MakeElmBalancer(m, step, verbose);
+    balancer->balance(weights, tolerance);
+    delete balancer;
+    apf::removeTagFromDimension(m, weights, m->getDimension());
+    m->destroyTag(weights);
+
+  } else {
+    Parma_PrintPtnStats(m, "postSplit", fineStats);
+    m->writeNative("bz2:postSplit/");
+
+    apf::MeshTag* weights = setWeights(m);
+    const double vtxImbTol = 1.03;
+    const double step = 0.3;
+    const int verbose = 2;
+    const double ignored = 42.42;
+
+    Parma_ProcessDisconnectedParts(m);
+    Parma_PrintPtnStats(m, "post ProcessDisconnectedParts", fineStats);
+
+    apf::Balancer* balancer = Parma_MakeHpsBalancer(m,verbose);
+    balancer->balance(weights, ignored);
+    delete balancer;
+    Parma_PrintPtnStats(m, "post HPS", fineStats);
+
+    for(int i=0; i<3; i++) {
+      balancer = Parma_MakeVtxElmBalancer(m, step, verbose);
+      balancer->balance(weights, vtxImbTol);
+      Parma_PrintPtnStats(m, "post Parma_MakeVtxElmBalancer", fineStats);
+      delete balancer;
+      double vtxImb = Parma_GetWeightedEntImbalance(m, weights, 0);
+      if( vtxImb <= vtxImbTol ) {
+        if( !PCU_Comm_Self() )
+          fprintf(stdout, "STATUS vtx imbalance target %.3f reached\n",
+            vtxImbTol);
+        break;
+      }
+    }
+    clearTags(m, weights);
+    m->destroyTag(weights);
+  }
+//  Parma_PrintPtnStats(m, "postRefine");
 }
 
 }
