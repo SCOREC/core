235b283ab371bae8b03bcf8898a6c4953d80934b
Cameron Smith <smithc11@rpi.edu>
2015-01-07 21:37:48 -0500

    static variables go boom with threads

--- parma/diffMC/parma_vtxBalancer.cc
+++ parma/diffMC/parma_vtxBalancer.cc
@@ -10,23 +10,26 @@
 namespace {
   class VtxBalancer : public parma::Balancer {
     private:
+      int sideTol;
       int getSideTol(parma::Sides* s) {
         double avg = static_cast<double>(s->total());
         PCU_Add_Doubles(&avg, 1);
         avg /= PCU_Comm_Peers();
         int tol = static_cast<int>(avg);
-        if( !PCU_Comm_Self() ) 
+        if( !PCU_Comm_Self() )
           fprintf(stdout, "sideTol %d\n", tol);
         return tol;
       }
     public:
       VtxBalancer(apf::Mesh* m, double f, int v)
-        : Balancer(m, f, v, "vertices") { }
+        : Balancer(m, f, v, "vertices") {
+          parma::Sides* s = parma::makeVtxSides(mesh);
+          sideTol = getSideTol(s);
+      }
       bool runStep(apf::MeshTag* wtag, double tolerance) {
         parma::Sides* s = parma::makeVtxSides(mesh);
-        static int sideTol = getSideTol(s);
         parma::Weights* w = parma::makeEntWeights(mesh, wtag, s, 0);
-        parma::Targets* t = 
+        parma::Targets* t =
           parma::makeWeightSideTargets(s, w, sideTol, factor);
         parma::Selector* sel = parma::makeVtxSelector(mesh, wtag);
         parma::Stepper b(mesh, factor, s, w, t, sel);
@@ -37,7 +40,7 @@ namespace {
 
 apf::Balancer* Parma_MakeVtxBalancer(apf::Mesh* m,
     double stepFactor, int verbosity) {
-  if( !PCU_Comm_Self() && verbosity ) 
+  if( !PCU_Comm_Self() && verbosity )
     fprintf(stdout,"PARMA_STATUS stepFactor %.3f\n", stepFactor);
   return new VtxBalancer(m, stepFactor, verbosity);
 }
