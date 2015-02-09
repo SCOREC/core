c72709d1746ee4e7bd61e39980449998e82ada7a
Cameron Smith <smithc11@rpi.edu>
2015-02-09 13:26:48 -0500

    reset component distance before running dijkstra on it

--- parma/diffMC/parma_dijkstra.cc
+++ parma/diffMC/parma_dijkstra.cc
@@ -5,9 +5,27 @@
 #include "parma_meshaux.h" 
 #include "parma_distQ.h"
 
+/*
+ * Components can have a seperator that may already be distanced.  If the
+ * seperator vertex distances are lower than the distance the current component
+ * would set than the vertices on the other side of the seperator belonging
+ * exclusively to the current component will not be distanced.
+ */
+void resetDist(apf::Mesh* m, parma::DijkstraContains* c, apf::MeshTag* d) {
+  const int far = INT_MAX;
+  apf::MeshEntity* v;
+  apf::MeshIterator* it = m->begin(0);
+  while( (v = m->iterate(it)) ) {
+    if( !c->has(v) ) continue;
+    m->setIntTag(v,d,&far);
+  }
+  m->end(it);
+}
+
 namespace parma {
   void dijkstra(apf::Mesh* m, DijkstraContains* c,
       apf::MeshEntity* src, apf::MeshTag* d) {
+    resetDist(m,c,d);
     parma::DistanceQueue<parma::Less> pq(m);
     int zero = 0;
     m->setIntTag(src, d, &zero);
