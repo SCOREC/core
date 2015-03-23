40af6a59acfedc391b88920f41e91a1ab0338e59
Brian Granzow <brian.granzow@gmail.com>
2015-03-23 10:24:31 -0400

    adding file for elasticity error computation

---
+++ dwr/elasticity/dwrElasticityErr.cc
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2011 Scientific Computation Research Center
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#include <iostream>
+#include <apf.h>
+#include "dwr.h"
+#include "dwrStress.h"
+#include "dwrStrain.h"
+#include "dwrElasticityProblem.h"
+
+namespace dwr {
+
+void estimateError(ElasticityProblem* p)
+{
+}
+
+}
