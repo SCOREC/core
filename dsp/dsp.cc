1d7eda96a45596ad11bbd3c143d27e514da673ff
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-04-04 13:41:50 -0400

    using apf::axpy in dsp::tryToDisplace

--- dsp/dsp.cc
+++ dsp/dsp.cc
@@ -1,17 +1,19 @@
 #include "dsp.h"
+#include <apf.h>
 
 namespace dsp {
 
 bool tryToDisplace(apf::Mesh2* m, apf::Field* df)
 {
   double f = 1;
-  displaceMesh(m, df, f);
+  apf::axpy(f, df, m->getCoordinateField());
   if (0 == verifyVolumes(m, false))
     return true;
   do {
     f /= 2;
-    displaceMesh(m, df, -f);
+    apf::axpy(-f, df, m->getCoordinateField());
   } while (0 != verifyVolumes(m, false));
+  apf::axpy(-f, df, df);
   return false;
 }
 
