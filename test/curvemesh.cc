d34b8c9fdb444d6c5e8d72c27027f8f2b3970925
Daniel Zaide <dan.zaide@gmail.com>
2015-05-07 14:32:11 -0400

    updated curved testing

--- test/curvemesh.cc
+++ test/curvemesh.cc
@@ -6,9 +6,10 @@
 #include <PCU.h>
 #include <SimUtil.h>
 #include <maCurveMesh.h>
-#include <apfField.h>
 #include <apfShape.h>
 #include <apfDynamicMatrix.h>
+#include <apfField.h>
+#include <maAdapt.h>
 
 void testInterpolationError(ma::Mesh* m, int entityDim,
     apf::DynamicVector & errors){
@@ -19,21 +20,21 @@ void testInterpolationError(ma::Mesh* m, int entityDim,
     errors[id] = 0.0;
     ma::Model* g = m->toModel(e);
     if (m->getModelType(g) == m->getDimension()) continue;
-    ma::Vector samplePt, maxPt;
-    errors[id++]= ma::interpolationError(m,e,21,samplePt,maxPt);
+    errors[id++]= ma::interpolationError(m,e,11);
   }
   m->end(it);
 }
 void testElementSize(ma::Mesh* m)
 {
   int dim = m->getDimension();
-  for( int d = 1; d < dim; ++d){
+  double sizes[3] = {0,0,0};
+  for( int d = 1; d <= dim; ++d){
     ma::Iterator* it = m->begin(d);
     ma::Entity* e;
     while ((e = m->iterate(it))) {
       apf::MeshElement* me = apf::createMeshElement(m,e);
       double v = apf::measure(me);
-      if(v < 0){
+      if(v < 0 && d < 3){
         std::stringstream ss;
         ss << "error: " << apf::Mesh::typeName[m->getType(e)]
            << " size " << v
@@ -42,10 +43,15 @@ void testElementSize(ma::Mesh* m)
         fprintf(stderr, "%s", s.c_str());
         abort();
       }
+      sizes[d-1] += v;
+      if(d == 3) printf("Volume %f\n",v);
       apf::destroyMeshElement(me);
     }
     m->end(it);
   }
+  printf("Total sizes for order %d %f %f %f\n",
+    m->getCoordinateField()->getShape()->getOrder(),
+    sizes[0],sizes[1],sizes[2]);
 }
 int main(int argc, char** argv)
 {
@@ -60,6 +66,7 @@ int main(int argc, char** argv)
   gmi_register_mesh();
   gmi_register_sim();
   apf::Mesh2* m = apf::loadMdsMesh(modelFile,meshFile);
+  testElementSize(m);
   int ne = m->count(1);
   int nf = m->count(2);
   m->destroyNative();
@@ -68,9 +75,12 @@ int main(int argc, char** argv)
   apf::DynamicMatrix edgeErrors(ne,6);
   apf::DynamicMatrix faceErrors(nf,6);
 
-  for(int order = 1; order < 7; ++order){
+  for(int order = 1; order <= 6; ++order){
     apf::Mesh2* m2 = apf::loadMdsMesh(modelFile,meshFile);
-    ma::curveMeshToBezier(m2,order);
+    ma::Input* in = ma::configureIdentity(m2);
+    ma::Adapt* adapt = new ma::Adapt(in);
+    ma::BezierCurver bc(adapt,order);
+    bc.run();
     testElementSize(m2);
     apf::DynamicVector ee(ne);
     apf::DynamicVector fe(nf);
@@ -78,8 +88,9 @@ int main(int argc, char** argv)
     testInterpolationError(m2,2,fe);
     edgeErrors.setColumn(order-1,ee);
     faceErrors.setColumn(order-1,fe);
-    ma::writePointSet(m2,1,21,outFile);
-    ma::writePointSet(m2,2,21,outFile);
+    //ma::writePointSet(m2,1,11,outFile);
+    //ma::writePointSet(m2,2,11,outFile);
+    ma::writePointSet(m2,3,11,outFile);
     m2->destroyNative();
     apf::destroyMesh(m2);
   }
@@ -96,7 +107,7 @@ int main(int argc, char** argv)
     faceErrors.getRow(id,fe);
     if(fe[0] > 1e-12)
       printf("face %d %.4e %.4e %.4e %.4e %.4e %.4e \n",
-          id,fe[0],fe[1],fe[2],fe[3],fe[4],fe[5]);
+         id,fe[0],fe[1],fe[2],fe[3],fe[4],fe[5]);
   }
 
   PCU_Comm_Free();
