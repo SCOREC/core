010140b075c5a7cf49a488bbe89d0e54f0f9d1e9
Fan <Fan@cii-wl-323.dynamic.rpi.edu>
2015-05-13 15:53:20 -0400

    fix infinite loop in semispring

--- dsp/dspSmoothers.cc
+++ dsp/dspSmoothers.cc
@@ -184,66 +184,73 @@ namespace dsp {
       apf::Vector3 d;
       
       //---------------------------------------------------------
-      int n_mb = 0; int n_in = 0; int n_fb = 0;
+      //data structure
+      int mb_0 = 0; int in_0 = 0; int fb_0 = 0;
+      vector < apf::MeshEntity* > V_total;
+      vector < apf::Vector3 > D_total;
+      apf::Numbering* numbers = apf::createNumbering(m, "my_numbers", m->getShape(), 1);
       
       //iterate vertex to count the number of each type of vertex
+      int id = 0;
       it = m->begin(0);
       while ((v = m->iterate(it))) {
-        me = m->toModel(v);
-        if (moving.count(me)) n_mb++;
-        else if (fixed.count(me)) n_fb++;
-        else n_in++;
-      }
-      m->end(it);
-      cout << "Number of vertices of each type (mb,fb,in): " << n_mb << ' ' << n_fb << ' ' << n_in << ' ' << endl;
-      //----------------------------------------------------------
-      int MB_begin = 0;            int MB_end = n_mb - 1;
-      int IN_begin = n_mb;         int IN_end = n_mb + n_in - 1;
-      int FB_begin = n_mb + n_in;  //int FB_end = n_mb + n_in + n_fb - 1;
-      int ithMB = 0; int ithIN =0; int ithFB = 0;
-      
-      vector < apf::MeshEntity* > V_total(n_mb + n_in + n_fb);
-      vector < apf::Vector3 > D_total(n_mb + n_in + n_fb);
-      apf::Numbering* numbers = numberOwnedDimension(m, "my_numbering", 0);
-      
-      //iterate to store vertices and points
-      it = m->begin(0);
-      while ((v = m->iterate(it))) {
-        apf::getVector(df, v, 0, d);
         me = m->toModel(v);
         if (moving.count(me)) {
-          V_total[MB_begin + ithMB] = v;
-          D_total[MB_begin + ithMB] = d;
-          apf::number(numbers, v, 0, 0, MB_begin + ithMB);
-          ithMB++;
+          V_total.push_back(v);
+          apf::getVector(df, v, 0, d);
+          D_total.push_back(d);
+          apf::number(numbers, v, 0, 0, id);
+          id++;
         }
-        else if (fixed.count(me)) {
-          V_total[FB_begin + ithFB] = v;
-          D_total[FB_begin + ithFB] = d;
-          apf::number(numbers, v, 0, 0, FB_begin + ithFB);
-          ithFB++;
+      }
+      m->end(it);
+      
+      in_0 = id;
+      
+      it = m->begin(0);
+      while ((v = m->iterate(it))) {
+        me = m->toModel(v);
+        if ((!fixed.count(me))&(!moving.count(me))) {
+          V_total.push_back(v);
+          apf::getVector(df, v, 0, d);
+          D_total.push_back(d);
+          apf::number(numbers, v, 0, 0, id);
+          id++;
         }
-        else {
-          V_total[IN_begin + ithIN] = v;
-          D_total[IN_begin + ithIN] = d;
-          // apf::number(numbers, v, 0, 0, IN_begin + ithIN);
-          ithIN++;
+      }
+      m->end(it);
+      
+      fb_0 = id;
+      
+      it = m->begin(0);
+      while ((v = m->iterate(it))) {
+        me = m->toModel(v);
+        if (fixed.count(me)) {
+          V_total.push_back(v);
+          apf::getVector(df, v, 0, d);
+          D_total.push_back(d);
+          apf::number(numbers, v, 0, 0, id);
+          id++;
         }
       }
       m->end(it);
       
       //----------------------------------------------------------
-      ithIN = 0; int zero = 0; int one = 1;
+      //reordering
+      //data structure
+      //tag = 1, indicates this is in queue before AND this is a interior vertex
+      apf::MeshTag* in_queue_tag = m->createIntTag("In_Queue_Tag", 1);
+      int zero = 0; int one = 1; int tag;
       apf::Adjacent adj;
+      
+      id = in_0;
       
       //make a queue and put all MB vertex in it
       queue < apf::MeshEntity* > q;
-      for (int i = MB_begin ; i < MB_end + 1 ; i++) {
+      for (int i = mb_0 ; i < in_0 ; i++) {
         q.push(V_total[i]);
       }
       
-      //tag = 1, indicates this is in queue before AND this is a interior vertex
-      apf::MeshTag* in_queue_tag = m->createIntTag("In_Queue_Tag", 1);
       it = m->begin(0);
       while ((v = m->iterate(it))) {
         m->setIntTag(v, in_queue_tag, &zero);
@@ -261,7 +268,6 @@ namespace dsp {
           apf::MeshEntity* adj_v = apf::getEdgeVertOppositeVert(m, adj[i], v);
           apf::ModelEntity* adj_v_me = m->toModel(adj_v);
           if ((!moving.count(adj_v_me)) & (!fixed.count(adj_v_me))) {
-            int tag;
             m->getIntTag(adj_v, in_queue_tag, &tag);
             if (tag == 0) {
               q.push(adj_v);
@@ -270,15 +276,14 @@ namespace dsp {
           }
         }
         if ((!moving.count(me)) & (!fixed.count(me))) {
-          V_total[IN_begin + ithIN] = v;
-          D_total[IN_begin + ithIN] = d;
-          apf::number(numbers, v, 0, 0, IN_begin + ithIN);
-          ithIN++;
+          V_total[id] = v;
+          D_total[id] = d;
+          apf::number(numbers, v, 0, 0, id);
+          id++;
         }
         q.pop();
       }
-      
-      cout << "Reordering part is done! " << endl;
+
       //----------------------------------------------------------
       double tol = 1.0E-5; //tolerance
       apf::Downward down;
@@ -286,13 +291,14 @@ namespace dsp {
       double cos_squ; double length_squ;
       vector < apf::Vector3 > tet_OP(3);
       vector < apf::Vector3 > tet_DP(3);
+      apf::Vector3 D_temp;
       
       // semi-torsional spring: stiffness = 1/length^2 + sum(1/sin(theta)^2)
       // check max, stop until it is less the tolerance
       double max = 1.0; int loop_times = 0;
       while (max > tol) {
         max = 0.0;
-        for (int i = IN_begin ; i < IN_end + 1 ; i++) {
+        for (int i = in_0 ; i < fb_0 ; i++) {
           apf::Vector3 force_sum = apf::Vector3(0, 0, 0);
           double stiffness_sum = 0.0;
           m->getAdjacent(V_total[i], 3, adj);
@@ -355,13 +361,17 @@ namespace dsp {
           D_total[i][0] = force_sum[0]/stiffness_sum;
           D_total[i][1] = force_sum[1]/stiffness_sum;
           D_total[i][2] = force_sum[2]/stiffness_sum;
-          double temp_max = sqrt(pow(D_total[i][0], 2) + pow(D_total[i][1], 2) + pow(D_total[i][2], 2));
+          
+          apf::getVector(df, V_total[i], 0, D_temp);
+          double d1 = D_total[i][0] - D_temp[0];
+          double d2 = D_total[i][1] - D_temp[1];
+          double d3 = D_total[i][2] - D_temp[2];
+          apf::setVector(df, V_total[i], 0, D_total[i]);
+          
+          double temp_max = sqrt(d1 * d1 + d2 * d2 + d3 * d3);
           if (max < temp_max) {
             max = temp_max;
           }
-          //update IN
-          apf::setVector(df, V_total[i], 0, D_total[i]);
-          std::cout << "Current Max is " << max << '\n';
         }
         loop_times++;
       }
