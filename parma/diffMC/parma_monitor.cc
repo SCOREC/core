f14fd325c395f2e37f674013936b135adb95c0ed
Cameron Smith <smithc11@rpi.edu>
2015-06-12 20:16:07 -0400

    Revert "debug stmts in the monitor"
    This reverts commit 213b1dbe4380388169f83498ed4f5ffacb44dc1a.

--- parma/diffMC/parma_monitor.cc
+++ parma/diffMC/parma_monitor.cc
@@ -1,7 +1,6 @@
 #include "parma_monitor.h"
 #include <assert.h>
 #include <stdlib.h>
-#include <PCU.h>
 namespace {
   const unsigned int order = 2;
   const double c[]={-3./2, 2., -1./2}; // 2nd order
@@ -35,10 +34,7 @@ namespace parma {
     assert( full() );
     double s = 0;
     for(unsigned int i=0; i<length(); i++)
-      PCU_Debug_Print("Slope i %d get %f c %f\n", s);
-    for(unsigned int i=0; i<length(); i++)
       s += c[i]*get(i);
-    PCU_Debug_Print("Slope s %f\n", s);
     return s;
   }
   Average::Average(unsigned int l) : CircBuffer(l) {}
@@ -46,7 +42,6 @@ namespace parma {
     double a = 0;
     for(unsigned int i=0; i<size(); i++)
       a += get(i);
-    PCU_Debug_Print("Average a %f size %d\n", a, size());
     return a /= size();
   }
 }
