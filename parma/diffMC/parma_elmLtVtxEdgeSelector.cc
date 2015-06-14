b6e7fffd1ef9955bed5b6b1cbb7a6f9e4b6310e3
Cameron Smith <smithc11@rpi.edu>
2015-06-14 05:58:22 -0400

    vtx and edge cancellation for elm selection

--- parma/diffMC/parma_elmLtVtxEdgeSelector.cc
+++ parma/diffMC/parma_elmLtVtxEdgeSelector.cc
@@ -7,6 +7,15 @@
 #include "parma_convert.h"
 
 namespace {
+  struct dtwo {
+    dtwo(double x, double y)
+      : a(x), b(y) {}
+    dtwo()
+      : a(0), b(0) {}
+    double a;
+    double b;
+  };
+  typedef std::map<int, dtwo> Midd;
   typedef std::set<apf::MeshEntity*> SetEnt;
 
   class ElmLtVtxEdgeSelector : public parma::VtxSelector {
@@ -23,7 +32,7 @@ namespace {
         double planW = 0;
         for(int max=2; max <= 12; max+=2)
           planW += select(tgts, plan, planW, max);
-        parma::Mid* capacity = trim(tgts,plan);
+        Midd* capacity = trim(tgts,plan);
         cancel(&plan, capacity);
         return plan;
       }
@@ -34,9 +43,20 @@ namespace {
         mesh->getAdjacent(e, 0, adjVtx);
         APF_ITERATE(apf::Adjacent, adjVtx, v) {
           apf::Parts res;
-         mesh->getResidence(*v,res);
+          mesh->getResidence(*v,res);
           if( !res.count(dest) ) //not on the boundary with dest
             s.insert(*v);
+        }
+      }
+
+      void insertInteriorEdges(apf::MeshEntity* e, int dest, SetEnt& s) {
+        apf::Adjacent adjEdges;
+        mesh->getAdjacent(e, 1, adjEdges);
+        APF_ITERATE(apf::Adjacent, adjEdges, edge) {
+          apf::Parts res;
+          mesh->getResidence(*edge,res);
+          if( !res.count(dest) ) //not on the boundary with dest
+            s.insert(*edge);
         }
       }
 
@@ -47,7 +67,7 @@ namespace {
         return w;
       }
 
-      void cancel(apf::Migration** plan, parma::Mid* capacity) {
+      void cancel(apf::Migration** plan, Midd* capacity) {
         typedef std::pair<apf::MeshEntity*, int> PairEntInt;
         apf::Migration* planA = *plan;
         std::vector<PairEntInt > keep; //temporary plan container
@@ -60,14 +80,19 @@ namespace {
         //adding its vertices to the weight does not exceed the peer's vtx
         //weight capacity, capacity[dest].
         std::map<int,SetEnt > peerVerts;
+        std::map<int,SetEnt > peerEdges;
         for(int i=0; i < planA->count(); i++) {
            apf::MeshEntity* e = planA->get(i);
            int dest = planA->sending(e);
            SetEnt vset = peerVerts[dest];
+           SetEnt eset = peerEdges[dest];
            insertInteriorVerts(e, dest, vset);
-           if( weight(vset) <= (*capacity)[dest] ) {
+           insertInteriorEdges(e, dest, eset);
+           if( weight(vset) <= (*capacity)[dest].a &&
+               weight(eset) <= (*capacity)[dest].b ) {
              keep.push_back(PairEntInt(e,dest));
              peerVerts[dest] = vset;
+             peerEdges[dest] = eset;
            }
         }
         delete capacity;
@@ -77,17 +102,29 @@ namespace {
           (*plan)->send(keep[i].first, keep[i].second);
       }
 
-      typedef std::pair<int,double> Migr;
+      struct Migr {
+        Migr(int i, double v, double e) :
+          id(i), vw(v), ew(e) {}
+        int id;
+        double vw;
+        double ew;
+      };
       struct CompareMigr {
+        //sort by vertex weight... because
         bool operator()(const Migr& a, const Migr& b) const {
-          if( a.second < b.second )
+          if( a.vw < b.vw )
             return true;
           else
             return false;
         }
       };
+      typedef std::set<Migr,CompareMigr> MigrComm;
 
-      parma::Mid* trim(parma::Targets*, apf::Migration* plan) {
+      //return  map<int neighbor, pair<double vtxW, double edgeW> > where
+      //  neighbor is a neighbor's part id
+      //  vtxW is the vtx weight capacity of neighbor
+      //  edgeW is the edge weight capacity of neighbor
+      Midd* trim(parma::Targets*, apf::Migration* plan) {
         //compute the weight of the vertices and edges being sent to each peer
         typedef std::map<int,SetEnt > PeerEntSet;
         PeerEntSet peerVerts;
@@ -96,50 +133,66 @@ namespace {
           apf::MeshEntity* elm = plan->get(i);
           const int dest = plan->sending(elm);
           insertInteriorVerts(elm, dest, peerVerts[dest]);
-          //insertInteriorEdges(elm, dest, peerVerts[dest]); //HERE
+          insertInteriorEdges(elm, dest, peerEdges[dest]);
         }
 
-        parma::Mid sendingVtx;
-        APF_ITERATE(PeerEntSet, peerVerts, pv)
-          sendingVtx[pv->first] = weight(pv->second);
-
-        typedef std::set<Migr,CompareMigr> MigrComm;
-
+        //send vtx and edge weight
         PCU_Comm_Begin();
-        APF_ITERATE(parma::Mid, sendingVtx, s)
-          PCU_COMM_PACK(s->first, s->second);
+        APF_ITERATE(PeerEntSet, peerVerts, pv) {
+          const int dest = pv->first;
+          double vw = weight(peerVerts[dest]);
+          PCU_COMM_PACK(dest, vw);
+          double ew = weight(peerEdges[dest]);
+          PCU_COMM_PACK(dest, ew);
+        }
         PCU_Comm_Send();
-
-        MigrComm incoming; //map<sender,weight sending>
-        double w;
+        MigrComm incoming;
+        double vw, ew;
         while (PCU_Comm_Listen()) {
-          PCU_COMM_UNPACK(w);
-          incoming.insert(Migr(PCU_Comm_Sender(),w));
+          PCU_COMM_UNPACK(vw);
+          PCU_COMM_UNPACK(ew);
+          incoming.insert(Migr(PCU_Comm_Sender(),vw,ew)); //Migr ctor does not exist
         }
 
-        double selfW = parma::getWeight(mesh,wtag,0);
-        parma::Mid accept;
-        double totW = selfW;
+        Midd accept;
+        double totW[2] = {
+          parma::getWeight(mesh,wtag,0), //vtx
+          parma::getWeight(mesh,wtag,1)  //edge
+        };
         APF_ITERATE(MigrComm, incoming, in) {
-          double avail = maxVtx - totW;
-          if( avail > 0 )
-            if( in->second <= avail )
-              accept[in->first] = in->second;
-            else
-              accept[in->first] = avail;
-          else
-            accept[in->first] = 0;
-          totW += accept[in->first];
+          const double inW[2] = {
+            (*in).vw,  //vtx
+            (*in).ew   //edge
+          };
+          double avail[2] = {
+            maxVtx - totW[0], //vtx
+            maxEdge - totW[1] //edge
+          };
+          const int nbor = (*in).id;
+          if( avail[0] > 0 && avail[1] > 0 ) {
+            if( inW[0] <= avail[0] && inW[1] <= avail[1] ) {
+              accept[nbor] = dtwo(inW[0],inW[1]);
+            } else {
+              accept[nbor] = dtwo(avail[0],avail[1]);
+            }
+          } else {
+            accept[nbor] = dtwo(0,0);
+          }
+          totW[0] += accept[nbor].a;
+          totW[1] += accept[nbor].b;
         }
 
         PCU_Comm_Begin();
-        APF_ITERATE(parma::Mid, accept, a)
-          PCU_COMM_PACK(a->first, a->second);
+        APF_ITERATE(Midd, accept, acc) {
+          PCU_COMM_PACK(acc->first, acc->second.a);
+          PCU_COMM_PACK(acc->first, acc->second.b);
+        }
         PCU_Comm_Send();
-        parma::Mid* capacity = new parma::Mid;
-        double outw;
+        Midd* capacity = new Midd;
+        dtwo outw;
         while (PCU_Comm_Listen()) {
-          PCU_COMM_UNPACK(outw);
+          PCU_COMM_UNPACK(outw.a);
+          PCU_COMM_UNPACK(outw.b);
           (*capacity)[PCU_Comm_Sender()] = outw;
         }
         return capacity;
