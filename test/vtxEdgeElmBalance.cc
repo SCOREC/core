3d72cff02a2bd100b30e2cea1ada943c1e197c9f
Cameron Smith <smithc11@rpi.edu>
2015-06-26 14:58:16 -0400

    only apply weights to vtx, edge, and elm
    disable fine stats

--- test/vtxEdgeElmBalance.cc
+++ test/vtxEdgeElmBalance.cc
@@ -17,17 +17,19 @@ namespace {
   apf::MeshTag* setWeights(apf::Mesh* m, double edgeWeight) {
     double weights[4] = {1.,edgeWeight,1.,1.};
     weights[1] = (!PCU_Comm_Self()) ? edgeWeight : 1.0;
-    fprintf(stderr, "STATUS %d entity weights %.2f %.2f %.2f %.2f\n",
-        PCU_Comm_Self(), weights[0], weights[1], weights[2], weights[3]);
+    const int d = m->getDimension();
     apf::MeshTag* tag = m->createDoubleTag("parma_weight", 1);
-    for(int i=0; i<=m->getDimension(); i++)
-      setWeight(m, tag, i, weights[i]);
+    setWeight(m, tag, 0, weights[0]);
+    setWeight(m, tag, 1, weights[1]);
+    setWeight(m, tag, d, weights[d]);
     return tag;
   }
 
   void clearTags(apf::Mesh* m, apf::MeshTag* t) {
-    for(int i=0; i<=m->getDimension(); i++)
-      apf::removeTagFromDimension(m, t, i);
+    const int d = m->getDimension();
+    apf::removeTagFromDimension(m, t, 0);
+    apf::removeTagFromDimension(m, t, 1);
+    apf::removeTagFromDimension(m, t, d);
   }
 }
 
@@ -48,14 +50,14 @@ int main(int argc, char** argv)
   double targetImb = atof(argv[5]);
   apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
   apf::MeshTag* weights = setWeights(m,atof(argv[4]));
-  Parma_PrintWeightedPtnStats(m, weights, "initial",true);
+  Parma_PrintWeightedPtnStats(m, weights, "initial");
   const double step = 0.5; const int verbose = 1;
   apf::Balancer* balancer = Parma_MakeVtxEdgeElmBalancer(m, step, verbose);
   if( !PCU_Comm_Self() )
     fprintf(stderr, "STATUS target imbalance %.2f\n", targetImb);
   balancer->balance(weights, targetImb);
   delete balancer;
-  Parma_PrintWeightedPtnStats(m, weights, "final",true);
+  Parma_PrintWeightedPtnStats(m, weights, "final");
   clearTags(m, weights);
   m->destroyTag(weights);
   m->writeNative(argv[3]);
