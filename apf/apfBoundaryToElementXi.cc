909829a3d63ef71e8193ee4a8358704a42c3c48e
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-07-15 12:12:26 -0400

    implement apf::boundaryToElementXi

---
+++ apf/apfBoundaryToElementXi.cc
@@ -0,0 +1,56 @@
+#include "apf.h"
+#include "apfMesh.h"
+#include "apfShape.h"
+
+namespace apf {
+
+/* at some point we may want to work these tables
+ * into a more central place along with the
+ * shape functions...
+ */
+static Vector3 const tet_vert_xi[4] = {
+  Vector3(0,0,0),
+  Vector3(1,0,0),
+  Vector3(0,1,0),
+  Vector3(0,0,1),
+};
+static Vector3 const* const elem_vert_xi[Mesh::TYPES] = {
+  0, /* vertex */
+  0, /* edge */
+  0, /* triangle */
+  0, /* quad */
+  tet_vert_xi,
+  0, /* hex */
+  0, /* prism */
+  0  /* pyramid */
+};
+
+Vector3 boundaryToElementXi(
+    Mesh* m,
+    MeshEntity* boundary,
+    MeshEntity* element,
+    Vector3 const& xi)
+{
+  Downward bv;
+  int nbv = m->getDownward(boundary, 0, bv);
+  int bt = m->getType(boundary);
+  Downward ev;
+  int nev = m->getDownward(element, 0, ev);
+  int et = m->getType(element);
+  /* the boundary parametric space
+   * and parent parametric space
+   * are linear functions of one another
+   */
+  EntityShape* shape = apf::getLagrange(1)->getEntityShape(bt);
+  NewArray<double> shape_vals;
+  shape->getValues(m, boundary, xi, shape_vals);
+  Vector3 exi(0,0,0);
+  for (int i = 0; i < nbv; ++i) {
+    int evi = findIn(ev, nev, bv[i]);
+    assert(evi >= 0);
+    exi += elem_vert_xi[et][evi] * shape_vals[i];
+  }
+  return exi;
+}
+
+}
