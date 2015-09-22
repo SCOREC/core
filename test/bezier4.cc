a10f96454d96b044686be192f38724a87d3849a2
samanthajlee2 <slee.bhs@gmail.com>
2015-09-22 09:29:17 -0400

    Added another bezier test

---
+++ test/bezier4.cc
@@ -0,0 +1,251 @@
+#include <crv.h>
+#include <crvBezier.h>
+#include <gmi_analytic.h>
+#include <gmi_null.h>
+#include <apfMDS.h>
+#include <apf.h>
+#include <PCU.h>
+#include <apfDynamicMatrix.h>
+#include <cassert>
+#include <cstdlib>
+
+/*
+  * Sam test to see what's going on
+*/
+
+/* this test file contains tests for
+ * a curved 2D surface mesh
+ * and a 3D Planar tetrahedron
+ *
+ * The basic idea is generate a mesh,
+ * curve it, test it, and destroy it,
+ * and do this for all orders
+ */
+
+/*
+ * This analytic surface is a "pringle",
+ * defined on [0,1] in R^2 as z = ((2x-1)^2-(2y-1)^2)*exp(xy)
+ * matlab >> ezsurf('((2*x-1)^2-(2*y-1)^2)*exp(x*y)',[0,1],[0,1])
+ */
+void vert0(double const p[2], double x[3], void*)
+{
+  (void)p;
+  (void)x;
+}
+// edges go counter clockwise
+void edge0(double const p[2], double x[3], void*)
+{
+  x[0] = p[0];
+  x[1] = 0.;
+  x[2] = (2.*p[0]-1.)*(2.*p[0]-1.)-1.;
+}
+void edge1(double const p[2], double x[3], void*)
+{
+  x[0] = 1;
+  x[1] = p[0];
+  x[2] = (1.-(2.*p[0]-1.)*(2.*p[0]-1.))*std::exp(p[0]);
+}
+void edge2(double const p[2], double x[3], void*)
+{
+  double u = 1.-p[0];
+  x[0] = u;
+  x[1] = 1.;
+  x[2] = ((2.*u-1.)*(2.*u-1.)-1.)*std::exp(u);
+}
+void edge3(double const p[2], double x[3], void*)
+{
+  double v = 1.-p[0];
+  x[0] = 0;
+  x[1] = v;
+  x[2] = 1.-(2.*v-1.)*(2.*v-1.);
+}
+void face0(double const p[2], double x[3], void*)
+{
+  x[0] = p[0];
+  x[1] = p[1];
+  x[2] = ((2.*p[0]-1.)*(2.*p[0]-1.)
+      -(2.*p[1]-1.)*(2.*p[1]-1.))
+      *std::exp(p[0]*p[1]);
+}
+void reparam_zero(double const from[2], double to[2], void*)
+{
+  (void)from;
+  to[0] = 0;
+  to[1] = 0;
+}
+void reparam_one(double const from[2], double to[2], void*)
+{
+  (void)from;
+  to[0] = 1;
+  to[1] = 0;
+}
+
+agm_bdry add_bdry(gmi_model* m, gmi_ent* e)
+{
+  return agm_add_bdry(gmi_analytic_topo(m), agm_from_gmi(e));
+}
+
+agm_use add_adj(gmi_model* m, agm_bdry b, int tag)
+{
+  agm* topo = gmi_analytic_topo(m);
+  int dim = agm_dim_from_type(agm_bounds(topo, b).type);
+  gmi_ent* de = gmi_find(m, dim - 1, tag);
+  return agm_add_use(topo, b, agm_from_gmi(de));
+}
+
+void make_edge_topo(gmi_model* m, gmi_ent* e, int v0tag, int v1tag)
+{
+  agm_bdry b = add_bdry(m, e);
+  agm_use u0 = add_adj(m, b, v0tag);
+  gmi_add_analytic_reparam(m, u0, reparam_zero, 0);
+  agm_use u1 = add_adj(m, b, v1tag);
+  gmi_add_analytic_reparam(m, u1, reparam_one, 0);
+}
+
+gmi_model* makeModel()
+{
+  gmi_model* model = gmi_make_analytic();
+  int edPer = 0;
+  double edRan[2] = {0, 1};
+  for(int i = 0; i < 4; ++i)
+    gmi_add_analytic(model, 0, i, vert0, NULL,NULL,NULL);
+  gmi_ent* eds[4];
+  eds[0] = gmi_add_analytic(model, 1, 0, edge0, &edPer, &edRan, 0);
+  eds[1] = gmi_add_analytic(model, 1, 1, edge1, &edPer, &edRan, 0);
+  eds[2] = gmi_add_analytic(model, 1, 2, edge2, &edPer, &edRan, 0);
+  eds[3] = gmi_add_analytic(model, 1, 3, edge3, &edPer, &edRan, 0);
+  for(int i = 0; i < 4; ++i)
+    make_edge_topo(model, eds[i], i, (i+1) % 4);
+  int faPer[2] = {0, 0};
+  double faRan[2][2] = {{0,1},{0,1}};
+  gmi_add_analytic(model, 2, 0, face0, faPer, faRan, 0);
+
+  return model;
+}
+
+apf::Mesh2* createMesh2D()
+{
+  gmi_model* model = makeModel();
+  apf::Mesh2* m = apf::makeEmptyMdsMesh(model, 2, true);
+  apf::MeshEntity* v[4];
+  apf::Vector3 points2D[4] =
+  {apf::Vector3(0,0,0),
+      apf::Vector3(1,0,0),
+      apf::Vector3(1,1,0),
+      apf::Vector3(0,1,0)};
+
+  for (int i = 0; i < 4; ++i){
+    v[i] = m->createVertex(m->findModelEntity(0,i),points2D[i],points2D[i]);
+  }
+  for (int i = 0; i < 4; ++i){
+    apf::ModelEntity* edge = m->findModelEntity(1,i);
+    apf::MeshEntity* ved[2] = {v[i],v[(i+1) % 4]};
+    apf::buildElement(m, edge, apf::Mesh::EDGE, ved);
+  }
+
+  apf::MeshEntity* ved[2] = {v[0],v[2]};
+  apf::buildElement(m, m->findModelEntity(2,0), apf::Mesh::EDGE, ved);
+
+  apf::MeshEntity* vf0[3] = {v[0],v[1],v[2]};
+  apf::buildElement(m, m->findModelEntity(2,0), apf::Mesh::TRIANGLE, vf0);
+  apf::MeshEntity* vf1[3] = {v[0],v[2],v[3]};
+  apf::buildElement(m, m->findModelEntity(2,0), apf::Mesh::TRIANGLE, vf1);
+  m->acceptChanges();
+  m->verify();
+  return m;
+}
+
+apf::Mesh2* createMesh3D()
+{
+  gmi_model* model = gmi_load(".null");
+  apf::Mesh2* m = apf::makeEmptyMdsMesh(model, 3, true);
+
+  apf::Vector3 points3D[4] =
+  {apf::Vector3(0,0,0),
+      apf::Vector3(1,0,0),
+      apf::Vector3(0,1,0),
+      apf::Vector3(0,0,1)};
+
+  apf::buildOneElement(m,0,apf::Mesh::TET,points3D);
+
+  apf::deriveMdsModel(m);
+  m->acceptChanges();
+  m->verify();
+  return m;
+}
+
+void test2D()
+{
+  // test all orders for all blending orders
+  for(int order = 1; order <= 6; ++order){
+    apf::Mesh2* m = createMesh2D();
+    crv::BezierCurver bc(m,order,0);
+    bc.run();
+    crv::writeControlPointVtuFiles(m,"curved2D");
+    crv::writeCurvedVtuFiles(m,apf::Mesh::TRIANGLE,10*order,"curved2D");
+    m->destroyNative();
+    apf::destroyMesh(m);
+  }
+}
+
+/* Tests Full Bezier tetrahedra, 4th order has one central node */
+void test3D()
+{
+  gmi_register_null();
+
+  for(int order = 1; order <= 4; ++order){
+    apf::Mesh2* m = createMesh3D();
+    apf::changeMeshShape(m, crv::getBezier(order),true);
+    crv::setBlendingOrder(0);
+    apf::FieldShape* fs = m->getShape();
+    crv::BezierCurver bc(m,4,0);
+    // go downward, and convert interpolating to control points
+    for(int d = 2; d >= 1; --d){
+      int n = (d == 2)? (order+1)*(order+2)/2 : order+1;
+      int ne = fs->countNodesOn(d);
+      apf::NewArray<double> c;
+      crv::getTransformationCoefficients(order,d,c);
+      apf::MeshEntity* e;
+      apf::MeshIterator* it = m->begin(d);
+      while ((e = m->iterate(it))) {
+        bc.convertInterpolationPoints(e,n,ne,c);
+      }
+      m->end(it);
+    }
+    m->acceptChanges();
+    apf::MeshIterator* it = m->begin(1);
+    apf::MeshEntity* e;
+    int num = 0;
+    while ((e = m->iterate(it))){
+      // do stuff here
+      for(int i = 0; i < m->getShape()->countNodesOn(apf::Mesh::EDGE); ++i){
+      apf::Vector3 point;
+      
+        m->getPoint(e,i,point);
+        std::cout << "edge " << num << " " << i << " " << point << std::endl;
+        point = point*-1.1;
+      m->setPoint(e,i,point);
+    }
+      num++;
+
+    }
+    m->end(it);
+    // write the field
+    crv::writeCurvedVtuFiles(m,apf::Mesh::EDGE,2,"curved");
+    crv::writeCurvedVtuFiles(m,apf::Mesh::TRIANGLE,10,"curved");
+    crv::writeCurvedVtuFiles(m,apf::Mesh::TET,order,"curved");
+    crv::writeControlPointVtuFiles(m,"curved");
+    m->destroyNative();
+    apf::destroyMesh(m);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  test2D();
+  test3D();
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
