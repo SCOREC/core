e991ab12d67067fbed4165226c3a7fbe61cf0413
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-01 16:41:29 -0400

    adding a simple test for 3x3 mth::decomposeQR

---
+++ test/qr.cc
@@ -0,0 +1,17 @@
+#include "mthQR.h"
+#include <iostream>
+
+int main()
+{
+  mth::Matrix3x3<double> a(
+      1, 1, 0,
+      0, 1, 0,
+      0, 0, 0);
+  std::cout << "A\n" << a;
+  mth::Matrix<double,3,3> q;
+  mth::Matrix<double,3,3> r;
+  decomposeQR(a, q, r);
+  std::cout << "Q\n" << q;
+  std::cout << "R\n" << r;
+  std::cout << "Q*R\n" << q * r;
+}
