f84e5d95b7425d4e592e62565b09e37bd8a6b6f7
Saurabh Tendulkar <saurabh@simmetrix.com>
2015-10-01 15:47:02 -0400

    Added a SIM_DEBUG variable to allow Simmetrix debug libraries to be used.

--- cmake/TPLs/FindTPLSimParasolid.cmake
+++ cmake/TPLs/FindTPLSimParasolid.cmake
@@ -2,5 +2,5 @@ INCLUDE(TribitsTplDeclareLibraries)
 
 TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( SimParasolid
   REQUIRED_HEADERS SimParasolidKrnl.h
-  REQUIRED_LIBS_NAMES SimParasolid270 pskernel
+  REQUIRED_LIBS_NAMES SimParasolid270${SIM_DEBUG} pskernel
 )
