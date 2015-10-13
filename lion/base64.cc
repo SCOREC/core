a9272573400ad2ff36aca5bd6430eb95040efbf7
Paul Revere <reverp@catan.scorec.rpi.edu>
2015-10-13 09:47:50 -0400

    Added files for Base64 encoding and decoding

---
+++ lion/base64.cc
@@ -0,0 +1,223 @@
+#include <string>
+#include <stdlib.h>
+
+#include "base64.h"
+
+// ===========================================================================
+// =========================================================================== 
+//  Implementation of base64.h. documentation of functions found in base64.h 
+// ===========================================================================
+// ===========================================================================
+
+static const std::string base64EncodeTable =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                              "abcdefghijklmnopqrstuvwxyz"
+                                              "0123456789+/=";
+
+// ===========================================================================
+
+static const unsigned char base64DecodeTable[256] =
+{ 
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  // ====================================
+  0xFF,0xFF,0xFF,0x3E,0xFF,0xFF,0xFF,0x3F,
+  0x34,0x35,0x36,0x37,0x38,0x39,0x3A,0x3B,
+  0x3C,0x3D,0xFF,0xFF,0xFF,0x00,0xFF,0xFF,
+  0xFF,0x00,0x01,0x02,0x03,0x04,0x05,0x06,
+  0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,
+  0x0F,0x10,0x11,0x12,0x13,0x14,0x15,0x16,
+  0x17,0x18,0x19,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,0x20,
+  0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x28,
+  0x29,0x2A,0x2B,0x2C,0x2D,0x2E,0x2F,0x30,
+  0x31,0x32,0x33,0xFF,0xFF,0xFF,0xFF,0xFF,
+  // ====================================
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+  0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
+};
+
+// ===========================================================================
+
+char getBase64Char (int index) {
+
+  return base64EncodeTable[index];
+}
+
+// ===========================================================================
+
+char getDecodedBase64Char (unsigned char c) {
+
+  return base64DecodeTable[c];
+}
+
+// ===========================================================================
+
+
+std::string base64Encode3Bytes (char* bytes) {
+
+  std::string encoded;
+
+  //first 6 bits of byte1
+  encoded += getBase64Char((bytes[0] >> 2) & 0x3F); 
+
+  //last 2 bits of byte1 and the first 4 of byte2
+  encoded += getBase64Char( ((bytes[0] << 4) & 0x30) | ((bytes[1] >> 4) & 0x0F));
+
+  //last 4 bits of byte2 and the first 2 of byte3
+  encoded += getBase64Char( ((bytes[1] << 2) & 0x3C) | ((bytes[2] >> 6) & 0x03));
+
+  //last 6 of byte3
+  encoded += getBase64Char( bytes[2] & 0x3F ); 
+
+  return encoded;
+}
+
+// ===========================================================================
+
+std::string base64Encode2Bytes (char* bytes) {
+
+  std::string encoded;
+
+  //first 6 bits of byte1
+  encoded += getBase64Char((bytes[0] >> 2) & 0x3F); 
+
+  //last 2 bits of byte1 combined with the the first 4 of byte2
+  encoded += getBase64Char( ((bytes[0] << 4) & 0x30) | ((bytes[1] >> 4) & 0x0F));
+
+  //last 4 bits of byte2 combined with the first 2 of byte3
+  encoded += getBase64Char( ((bytes[1] << 2) & 0x3C) );
+
+  //add a padding char since there is no byte 3
+  encoded += '=';
+
+  return encoded;
+}
+
+// ===========================================================================
+
+std::string base64Encode1Byte (char byte) {
+
+  std::string encoded;
+
+  //first 6 bits of the byte
+  encoded += getBase64Char((byte >> 2) & 0x3F); 
+
+  //last 2 bits of the byte
+  encoded += getBase64Char((byte << 4) & 0x30);
+
+  //add the padding chars since there is no byte 2 or 3
+  encoded += "==";
+
+  return encoded;
+}
+
+// ===========================================================================
+
+
+std::string base64Encode (const char* input,
+                          const unsigned long len ) {
+
+  std::string encoded;
+  char* inputChars = (char*)malloc(3*sizeof(char));
+  unsigned int index = 0;
+
+  //encode all the string in 3 byte sections, this loop won't encode the last
+  // 1 or 2 bytes if the len is not divisible by 3
+  for ( ; index <= len - 3; index += 3 ) {
+    inputChars[0] = input[index];
+    inputChars[1] = input[index+1];
+    inputChars[2] = input[index+2];
+    encoded += base64Encode3Bytes(inputChars);
+  }
+
+  //encode the last 2 bytes if there are 2 bytes left at the end of the string
+  // i.e. if (len % 3 == 2)
+  if ( len - index == 2 ) {
+    inputChars[0] = input[index];
+    inputChars[1] = input[index+1];
+    encoded += base64Encode2Bytes(inputChars);
+    index += 2;
+  }
+
+  //encode the last 1 byte if there is one byte left at the end of the string
+  // i.e. if (len % 3 == 1)
+  if ( len - index == 1 ) {
+    char inputChar = input[index];
+    encoded += base64Encode1Byte(inputChar);
+    index++;
+  }
+
+  free(inputChars);
+
+  return encoded;
+}
+
+// ===========================================================================
+
+std::string base64Decode4Bytes (char* bytes){
+  
+  std::string decoded;
+
+  //get the Base64 codes for the input chars
+  char* base64Chars = (char*)malloc(4*sizeof(char));
+  base64Chars[0] = getDecodedBase64Char(bytes[0]);
+  base64Chars[1] = getDecodedBase64Char(bytes[1]);
+  base64Chars[2] = getDecodedBase64Char(bytes[2]);
+  base64Chars[3] = getDecodedBase64Char(bytes[3]);
+
+  //check that the input was all Base64 chars, getDecodedBase64Char returns
+  // 0xFF if the char isn't a Base64 char
+  if ( base64Chars[0] == 0xFF || base64Chars[1] == 0xFF
+      || base64Chars[2] == 0xFF || base64Chars[3] == 0xFF ) {
+    return ""; //input had non-Base64 chars
+  }
+
+  //convert the 6 bits of char1 and the first 2 bits of char2 to plaintext
+  decoded += (char)(((base64Chars[0] << 2) & 0xFC) | ((base64Chars[1] >> 4) & 0x03));
+  
+  //check if there's padding instead of a Base64 char at the 2 index, decode if
+  // there isn't
+  if ( bytes[2] != '=' ) {
+    decoded += (char)(((base64Chars[1] << 4) & 0xF0) | ((base64Chars[2] >> 2) & 0x0F));
+  }
+
+  //check if there's padding instead of a Base64 char at the 2 and 3 indicies, 
+  // decode if there isn't
+  if ( bytes[2] != '=' && bytes[3] != '=') {
+    decoded += (char)(((base64Chars[2] << 6) & 0xC0) | (base64Chars[3] & 0x3F));
+  }
+
+  free(base64Chars);
+
+  return decoded;
+}
+
+// ===========================================================================
+
+// TODO: PR4: Implement
+std::string base64Decode (std::string encoded) {
+
+  std::string decoded;
+  
+  return decoded;
+}
+
+// ===========================================================================
+// ===========================================================================
---
+++ lion/base64.h
@@ -0,0 +1,128 @@
+#ifndef BASE64_H
+#define BASE64_H
+
+// ===========================================================================
+// ===========================================================================
+
+/*
+Function getBase64Char:
+  gets the char at the index given to it from the encode table
+*/
+char getBase64Char (int index);
+
+// ===========================================================================
+
+/*
+Function getDecodedBase64Char:
+  gets the Base64 value of an ASCII char sent to it. Uses the  
+  base64DecodeTable. We have to do this because our Base64 string is
+  represented as ASCII chars which have different codes than Base64.
+*/
+char getDecodedBase64Char (unsigned char c);
+
+// ===========================================================================
+
+/*
+Function base64Encode3Bytes:
+  Encodes 3 bytes sent to it into Base64
+
+Arguments:
+  char* bytes - char of array of bytes to be encoded - the first three positions
+                will be accessed so len(bytes) must be >= 3
+
+Returns:
+  std::string - string of length 4 of the encoded bytes
+*/
+std::string base64Encode3Bytes (char* bytes);
+
+// ===========================================================================
+
+/*
+Function base64Encode2Bytes:
+  Encodes 2 bytes sent to it into Base64. Used a when the end of a string is 
+  being encoded and the string is of a length not divisible by 3. Since Base64
+  encodes in 3 byte sections a padding char, '=', must be added to take the 
+  place of the missing byte.
+
+Arguments:
+  char* bytes - char array of bytes to be encoded, the first two positions
+                will be accessed so len(bytes) must be >= 2
+
+Returns:  
+  std::string - string of length 4 of the encoded bytes including padding
+*/
+std::string base64Encode2Bytes (char* bytes);
+
+// ===========================================================================
+
+/*
+Function base64Encode1Byte:
+  Encodes a single byte sent to it into Base64. Used when the end of a string is
+  being encode and the tsring is of a length not divisible by 3. Since Base64
+  encodes in 3 byte sections, two padding chars, "==", must be added to take the
+  place of the missing bits.
+
+Arguments:
+  char byte - char of the byte to be encoded
+
+Returns:
+  std::string - string of length 4 of the encoded bytes including padding 
+*/
+std::string base64Encode1Byte (char byte);
+
+// ===========================================================================
+
+/*
+Function base64Encode:
+  Encodes a series of bytes specified by the arguments into a string of Base64
+  chars that will represent them
+
+Arguments:
+  char* input - pointer to start of byte string to be encoded
+  long len - number of bytes to be encoded
+
+Returns:
+  std::string - Base64 encoded string of the input bytes
+*/
+std::string base64Encode (const char* input,
+                          const unsigned long len );
+
+// ===========================================================================
+
+/*
+Function base64Decode4Bytes:
+  Decodes 4 bytes send to it from Base64 to plaintext,
+
+Arguments:
+  char* bytes - char array of bytes to be decoded into plaintext, the first
+                four positions will be accessed so len(bytes) must be >= 4
+
+Returns:
+  std::string - string of length 1, 2, or 3 based on the amount of padding in
+                the chars sent to the function. len(string) will be 1 if the
+                input has two padding chars ('='*2). len(string) will be 2 if
+                the input has one padding char ('='). len(string) will be 3
+                if the input has no padding chars.
+*/
+std::string base64Decode4Bytes (char* bytes);
+
+// ===========================================================================
+
+/*
+TODO: PR4: Implement
+
+Function base64Decode:
+  Decodes a string of Base64 chars to the bytes they represent
+
+Arguments:
+  std::string encoded - Base64 encoded string
+
+Returns:
+  std::string - decoded string of bytes from the encoded data
+*/
+std::string base64Decode (std::string encoded);
+
+// ===========================================================================
+// ===========================================================================
+
+#endif
