4d8f91f0a586350cddb3af7bf130eeb75d1fc661
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-20 12:02:27 -0400

    fixed legitimate resource leak, thanks Coverity !

--- ma/maMatchedCollapse.h
+++ ma/maMatchedCollapse.h
@@ -35,6 +35,7 @@ struct Rebuilds : public RebuildCallback {
 struct MatchedCollapse
 {
   MatchedCollapse(Adapt* a);
+  ~MatchedCollapse();
   void setEdge(Entity* e);
   bool requestLocality(apf::CavityOp* o);
   void setEdges();
