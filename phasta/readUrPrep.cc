10f4dcb2d769f0ceaa4167833d8cbf42cd51cf23
Cameron Smith <smithc11@rpi.edu>
2015-11-30 21:23:23 -0500

    kitchen api test driver

---
+++ phasta/readUrPrep.cc
@@ -0,0 +1,36 @@
+#include <apf.h>
+#include <apfMesh.h>
+#include <apfMDS.h>
+#include <gmi_mesh.h>
+#include <PCU.h>
+#include <phKitchen.h>
+
+namespace {
+  void freeMesh(apf::Mesh* m) {
+    m->destroyNative();
+    apf::destroyMesh(m);
+  }
+  static FILE* openfile_read(ph::Input&, const char* path) {
+    return fopen(path, "r");
+  }
+}
+
+int main(int argc, char** argv)
+{
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  PCU_Protect();
+  gmi_register_mesh();
+  apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
+  ph::Input ctrl;
+  ctrl.load(argv[3]);
+  ctrl.openfile_read = openfile_read;
+  ctrl.buildMapping = 0; //can't map new vertices from UR
+  kitchen::readAndAttachFields(ctrl,m);
+  kitchen::uniformRefinement(ctrl,m);
+  kitchen::preprocess(m,ctrl);
+  freeMesh(m);
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
+
