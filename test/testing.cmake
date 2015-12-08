fd7bf7b9e7979b5d32ed95ec9081a393258b51ee
Cameron Smith <smithc11@rpi.edu>
2015-12-08 13:35:43 -0500

    chef test using simmetrix attributes

--- test/testing.cmake
+++ test/testing.cmake
@@ -333,6 +333,8 @@ if (PCU_COMPRESS)
   add_test(NAME chef8
     COMMAND diff -r -x .svn out_mesh/ good_mesh/
     WORKING_DIRECTORY ${MDIR})
+  set(MDIR ${MESHES}/phasta/simModelAndAttributes)
+  cook(chef9 ${CMAKE_CURRENT_BINARY_DIR}/chef 2 1 ${MDIR})
   set(MDIR ${MESHES}/phasta/4-1-Chef-Tet-Part/4-4-Chef-Part-ts20/run)
   add_test(NAME chefReadUrPrep
     COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4
