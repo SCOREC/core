308fc804bf27e8a72bdf1f2b0145a7784c4f3427
Thenerdstation <keeper6928@gmail.com>
2015-12-27 14:17:13 -0500

    Fixed C++11 dependancy for AD

--- mth/mthAD.h
+++ mth/mthAD.h
@@ -2,7 +2,7 @@
 #define MTH_AD_H
 
 #include <cmath>
-
+#include <iostream>
 #include "canArray.h"
 namespace mth {
 
@@ -11,20 +11,22 @@ namespace mth {
 template <class T=double, unsigned int N=0>
 class AD
 {
-  protected:
+  public:
     /** \brief the variable value */
-    T x_;
+    double x_;
     /** \brief the derivative array */
     T dx_[N];
-  public:
+
     /** \brief the number of derivatives */
     enum { degree = N };
     /** \brief default constructor */
     AD():x_(0.) {zero();}
-    /** \brief construct from a double */
-    AD(double x):x_(x) {zero();}
+    /** \brief default constructor from a double */
+    explicit AD(double x):x_(x) {zero();}
     /** \brief copy constructor */
     AD(AD<T, N> const& other) {copy(other);}
+    template <class B>
+    AD(AD<B, N> const& other) {copy(other);}
     /** \brief get the size of the derivative array */
     unsigned int size() const {return N;}
     /** \brief set as the ith variable of N */
@@ -42,8 +44,10 @@ class AD
     T& dx(unsigned int i) {return dx_[i];}
     /** \brief get the ith derivative value (immutable) */
     const T& dx(unsigned int i) const {return dx_[i];}
-    /** \breif resize for static AD (no-op) */
+    /** \brief resize for static AD (no-op) */
     inline void resize(unsigned int i) {(void)i;}
+    /** \brief type conversion from AD to double*/
+    operator double() const {return double(x_);}
     /** \brief assignment to a double */
     AD<T, N>& operator=(double other)
     {
@@ -57,6 +61,13 @@ class AD
       copy(other);
       return *this;
     }
+
+    template <class B>
+    AD<T, N>& operator=(AD<B, N> const& other)
+    {
+      copy(other);
+      return *this;
+    }
     /** \brief addition assignment with a double */
     AD<T, N>& operator+=(double other)
     {
@@ -64,7 +75,8 @@ class AD
       return *this;
     }
     /** \brief addition assignment with another AD variable */
-    AD<T, N>& operator+=(AD<T, N> const& other)
+    template< class B>
+    AD<T, N>& operator+=(AD<B, N> const& other)
     {
       x_ += other.x_;
       for (unsigned int i=0; i < N; ++i)
@@ -78,7 +90,8 @@ class AD
       return *this;
     }
     /** \brief subtraction assignment with another AD variable */
-    AD<T, N>& operator-=(AD<T, N> const& other)
+    template< class B>
+    AD<T, N>& operator-=(AD<B, N> const& other)
     {
       x_ -= other.x_;
       for (unsigned int i=0; i < N; ++i)
@@ -94,11 +107,12 @@ class AD
       return *this;
     }
     /** \brief multiplication assignment with another AD variable */
-    AD<T, N>& operator*=(AD<T, N> const& other)
+    template<class B>
+    AD<T, N>& operator*=(AD<B, N> const& other)
     {
       x_ *= other.x_;
       for (unsigned int i=0; i < N; ++i)
-        dx_[i] = dx_[i]*other.x_ + x_*other.dx_[i];
+        dx_[i] = dx_[i]*other + x_*other.dx_[i];
     }
     /** \brief division assignment with a double */
     AD<T, N>& operator/=(double other)
@@ -109,11 +123,12 @@ class AD
       return *this;
     }
     /** \brief division assignment with another AD variable */
-    AD<T, N>& operator/=(AD<T, N> const& other)
+    template<class B>
+    AD<T, N>& operator/=(AD<B, N> const& other)
     {
       x_ /= other.x_;
       for (unsigned int i=0; i < N; ++i)
-        dx_[i] = (dx_[i]*other.x_ - x_*other.dx_[i]) / (other.x_*other.x_);
+        dx_[i] = (dx_[i]*other - other*other.dx_[i]) / (other*other);
     }
   private:
     void zero()
@@ -121,30 +136,33 @@ class AD
       for (unsigned int i=0; i < N; ++i)
         dx_[i] = 0.;
     }
-    void copy(AD<T, N> const& other)
+
+    template <class B>
+    void copy(AD<B, N> const& other)
     {
-      x_ = other.x_;
+      this->x_ = other.x_;
       for (unsigned int i=0; i < N; ++i)
-        dx_[i] = other.dx_[i];
+        this->dx_[i] = (T)other.dx_[i];
     }
 };
 /** \brief forward automatic differentiation variable with dynamic variable array */
-template <>
-class AD<double,0>
+
+template <class T>
+class AD<T,0>
 {
-  protected:
+  public:
     /** \brief the variable value */
     double x_;
     /** \brief the dynamic derivative array*/
-    can::Array<double> dx_;
-  public:
+    can::Array<T> dx_;
     const static double _zero_;
     /** \brief default constructor */
     AD():x_(0), dx_() {zero();}
     /** \brief constructs from a double*/
-    AD(double val):x_(val), dx_() {zero();}
+    explicit AD(double val):x_(val), dx_() {zero();}
     /** \brief constructs from other AD*/
-    AD(AD<double, 0> const& other) {zero(); copy(other);}
+    template<class B>
+    AD(AD<B, 0> const& other) {zero(); copy(other);}
     unsigned size() { return dx_.size();}
     unsigned size() const { return dx_.size();}
     void diff(unsigned int i, unsigned int n)
@@ -158,7 +176,7 @@ class AD<double,0>
     /** \brief get the value of the variable (immutable) */
     const double& val() const {return x_;}
     /** \brief get the ith derivative value (mutable) */
-    double& dx(unsigned int i)
+    T& dx(unsigned int i)
     {
       if(i >= size())
       {
@@ -170,33 +188,36 @@ class AD<double,0>
       return dx_[i];
     }
     /** \brief get the ith deriative value (immutable) */
-    const double& dx(unsigned int i) const {
+    const T dx(unsigned int i) const {
       if(i >= size())
-        return _zero_;
+        return T(_zero_);
       return dx_[i];
     }
+    /** \brief type conversion from AD to double */
+    operator double() const {return double(x_);}
     /** \brief assignment to a double */
-    AD<double, 0>& operator=(double other)
+    AD<T, 0>& operator=(double other)
     {
       x_ = other;
       zero();
       return *this;
     }
     /** \brief assignment to another AD variable */
-    AD<double, 0>& operator=(AD const& other)
+    template<class B>
+    AD<T, 0>& operator=(AD<B, 0> const& other)
     {
       resize(other.size());
       copy(other);
       return *this;
     }
     /** \brief addition assignment with a double */
-    AD<double, 0>& operator+=(double other)
+    AD<T, 0>& operator+=(double other)
     {
       x_ += other;
       return *this;
     }
     /** \brief addition assignment with another AD variable */
-    AD<double, 0>& operator+=(AD<double, 0> const& other)
+    AD<T, 0>& operator+=(AD<T, 0> const& other)
     {
       resize(other.size());
       x_ += other.x_;
@@ -205,22 +226,23 @@ class AD<double,0>
       return *this;
     }
     /** \brief subtraction assignment with a double */
-    AD<double, 0>& operator-=(double other)
+    AD<T, 0>& operator-=(double other)
     {
       x_ -= other;
       return *this;
     }
     /** \brief subtraction assignment with another AD variable */
-    AD<double, 0>& operator-=(AD<double, 0> const& other)
+    AD<T, 0>& operator-=(AD<T, 0> const& other)
     {
-      resize(other.size());
+      if (other.size() > size())
+        resize(other.size());
       x_ -= other.x_;
       for (unsigned int i=0; i < size(); ++i)
         dx_[i] -= other.dx_[i];
       return *this;
     }
     /** \brief multiplication assignment with a double */
-    AD<double, 0>& operator*=(double other)
+    AD<T, 0>& operator*=(double other)
     {
       x_ *= other;
       for (unsigned int i=0; i < size(); ++i)
@@ -228,7 +250,7 @@ class AD<double,0>
       return *this;
     }
     /** \brief multiplication assignment with another AD variable */
-    AD<double, 0>& operator*=(AD<double, 0> const& other)
+    AD<T, 0>& operator*=(AD<T, 0> const& other)
     {
       resize(other.size());
       x_ *= other.x_;
@@ -237,7 +259,7 @@ class AD<double,0>
       return *this;
     }
     /** \brief division assignment with a double */
-    AD<double, 0>& operator/=(double other)
+    AD<T, 0>& operator/=(double other)
     {
       x_ /= other;
       for (unsigned int i=0; i < size(); ++i)
@@ -245,7 +267,7 @@ class AD<double,0>
       return *this;
     }
     /** \brief division assignment with another AD variable */
-    AD<double, 0>& operator/=(AD<double, 0> const& other)
+    AD<T, 0>& operator/=(AD<T, 0> const& other)
     {
       resize(other.size());
       x_ /= other.x_;
@@ -263,16 +285,19 @@ class AD<double,0>
       for (unsigned int i=0; i < size(); ++i)
         dx_[i] = 0.;
     }
-    void copy(AD<double, 0> const& other)
+    template<class B> //templating allows for different derivative sizes
+    void copy(AD<B, 0> const& other)
     {
       if(size() != other.size())
         dx_.resize(other.size());
       x_ = other.x_;
       for (unsigned int i=0; i < size(); ++i)
-        dx_[i] = other.dx_[i];
+        dx_[i] = (T)other.dx_[i];
     }
 };
-const double mth::AD<double, 0>::_zero_ = 0;
+
+template<class T>
+const double mth::AD<T, 0>::_zero_ = 0.;
 /**********************
   * UNARY OPERATIONS *
 ***********************/
@@ -283,7 +308,7 @@ AD<T, N> operator-(AD<T, N> const& A)
 {
   AD<T, N> tmp;
   tmp.resize(A.size());
-  tmp -= A;
+  tmp  = AD<T, N>(-1.) * A;
   return tmp;
 }
 
@@ -298,7 +323,7 @@ AD<T, N> operator+(double L, AD<T, N> const& R)
   AD<T, N> tmp;
   tmp.resize(R.size());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = R.dx(i);
+    tmp.dx(i) = T(R.dx(i));
   tmp.val() = L + R.val();
   return tmp;
 }
@@ -310,20 +335,19 @@ AD<T, N> operator+(AD<T, N> const& L, double R)
   AD<T, N> tmp;
   tmp.resize(L.size());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = L.dx(i);
+    tmp.dx(i) = T(L.dx(i));
   tmp.val() = L.val() + R;
   return tmp;
 }
 /** \brief binary addition between two AD variables */
-
-template <class T, unsigned int N>
-AD<T, N> operator+(AD<T, N> const& L, AD<T, N> const& R)
+template <class T, class B, unsigned int N>
+AD<T, N> operator+(AD<T, N> const& L, AD<B, N> const& R)
 {
   AD<T, N> tmp;
   unsigned int max = L.size() > R.size() ? L.size() : R.size();
   tmp.resize(max);
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = L.dx(i) + R.dx(i);
+    tmp.dx(i) = T(L.dx(i) + R.dx(i));
   tmp.val() = L.val() + R.val();
   return tmp;
 }
@@ -335,7 +359,7 @@ AD<T, N> operator-(double L, AD<T, N> const& R)
   AD<T, N> tmp;
   tmp.resize(R.size());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = -R.dx(i);
+    tmp.dx(i) = T(-R.dx(i));
   tmp.val() = L - R.val();
   return tmp;
 }
@@ -347,20 +371,20 @@ AD<T, N> operator-(AD<T, N> const& L, double R)
   AD<T, N> tmp;
   tmp.resize(L.size());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = L.dx(i);
+    tmp.dx(i) = T(L.dx(i));
   tmp.val() = L.val() - R;
   return tmp;
 }
 
 /** \brief binary subtraction between two AD variables */
-template <class T, unsigned int N>
-AD<T, N> operator-(AD<T, N> const& L, AD<T, N> const& R)
+template <class T, class B, unsigned int N>
+AD<T, N> operator-(AD<T, N> const& L, AD<B, N> const& R)
 {
   AD<T, N> tmp;
   unsigned int max = L.size() > R.size() ? L.size() : R.size();
   tmp.resize(max);
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = L.dx(i) - R.dx(i);
+    tmp.dx(i) = T(L.dx(i) - R.dx(i));
   tmp.val() = L.val() - R.val();
   return tmp;
 }
@@ -372,7 +396,7 @@ AD<T, N> operator*(double L, AD<T, N> const& R)
   AD<T, N> tmp;
   tmp.resize(R.size());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = L * R.dx(i);
+    tmp.dx(i) = T(L * R.dx(i));
   tmp.val() = L * R.val();
   return tmp;
 }
@@ -384,21 +408,20 @@ AD<T, N> operator*(AD<T, N> const& L, double R)
   AD<T, N> tmp;
   tmp.resize(L.size());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = L.dx(i) * R;
+    tmp.dx(i) = T(L.dx(i) * R);
   tmp.val() = L.val() * R;
   return tmp;
 }
 
-
 /** \brief binary multiplication between two AD variables */
-template <class T, unsigned int N>
-AD<T, N> operator*(AD<T, N> const& L, AD<T, N> const& R)
+template <class T, class B, unsigned int N>
+AD<T, N> operator*(AD<T, N> const& L, AD<B, N> const& R)
 {
   AD<T, N> tmp;
   unsigned int max = L.size() > R.size() ? L.size() : R.size();
   tmp.resize(max);
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = L.dx(i) * R.val() + L.val() * R.dx(i);
+    tmp.dx(i) = T(L.dx(i) * R + L * R.dx(i));
   tmp.val() = L.val() * R.val();
   return tmp;
 }
@@ -409,8 +432,9 @@ AD<T, N> operator/(double L, AD<T, N> const& R)
 {
   AD<T, N> tmp;
   tmp.resize(R.size());
-  for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = ( -L*R.dx(i) ) / (R.val()*R.val());
+  T R_tmp = R; //Recursive R, used to prevent infinite recurrsion.
+  for (unsigned int i = 0; i < R.size(); i++)
+    tmp.dx(i) = T(-L * R.dx(i) * (1. / R_tmp) * (1. / R_tmp));
   tmp.val() = L / R.val();
   return tmp;
 }
@@ -422,20 +446,20 @@ AD<T, N> operator/(AD<T, N> const& L, double R)
   AD<T, N> tmp;
   tmp.resize(L.size());
   for (unsigned int i=0; i < L.size(); ++i)
-    tmp.dx(i) = L.dx(i) / R;
+    tmp.dx(i) = T(L.dx(i) / R);
   tmp.val() = L.val() / R;
   return tmp;
 }
 
 /** \brief binary division between two AD variables */
-template <class T, unsigned int N>
-AD<T, N> operator/(AD<T, N> const& L, AD<T, N> const& R)
+template <class T,  class B, unsigned int N>
+AD<T, N> operator/(AD<B, N> const& L, AD<T, N> const& R)
 {
   AD<T, N> tmp;
   unsigned int max = L.size() > R.size() ? L.size() : R.size();
   tmp.resize(max);
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = (L.dx(i) * R.val() - L.val() * R.dx(i) ) / (R.val() * R.val());
+    tmp.dx(i) = T(((L.dx(i) * R) - (L * R.dx(i))) * (1. / R) * (1. / R));
   tmp.val() = L.val() / R.val();
   return tmp;
 }
@@ -444,16 +468,29 @@ AD<T, N> operator/(AD<T, N> const& L, AD<T, N> const& R)
   * FANCY FUNCIONS *
 *********************/
 
+/** \brief wrapper to standard exp function */
+double exp(double x)
+{
+  return std::exp(x);
+}
+
 /** \brief exponent of an AD variable */
 template <class T, unsigned int N>
 AD<T, N> exp(AD<T, N> const& A)
 {
   AD<T, N> tmp;
   tmp.resize(A.size());
+  T A_tmp = A;
   tmp.val() = std::exp(A.val());
   for (unsigned int i=0; i < A.size(); ++i)
-    tmp.dx(i) = A.dx(i) * std::exp(A.val());
+    tmp.dx(i) = T(A.dx(i) * exp(A_tmp));
   return tmp;
+}
+
+/** \brief wrapper for stander log function */
+double log(double A)
+{
+  return std::log(A);
 }
 
 /** \brief logarithm of an AD variable */
@@ -462,12 +499,18 @@ AD<T, N> log(AD<T, N> const& A)
 {
   AD<T, N> tmp;
   tmp.resize(A.size());
+  T A_tmp = A;
   tmp.val() = std::log(A.val());
-  for (unsigned int i=0; i < N; ++i)
-    tmp.dx(i) = A.dx(i) / A.val();
+  for (unsigned int i=0; i < A.size(); ++i)
+    tmp.dx(i) = T(A.dx(i) / A_tmp);
   return tmp;
 }
 
+/** \brief wrapper to standard pow function */
+double pow(double A, double e)
+{
+  return std::pow(A, e);
+}
 
 /** \brief AD variable raised to an integer power */
 template <class T, unsigned int N>
@@ -475,9 +518,10 @@ AD<T, N> pow(AD<T, N> const& A, const int e)
 {
   AD<T, N> tmp;
   tmp.resize(A.size());
+  T A_tmp = A;
   tmp.val() = std::pow(A.val(), e);
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = e*A.dx(i)*std::pow(A.val(), (double)e-1.);
+    tmp.dx(i) = T(AD<T, N>(e)*A.dx(i)*pow(A_tmp, (double)e-1.));
   return tmp;
 }
 
@@ -487,12 +531,12 @@ AD<T, N> pow(AD<T, N> const& A, const double e)
 {
   AD<T, N> tmp;
   tmp.resize(A.size());
-  tmp.val() = std::pow(A.val(), e);
+  T A_tmp = A; 
+  tmp.val() = pow(A.val(), e);
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = e*A.dx(i)*std::pow(A.val(), e-1.);
+    tmp.dx(i) = T(e*A.dx(i)*pow(A_tmp, e - 1.));
   return tmp;
 }
-
 
 /** \brief integer raised to an AD power */
 template <class T, unsigned int N>
@@ -500,9 +544,10 @@ AD<T, N> pow(const int base, AD<T, N> const& A)
 {
   AD<T, N> tmp;
   tmp.resize(A.size());
+  T A_tmp = A;
   tmp.val() = std::pow((double)base, A.val());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = std::log((double)base) * std::pow((double)base, A.val())  * A.dx(i);
+    tmp.dx(i) = T(std::log((double)base) * pow((double)base, A_tmp)  * A.dx(i));
   return tmp;
 }
 
@@ -512,9 +557,10 @@ AD<T, N> pow(const double base, AD<T, N> const& A)
 {
   AD<T, N> tmp;
   tmp.resize(A.size());
+  T A_tmp = A;
   tmp.val() = std::pow((double)base, A.val());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = std::log(base) * std::pow(base, A.val())  * A.dx(i);
+    tmp.dx(i) = T(std::log(base) * pow(base, A_tmp) * A.dx(i));
   return tmp;
 }
 
@@ -526,11 +572,19 @@ AD<T, N> pow(AD<T, N> const& A, AD<T, N> const& e)
   AD<T, N> tmp;
   unsigned int max = A.size() > e.size() ? A.size() : e.size();
   tmp.resize(max);
+  T A_tmp = A;
+  T e_tmp = e;
   tmp.val() = std::pow(A.val(), e.val());
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = e.dx(i) * std::log(A.val()) * std::pow(A.val(), e.val()) +
-      e.val() * A.dx(i) * std::pow(A.val(), e.val()-1.);
+    tmp.dx(i) = T(e.dx(i) * log(A_tmp) * pow(A_tmp, e_tmp) +
+      e_tmp * A.dx(i) * pow(A_tmp, e_tmp - 1.));
   return tmp;
+}
+
+/** \brief wrapper for standard sqrt function */
+double sqrt(double A)
+{
+  return std::sqrt(A);
 }
 
 /** \brief square root of an AD variable */
@@ -540,9 +594,22 @@ AD<T, N> sqrt(AD<T, N> const& A)
   AD<T, N> tmp;
   tmp.resize(A.size());
   tmp.val() = std::sqrt(A.val());
+  T A_tmp = A;
   for (unsigned int i=0; i < tmp.size(); ++i)
-    tmp.dx(i) = A.dx(i) / (2. * std::sqrt(A.val()));
+    tmp.dx(i) = T(.5 * A.dx(i) / sqrt(A_tmp));
   return tmp;
+}
+
+/** \brief wrapper for standard sin function */
+double sin(double A)
+{
+  return std::sin(A);
+}
+
+/** \brief wrapper for standard cos function */
+double cos(double A)
+{
+  return std::cos(A);
 }
 
 /** \brief sin of an AD variable */
@@ -550,10 +617,11 @@ template <class T, unsigned int N>
 AD<T, N> sin(AD<T, N> A)
 {
   AD<T, N> tmp;
-  tmp.val() = std::sin(A.val());
   tmp.resize(A.size());
+  tmp.val() = std::sin(A.val());
+  T A_tmp = A;
   for(unsigned int i = 0; i < tmp.size(); i++)
-    tmp.dx(i) = std::cos(A.val()) * A.dx(i);
+    tmp.dx(i) = T(cos(A_tmp) * A.dx(i));
   return tmp;
 }
 
@@ -563,20 +631,23 @@ AD<T, N> cos(AD<T, N> A)
 {
   AD<T, N> tmp;
   tmp.val() = std::cos(A.val());
+  T A_tmp = A;
   tmp.resize(A.size());
   for(unsigned int i = 0; i < tmp.size(); i++)
-    tmp.dx(i) = -std::sin(A.val()) * A.dx(i);
+    tmp.dx(i) = T(-sin(A_tmp) * A.dx(i));
   return tmp;
 }
 
+/** \brief tan of an AD variable */
 template <class T, unsigned int N>
 AD<T, N> tan(AD<T, N> A)
 {
   AD<T, N> tmp;
   tmp.val() = std::tan(A.val());
+  T A_tmp = A;
   tmp.resize(A.size());
   for(unsigned int i = 0; i < tmp.size(); i++)
-    tmp.dx(i) = A.dx(i) * (1/(std::cos(A.val()) * std::cos(A.val())));
+    tmp.dx(i) = T(A.dx(i) * (1. / (cos(A_tmp) * cos(A_tmp))));
   return tmp;
 }
 
