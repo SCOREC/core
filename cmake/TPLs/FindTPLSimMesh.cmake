6b73e18a7889f36eafd22a8de4f885bed03460a1
Saurabh Tendulkar <saurabh@simmetrix.com>
2016-01-07 14:14:27 -0500

    Ability to use shared Simmetrix libs
    New var SIM_BASEMPI is required (even for static)

--- cmake/TPLs/FindTPLSimMesh.cmake
+++ cmake/TPLs/FindTPLSimMesh.cmake
@@ -2,6 +2,6 @@ INCLUDE(TribitsTplDeclareLibraries)
 
 TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( SimMesh
   REQUIRED_HEADERS MeshSim.h SimPartitionedMesh.h
-  REQUIRED_LIBS_NAMES SimPartitionedMesh-mpi${SIM_DEBUG} SimMeshing${SIM_DEBUG} SimMeshTools${SIM_DEBUG} SimPartitionWrapper-${SIM_MPI}
+  REQUIRED_LIBS_NAMES SimPartitionedMesh-${SIM_BASEMPI}${SIM_DEBUG} SimMeshing${SIM_DEBUG} SimMeshTools${SIM_DEBUG} SimPartitionWrapper-${SIM_MPI}${SIM_DEBUG}
 )
 
