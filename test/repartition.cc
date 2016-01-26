45208fc11d38f8f9bc30d79d9cfc22910ec07f84
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-01-26 08:17:42 -0500

    draft of any-output-count "repartition" program

---
+++ test/repartition.cc
@@ -0,0 +1,105 @@
+#include <gmi_mesh.h>
+#include <apf.h>
+#include <apfMesh2.h>
+#include <apfMDS.h>
+#include <PCU.h>
+#include <parma.h>
+#include <apfZoltan.h>
+#include <apfPartition.h>
+#include <cassert>
+#include <cstdlib>
+
+namespace {
+
+const char* modelFile = 0;
+const char* meshFile = 0;
+const char* outFile = 0;
+int inputPartCount = 1;
+
+void freeMesh(apf::Mesh* m)
+{
+  m->destroyNative();
+  apf::destroyMesh(m);
+}
+
+bool switchToOriginals()
+{
+  apf::Contract contract(inputPartCount, PCU_Comm_Self());
+  int self = PCU_Comm_Self();
+  int group;
+  int groupRank;
+  bool isOriginal = contract.isValid(self);
+  if (isOriginal) {
+    group = 0;
+    groupRank = contract(self);
+  } else {
+    group = MPI_UNDEFINED;
+    groupRank = 0;
+  }
+  MPI_Comm groupComm;
+  MPI_Comm_split(MPI_COMM_WORLD, group, groupRank, &groupComm);
+  PCU_Switch_Comm(groupComm);
+  return isOriginal;
+}
+
+void switchToAll()
+{
+  MPI_Comm prevComm = PCU_Get_Comm();
+  PCU_Switch_Comm(MPI_COMM_WORLD);
+  MPI_Comm_free(&prevComm);
+  PCU_Barrier();
+}
+
+void getConfig(int argc, char** argv)
+{
+  if ( argc != 5 ) {
+    if ( !PCU_Comm_Self() )
+      printf("Usage: %s <model> <mesh> <outMesh> <factor>\n", argv[0]);
+    MPI_Finalize();
+    exit(EXIT_FAILURE);
+  }
+  modelFile = argv[1];
+  inputPartCount = atoi(argv[2]);
+  meshFile = argv[3];
+  outFile = argv[4];
+  assert(inputPartCount <= PCU_Comm_Peers());
+}
+
+void balance(apf::Mesh2* m)
+{
+  apf::MeshTag* weights = m->createDoubleTag("zoltan_weight", 1);
+  {
+    apf::MeshIterator* it = m->begin(m->getDimension());
+    apf::MeshEntity* e;
+    double value = 1.0;
+    while ((e = m->iterate(it)))
+      m->setDoubleTag(e, weights, &value);
+  }
+  apf::Balancer* b = apf::makeZoltanBalancer(m, apf::GRAPH, apf::REPARTITION);
+  b->balance(weights, 0.1);
+  m->destroyTag(weights);
+}
+
+}
+
+int main(int argc, char** argv)
+{
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  gmi_register_mesh();
+  getConfig(argc,argv);
+  gmi_model* g = gmi_load(modelFile);
+  apf::Mesh2* m = 0;
+  bool isOriginal = switchToOriginals();
+  if (isOriginal)
+    m = apf::loadMdsMesh(g, meshFile);
+  switchToAll();
+  m = apf::expandMdsMesh(m, g, inputPartCount);
+  balance(m);
+  m->writeNative(outFile);
+  freeMesh(m);
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
+
+
