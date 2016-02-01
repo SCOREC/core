4a6a360ac6df0cc5aa13c4b74cf39105aa22748c
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-01-31 20:22:12 -0500

    adding gmi_sim to some more programs

--- test/renderClass.cc
+++ test/renderClass.cc
@@ -1,15 +1,20 @@
 #include <apf.h>
 #include <gmi_mesh.h>
+#include <gmi_sim.h>
 #include <apfMDS.h>
 #include <apfMesh2.h>
 #include <apfNumbering.h>
 #include <PCU.h>
+#include <SimUtil.h>
 #include <cstdlib>
 
 int main(int argc, char** argv)
 {
   MPI_Init(&argc,&argv);
   PCU_Comm_Init();
+  SimUtil_start();
+  Sim_readLicenseFile(NULL);
+  gmi_sim_start();
   if ( argc != 4 ) {
     if ( !PCU_Comm_Self() )
       printf("Usage: %s <model> <mesh> <out prefix>\n", argv[0]);
@@ -33,6 +38,9 @@ int main(int argc, char** argv)
   apf::writeVtkFiles(argv[3], m);
   m->destroyNative();
   apf::destroyMesh(m);
+  Sim_unregisterAllKeys();
+  SimUtil_stop();
+  PCU_Comm_Free();
   PCU_Comm_Free();
   MPI_Finalize();
 }
--- test/serialize.cc
+++ test/serialize.cc
@@ -2,6 +2,8 @@
 #include <PCU.h>
 #include <apfMDS.h>
 #include <gmi_mesh.h>
+#include <gmi_sim.h>
+#include <SimUtil.h>
 #include <cassert>
 
 struct GroupCode : public Parma_GroupCode
@@ -19,12 +21,19 @@ int main( int argc, char* argv[])
   assert(argc==4);
   MPI_Init(&argc,&argv);
   PCU_Comm_Init();
+  SimUtil_start();
+  Sim_readLicenseFile(NULL);
+  gmi_sim_start();
   gmi_register_mesh();
+  gmi_register_sim();
   GroupCode code;
   code.mesh = apf::loadMdsMesh(argv[1], argv[2]);
   code.meshFile = argv[3];
   apf::Unmodulo outMap(PCU_Comm_Self(), PCU_Comm_Peers());
   Parma_ShrinkPartition(code.mesh, PCU_Comm_Peers(), code);
+  gmi_sim_stop();
+  Sim_unregisterAllKeys();
+  SimUtil_stop();
   PCU_Comm_Free();
   MPI_Finalize();
 }
