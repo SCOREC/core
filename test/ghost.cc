8c7d01f9ff11cb9fe16d4347ead5ea73ffbe08d7
Cameron Smith <smithc11@rpi.edu>
2016-02-13 21:23:33 -0500

    fun3d weights
    remove unused unit weight fn
    remove unused headers

--- test/ghost.cc
+++ test/ghost.cc
@@ -1,5 +1,4 @@
 #include <gmi_mesh.h>
-#include <ma.h>
 #include <apf.h>
 #include <apfMesh2.h>
 #include <apfMDS.h>
@@ -24,28 +23,44 @@ namespace {
     meshFile = argv[2];
   }
 
-  apf::MeshTag* applyUnitWeight(apf::Mesh* m) {
-    apf::MeshTag* wtag = m->createDoubleTag("ghostUnitWeight",1);
+  double getFun3dElmW(int type) {
+    assert(type >= apf::Mesh::TET && type <= apf::Mesh::PYRAMID);
+    const double tetw = 1.0;
+    const double pyrw = 6.8;
+    const double przw = 7.5;
+    const double hexw = 13.8;
+    const double elmWeights[4] = {tetw, hexw, przw, pyrw};
+    const int offset = apf::Mesh::TET;
+    return elmWeights[type-offset];
+  }
+
+  apf::MeshTag* applyFun3dWeight(apf::Mesh* m) {
+    assert(3 == m->getDimension());
+    apf::MeshTag* wtag = m->createDoubleTag("ghostWeight",1);
     apf::MeshEntity* e;
-    for(int d=0; d <= m->getDimension(); d++) {
+    for(int d=0; d < m->getDimension(); d++) {
       apf::MeshIterator* itr = m->begin(d);
       double w = 1;
       while( (e = m->iterate(itr)) )
         m->setDoubleTag(e, wtag, &w);
       m->end(itr);
     }
+    apf::MeshIterator* itr = m->begin(3);
+    while( (e = m->iterate(itr)) ) {
+      double w = getFun3dElmW(m->getType(e));
+      m->setDoubleTag(e, wtag, &w);
+    }
+    m->end(itr);
     return wtag;
   }
 
-  void runParma(apf::Mesh* m) {
-    apf::MeshTag* weights = applyUnitWeight(m);
+  void runParma(apf::Mesh* m, apf::MeshTag* weights) {
     const int layers = 1;
     const double stepFactor = 0.5;
     const int verbosity = 2;
     apf::Balancer* ghost =
       Parma_MakeGhostDiffuser(m, layers, stepFactor, verbosity);
     ghost->balance(weights, 1.05);
-    m->destroyTag(weights);
     delete ghost;
   }
 }
@@ -60,7 +75,9 @@ int main(int argc, char** argv)
   gmi_register_mesh();
   getConfig(argc,argv);
   apf::Mesh2* m = apf::loadMdsMesh(modelFile,meshFile);
-  runParma(m);
+  apf::MeshTag* weights = applyFun3dWeight(m);
+  runParma(m,weights);
+  m->destroyTag(weights);
   apf::writeVtkFiles(argv[3],m);
   freeMesh(m);
   PCU_Comm_Free();
