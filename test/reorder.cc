9c0e6553ba12acd2868f2d7699214a2e64cdbb28
Cameron Smith <smithc11@rpi.edu>
2016-03-06 16:50:41 -0500

    geomsim support

--- test/reorder.cc
+++ test/reorder.cc
@@ -1,15 +1,20 @@
 #include <gmi_null.h>
 #include <gmi_mesh.h>
+#include <gmi_sim.h>
 #include <apfMDS.h>
 #include <apfMesh2.h>
 #include <apf.h>
 #include <PCU.h>
 #include <parma.h>
+#include <SimUtil.h>
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
   MPI_Init(&argc,&argv);
   PCU_Comm_Init();
+  SimUtil_start();
+  Sim_readLicenseFile(NULL);
+  gmi_sim_start();
   if ( argc != 4 ) {
     if ( !PCU_Comm_Self() )
       printf("Usage: %s <model> <mesh> <out prefix>\n", argv[0]);
@@ -18,12 +23,16 @@ int main(int argc, char** argv) {
   }
   gmi_register_null();
   gmi_register_mesh();
+  gmi_register_sim();
   apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
   apf::MeshTag* order = Parma_BfsReorder(m);
   apf::reorderMdsMesh(m, order);
   m->writeNative(argv[3]);
   m->destroyNative();
   apf::destroyMesh(m);
+  gmi_sim_stop();
+  Sim_unregisterAllKeys();
+  SimUtil_stop();
   PCU_Comm_Free();
   MPI_Finalize();
 }
