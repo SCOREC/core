SCOREC/core

commit 7d82aede928c3dc4e726a564b23ffb9a1c504e4c

try to ignore PHASTA array VOF solution

phasta/phRestart.cc
@@ -99,7 +99,10 @@ int readAndAttachField(
   int ret = ph_read_field(f, anyfield, swap,
       &data, &nodes, &vars, &step, hname);
   /* no field was found or the field has an empty data block */
-  if(ret==0 || ret==1) return ret;
+  if(ret==0 || ret==1)
+    return ret;
+  if ( std::string(hname) == std::string("VOF solution") )
+    return 1;
   assert(nodes == static_cast<int>(m->count(0)));
   assert(step == in.timeStepNumber);
   int out_size = vars;
@@ -162,7 +165,8 @@ void readAndAttachFields(Input& in, apf::Mesh* m) {
     abort();
   }
   int swap = ph_should_swap(f);
-  while( readAndAttachField(in,f,m,swap) ); /* inf loop?? */
+  /* stops when ph_read_field returns 0 */
+  while( readAndAttachField(in,f,m,swap) );
   fclose(f);
   double t1 = PCU_Time();
   if (!PCU_Comm_Self())

commit ce5208dc24dabbb166dac9e234eeba0f80c4cc99

fix memory leak introduced by 7d82aed

phasta/phRestart.cc
@@ -101,8 +101,10 @@ int readAndAttachField(
   /* no field was found or the field has an empty data block */
   if(ret==0 || ret==1)
     return ret;
-  if ( std::string(hname) == std::string("VOF solution") )
+  if ( std::string(hname) == std::string("VOF solution") ) {
+    free(data);
     return 1;
+  }
   assert(nodes == static_cast<int>(m->count(0)));
   assert(step == in.timeStepNumber);
   int out_size = vars;