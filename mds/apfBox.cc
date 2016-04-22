de83c5bbba087687b9b05f5e128c634791c783b2
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-04-22 11:27:19 -0400

    expose some BoxBuilder details

--- mds/apfBox.cc
+++ mds/apfBox.cc
@@ -1,103 +1,68 @@
+#include "apfBox.h"
+
 #include "apfMDS.h"
-#include <apfMesh2.h>
-#include <gmi_base.h>
 #include <gmi_lookup.h>
 #include <cassert>
 #include <cstdlib>
 
 namespace apf {
 
-struct Indices
-{
-  Indices() {}
-  Indices(int a, int b, int c): x(a),y(b),z(c) {}
-  int x, y, z;
-  int& operator[](int i)
-  {
-    switch (i) {
-      case 0: return x;
-      case 1: return y;
-      case 2: return z;
-      default: printf("i must be in {0,1,2}"); abort(); return x;
-    }
-  }
-  Indices operator+(Indices oi)
-  {
-    return Indices(x + oi.x, y + oi.y, z + oi.z);
-  }
-  Indices operator*(int s)
-  {
-    return Indices(x * s, y * s, z * s);
-  }
-  static Indices unit(int d)
-  {
-    Indices i(0,0,0);
-    i[d] = 1;
-    return i;
-  }
-};
+Indices::Indices() {}
 
-struct Grid {
-  Grid(int nx, int ny, int nz):
-    size(nx, ny, nz)
-  {
-    stride[0] = 1;
-    for (int i = 0; i < 3; ++i)
-      stride[i + 1] = stride[i] * size[i];
-  }
-  int total() {return stride[3];}
-  Indices out(int i)
-  {
-    Indices is;
-    for (int j = 0; j < 3; ++j)
-      is[j] = (i % stride[j + 1]) / stride[j];
-    return is;
-  }
-  int in(Indices is)
-  {
-    int i = 0;
-    for (int j = 0; j < 3; ++j)
-      i += is[j] * stride[j];
-    return i;
-  }
-  Indices size;
-  int stride[4];
-};
+Indices::Indices(int a, int b, int c): x(a),y(b),z(c) {}
 
-struct BoxBuilder
+int& Indices::operator[](int i)
 {
-  Grid grid;
-  Grid mgrid;
-  int dim;
-  double w[3];
-  bool is_simplex;
-  struct { int dim; int tag; } modelTable[27];
-  int modelCounts[4];
-  apf::Mesh2* m;
-  std::vector<apf::MeshEntity*> v;
-  BoxBuilder(int nx, int ny, int nz,
-      double wx, double wy, double wz,
-      bool is);
-  void formModelTable();
-  void addModelUse(gmi_base* gb, agm_bdry ab, Indices di);
-  gmi_model* buildModel();
-  int getModelIndex(int i, int d);
-  Indices getModelIndices(Indices vi);
-  apf::ModelEntity* getModelEntity(Indices mi);
-  void buildCellVert(int i);
-  apf::MeshEntity* getVert(Indices vi);
-  void buildCellEdges(int i);
-  void buildTriangles(apf::MeshEntity* fv[4], apf::ModelEntity* me);
-  void buildFace(apf::MeshEntity* fv[4], apf::ModelEntity* me);
-  void buildCellFaces(int i);
-  void buildTets(apf::MeshEntity* rv[8], apf::ModelEntity* me);
-  void buildHex(apf::MeshEntity* rv[8], apf::ModelEntity* me);
-  void buildRegion(apf::MeshEntity* rv[8], apf::ModelEntity* me);
-  void buildCellRegion(int i);
-  void buildCell(int i, int d);
-  void buildDimension(int d);
-  void buildMeshAndModel();
-};
+  switch (i) {
+    case 0: return x;
+    case 1: return y;
+    case 2: return z;
+    default: printf("i must be in {0,1,2}"); abort(); return x;
+  }
+}
+
+Indices Indices::operator+(Indices oi)
+{
+  return Indices(x + oi.x, y + oi.y, z + oi.z);
+}
+
+Indices Indices::operator*(int s)
+{
+  return Indices(x * s, y * s, z * s);
+}
+
+Indices Indices::unit(int d)
+{
+  Indices i(0,0,0);
+  i[d] = 1;
+  return i;
+}
+
+Grid::Grid(int nx, int ny, int nz):
+  size(nx, ny, nz)
+{
+  stride[0] = 1;
+  for (int i = 0; i < 3; ++i)
+    stride[i + 1] = stride[i] * size[i];
+}
+
+int Grid::total() {return stride[3];}
+
+Indices Grid::out(int i)
+{
+  Indices is;
+  for (int j = 0; j < 3; ++j)
+    is[j] = (i % stride[j + 1]) / stride[j];
+  return is;
+}
+
+int Grid::in(Indices is)
+{
+  int i = 0;
+  for (int j = 0; j < 3; ++j)
+    i += is[j] * stride[j];
+  return i;
+}
 
 BoxBuilder::BoxBuilder(int nx, int ny, int nz,
       double wx, double wy, double wz,
@@ -112,7 +77,7 @@ BoxBuilder::BoxBuilder(int nx, int ny, int nz,
   is_simplex = is;
   formModelTable();
   gmi_model* gm = buildModel();
-  m = apf::makeEmptyMdsMesh(gm, dim, false);
+  m = makeEmptyMdsMesh(gm, dim, false);
   v.resize(grid.total());
   buildMeshAndModel();
 }
@@ -150,7 +115,7 @@ Indices BoxBuilder::getModelIndices(Indices vi)
   return mi;
 }
 
-apf::ModelEntity* BoxBuilder::getModelEntity(Indices mi)
+ModelEntity* BoxBuilder::getModelEntity(Indices mi)
 {
   int mj = mgrid.in(mi);
   int mdim = modelTable[mj].dim;
@@ -214,11 +179,11 @@ void BoxBuilder::buildCellVert(int i)
 {
   Indices vi = grid.out(i);
   v[i] = m->createVert(getModelEntity(getModelIndices(vi)));
-  apf::Vector3 pt(w[0] * vi.x, w[1] * vi.y, w[2] * vi.z);
+  Vector3 pt(w[0] * vi.x, w[1] * vi.y, w[2] * vi.z);
   m->setPoint(v[i], 0, pt);
 }
 
-apf::MeshEntity* BoxBuilder::getVert(Indices vi)
+MeshEntity* BoxBuilder::getVert(Indices vi)
 {
   return v.at(grid.in(vi));
 }
@@ -227,7 +192,7 @@ void BoxBuilder::buildCellEdges(int i)
 {
   Indices vi = grid.out(i);
   Indices mi = getModelIndices(vi);
-  apf::MeshEntity* ev[2];
+  MeshEntity* ev[2];
   ev[0] = getVert(vi);
   for (int j = 0; j < 3; ++j) {
     if (mi[j] == mgrid.size[j] - 1)
@@ -235,33 +200,33 @@ void BoxBuilder::buildCellEdges(int i)
     ev[1] = getVert(vi + Indices::unit(j));
     Indices emi = mi;
     emi[j] = 1;
-    apf::ModelEntity* me = getModelEntity(emi);
-    apf::buildElement(m, me, apf::Mesh::EDGE, ev);
+    ModelEntity* me = getModelEntity(emi);
+    buildElement(m, me, Mesh::EDGE, ev);
   }
 }
 
-void BoxBuilder::buildTriangles(apf::MeshEntity* fv[4], apf::ModelEntity* me)
+void BoxBuilder::buildTriangles(MeshEntity* fv[4], ModelEntity* me)
 {
-  apf::MeshEntity* tv[3];
+  MeshEntity* tv[3];
   tv[0] = fv[0]; tv[1] = fv[1]; tv[2] = fv[2];
-  apf::buildElement(m, me, apf::Mesh::TRIANGLE, tv);
+  buildElement(m, me, Mesh::TRIANGLE, tv);
   tv[0] = fv[2]; tv[1] = fv[3]; tv[2] = fv[0];
-  apf::buildElement(m, me, apf::Mesh::TRIANGLE, tv);
+  buildElement(m, me, Mesh::TRIANGLE, tv);
 }
 
-void BoxBuilder::buildFace(apf::MeshEntity* fv[4], apf::ModelEntity* me)
+void BoxBuilder::buildFace(MeshEntity* fv[4], ModelEntity* me)
 {
   if (is_simplex)
     buildTriangles(fv, me);
   else
-    apf::buildElement(m, me, apf::Mesh::QUAD, fv);
+    buildElement(m, me, Mesh::QUAD, fv);
 }
 
 void BoxBuilder::buildCellFaces(int i)
 {
   Indices vi = grid.out(i);
   Indices mi = getModelIndices(vi);
-  apf::MeshEntity* fv[4];
+  MeshEntity* fv[4];
   fv[0] = getVert(vi);
   for (int jx = 0; jx < 3; ++jx) {
     int jy = (jx + 1) % 3;
@@ -274,12 +239,12 @@ void BoxBuilder::buildCellFaces(int i)
     Indices emi = mi;
     emi[jx] = 1;
     emi[jy] = 1;
-    apf::ModelEntity* me = getModelEntity(emi);
+    ModelEntity* me = getModelEntity(emi);
     buildFace(fv, me);
   }
 }
 
-void BoxBuilder::buildTets(apf::MeshEntity* rv[8], apf::ModelEntity* me)
+void BoxBuilder::buildTets(MeshEntity* rv[8], ModelEntity* me)
 {
   static int const tet_verts[6][4] = {
   {0,1,2,6},
@@ -289,19 +254,19 @@ void BoxBuilder::buildTets(apf::MeshEntity* rv[8], apf::ModelEntity* me)
   {0,4,5,6},
   {0,5,1,6}};
   for (int i = 0; i < 6; ++i) {
-    apf::MeshEntity* tv[4];
+    MeshEntity* tv[4];
     for (int j = 0; j < 4; ++j)
       tv[j] = rv[tet_verts[i][j]];
-    apf::buildElement(m, me, apf::Mesh::TET, tv);
+    buildElement(m, me, Mesh::TET, tv);
   }
 }
 
-void BoxBuilder::buildHex(apf::MeshEntity* rv[8], apf::ModelEntity* me)
+void BoxBuilder::buildHex(MeshEntity* rv[8], ModelEntity* me)
 {
-  apf::buildElement(m, me, apf::Mesh::HEX, rv);
+  buildElement(m, me, Mesh::HEX, rv);
 }
 
-void BoxBuilder::buildRegion(apf::MeshEntity* rv[8], apf::ModelEntity* me)
+void BoxBuilder::buildRegion(MeshEntity* rv[8], ModelEntity* me)
 {
   if (is_simplex)
     buildTets(rv, me);
@@ -317,7 +282,7 @@ void BoxBuilder::buildCellRegion(int i)
       mi.y == mgrid.size.y - 1 ||
       mi.z == mgrid.size.z - 1)
     return;
-  apf::MeshEntity* rv[8];
+  MeshEntity* rv[8];
   rv[0] = getVert(Indices(vi.x + 0, vi.y + 0, vi.z + 0));
   rv[1] = getVert(Indices(vi.x + 1, vi.y + 0, vi.z + 0));
   rv[2] = getVert(Indices(vi.x + 1, vi.y + 1, vi.z + 0));
@@ -326,7 +291,7 @@ void BoxBuilder::buildCellRegion(int i)
   rv[5] = getVert(Indices(vi.x + 1, vi.y + 0, vi.z + 1));
   rv[6] = getVert(Indices(vi.x + 1, vi.y + 1, vi.z + 1));
   rv[7] = getVert(Indices(vi.x + 0, vi.y + 1, vi.z + 1));
-  apf::ModelEntity* me = getModelEntity(Indices(1,1,1));
+  ModelEntity* me = getModelEntity(Indices(1,1,1));
   buildRegion(rv, me);
 }
 
@@ -361,7 +326,7 @@ void BoxBuilder::buildMeshAndModel()
   m->acceptChanges();
 }
 
-apf::Mesh2* makeMdsBox(
+Mesh2* makeMdsBox(
     int nex, int ney, int nez,
     double wx, double wy, double wz, bool is)
 {
