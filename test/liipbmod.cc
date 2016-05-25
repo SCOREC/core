182487dde8d0930d7cadf413fd51cc41013ed22d
Cameron Smith <smithc11@rpi.edu>
2016-05-24 23:50:00 -0400

    builds

--- test/liipbmod.cc
+++ test/liipbmod.cc
@@ -1,27 +1,30 @@
 #include "LIIPBMod.h"
+#include <apf.h>
+#include <apfMDS.h>
+#include <gmi_mesh.h>
+#include <parma.h>
+#include <PCU.h>
+#include <cassert>
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-   ParUtil::Instance()->init(argc, argv);
-
-   pGModel model = 0;
-   pMesh mesh = MS_newMesh(model);
-   
-   PM_load(mesh, "geom.sms");
-
-   int numParts = atoi(argv[1]);
-   vector<pMesh> meshes;
-   meshes.push_back(mesh);
-
-   zoltanCB zlb;
-   zlb.setLocalNumParts(numParts);
-   M_loadbalance2(meshes,zlb);
-
-   LIIPBMod liipbmod;
-   liipbmod.run(meshes);
-
-   ParUtil::Instance()->Finalize();
-
-
-   return 1;
+  assert(argc == 4);
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  if ( argc != 4 ) {
+    if ( !PCU_Comm_Self() )
+      printf("Usage: %s <model> <mesh> <out mesh>\n", argv[0]);
+    MPI_Finalize();
+    exit(EXIT_FAILURE);
+  }
+  gmi_register_mesh();
+  apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
+  Parma_PrintPtnStats(m, "initial");
+  LIIPBMod liipbmod;
+  liipbmod.run(m);
+  Parma_PrintPtnStats(m, "final");
+  m->destroyNative();
+  apf::destroyMesh(m);
+  PCU_Comm_Free();
+  MPI_Finalize();
 }
