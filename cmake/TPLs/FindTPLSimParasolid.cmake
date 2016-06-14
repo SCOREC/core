7673359dd5b6ae6b653de961856542762411d007
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-06-13 20:28:49 -0400

    increase versions of Simmetrix CAD interfaces

--- cmake/TPLs/FindTPLSimParasolid.cmake
+++ cmake/TPLs/FindTPLSimParasolid.cmake
@@ -2,5 +2,5 @@ INCLUDE(TribitsTplDeclareLibraries)
 
 TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( SimParasolid
   REQUIRED_HEADERS SimParasolidKrnl.h
-  REQUIRED_LIBS_NAMES SimParasolid260 pskernel
+  REQUIRED_LIBS_NAMES SimParasolid270 pskernel
 )
