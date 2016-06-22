8690e64fb67d7e5128012afcf28e6e16c7b97766
Seegyoung Seol <seols@rpi.edu>
2016-06-22 18:20:35 +0000

    change in trunk - updating mpich3 config.sh
    git-svn-id: http://redmine.scorec.rpi.edu/svn/pumi/trunk@14 9ee00306-6ec0-4207-ac95-3d6da376c8fb

--- mpich3-gcc4.4.5-config.sh
+++ mpich3-gcc4.4.5-config.sh
@@ -1,5 +1,7 @@
 PETSC_DIR=/fasttmp/seol/petsc-3.5.4
 PETSC_ARCH=real-mpich3-gcc4.4.5
+ZOLTAN_DIR= /usr/local/zoltan/3.81/mpich3.1.2
+PARMETIS_DIR=/usr/local/parmetis/4.0.3/mpich3.1.2
 
 cmake .. \
   -DCMAKE_C_COMPILER="/usr/local/mpich3/latest/bin/mpicc" \
@@ -7,12 +9,12 @@ cmake .. \
   -DCMAKE_C_FLAGS=" -g -O2" \
   -DCMAKE_CXX_FLAGS=" -g -O2" \
   -DENABLE_ZOLTAN=ON \
-  -DZOLTAN_INCLUDE_DIR="/fasttmp/seol/mpich3-gcc4.4.5-install/include" \
-  -DZOLTAN_LIBRARY="/fasttmp/seol/mpich3-gcc4.4.5-install/lib/libzoltan.a" \
-  -DPARMETIS_INCLUDE_DIR="$PETSC_DIR/$PETSC_ARCH/include" \
-  -DPARMETIS_LIBRARY="$PETSC_DIR/$PETSC_ARCH/lib/libparmetis.a" \
-  -DMETIS_LIBRARY="$PETSC_DIR/$PETSC_ARCH/lib/libmetis.a" \
-  -DCMAKE_INSTALL_PREFIX="/fasttmp/seol/mpich3-gcc4.4.5-install" \
+  -DZOLTAN_INCLUDE_DIR="$ZOLTAN_DIR/include" \
+  -DZOLTAN_LIBRARY="$ZOLTAN_DIR/lib/libzoltan.a" \
+  -DPARMETIS_INCLUDE_DIR="$PARMETIS_DIR/include" \
+  -DPARMETIS_LIBRARY="$PARMETIS_DIR/lib/libparmetis.a" \
+  -DMETIS_LIBRARY="$PARMETIS_DIR/lib/libmetis.a" \
+  -DCMAKE_INSTALL_PREFIX="SET_YOUR_PREFIX" \
   -DENABLE_OMEGA_H=ON \
   -DCMAKE_BUILD_TYPE=Debug
 
