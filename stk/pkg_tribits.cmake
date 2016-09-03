695b555384895d261cfca9f4974b78958ba5a39a
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-09-03 07:53:23 -0400

    fix bug in apf_stk TriBITS build

--- stk/pkg_tribits.cmake
+++ stk/pkg_tribits.cmake
@@ -1,12 +1,12 @@
-# determine if Trilinos has been configured with stk
+tribits_package(SCORECapf_stk)
+
+# determine if configured with stk
 SET(ENABLE_STK_MESH OFF)
-if(Trilinos_ENABLE_STKIO AND
-   Trilinos_ENABLE_STKMesh AND
-   Trilinos_ENABLE_SEACASIoss)
+if(SCORECapf_stk_ENABLE_STKIO AND
+   SCORECapf_stk_ENABLE_STKMesh AND
+   SCORECapf_stk_ENABLE_SEACASIoss)
   SET(ENABLE_STK_MESH ON)
 endif()
-
-tribits_package(SCORECapf_stk) 
 
 include_directories(${CMAKE_CURRENT_SOURCE_DIR})
 
@@ -21,7 +21,7 @@ set(SOURCES
   apfSTK.cc)
 set(HEADERS apfAlbany.h)
 
-if(HAS_STK)
+if(ENABLE_STK_MESH)
   set(HEADERS ${HEADERS} apfSTK.h)
   set(SOURCES ${SOURCES} apfExodusOutput.cc)
 endif()
