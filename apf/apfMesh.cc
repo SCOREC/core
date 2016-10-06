SCOREC/core

commit 429e410e9e0c34eede204fb2891c94ae2116ca6d

try to fix matched CavityOp
CavityOp was calling mesh->isShared,
which only considers remote copies.
most everything else was being delegated
to a Sharing object.
However, Sharing objects couldn't yet
answer the isShared query, so I added
that as well (which affected every
object that derives from Sharing).
This is in response to issue #45

apf/apfMesh.cc
@@ -812,6 +812,10 @@ void NormalSharing::getCopies(MeshEntity* e,
   getRemotesArray(mesh, e, copies);
 }
 
+bool NormalSharing::isShared(MeshEntity* e) {
+  return mesh->isShared(e);
+}
+
 /* okay... so previously this used a min-rank rule
    for matched copies, but thats inconsistent with
    the min-count rule in MDS for regular copies,
@@ -898,6 +902,15 @@ void MatchedSharing::formCountMap()
     PCU_COMM_UNPACK(oc);
     countMap[PCU_Comm_Sender()] = oc;
   }
+}
+
+bool MatchedSharing::isShared(MeshEntity* e) {
+  CopyArray copies;
+  this->getCopies(e, copies);
+  APF_ITERATE(CopyArray, copies, it)
+    if (it->peer != PCU_Comm_Self())
+      return false;
+  return true;
 }
 
 Sharing* getSharing(Mesh* m)

apf/apfCavityOp.cc
@@ -39,7 +39,7 @@ void CavityOp::applyLocallyWithModification(int d)
     {
       Outcome o = setEntity(e);
       if (o == OK)
-      { 
+      {
         movedByDeletion = false;
         apply();
         if (movedByDeletion)
@@ -107,7 +107,7 @@ void CavityOp::applyToDimension(int d, bool matched)
   /* the iteration count of this loop is hard to predict,
    * but typical cavity definitions should cause a small
    * constant number of iterations that does not grow
-   * with parallelism 
+   * with parallelism
    */
   do {
     /* apply the operator to all local cavities
@@ -129,7 +129,7 @@ bool CavityOp::requestLocality(MeshEntity** entities, int count)
 {
   bool areLocal = true;
   for (int i=0; i < count; ++i)
-    if (mesh->isShared(entities[i]))
+    if (sharing->isShared(entities[i]))
       areLocal = false;
   if (isRequesting && ( ! areLocal))
     for (int i=0; i < count; ++i)

apf/apfMesh.h
@@ -447,6 +447,7 @@ struct Sharing
 /** \brief get the copies of the entity */
   virtual void getCopies(MeshEntity* e,
       CopyArray& copies) = 0;
+  virtual bool isShared(MeshEntity* e) = 0;
 };
 
 struct NormalSharing : public Sharing
@@ -455,6 +456,7 @@ struct NormalSharing : public Sharing
   virtual bool isOwned(MeshEntity* e);
   virtual void getCopies(MeshEntity* e,
       CopyArray& copies);
+  virtual bool isShared(MeshEntity* e);
 private:
   Mesh* mesh;
 };
@@ -466,6 +468,7 @@ struct MatchedSharing : public Sharing
   virtual bool isOwned(MeshEntity* e);
   virtual void getCopies(MeshEntity* e,
       CopyArray& copies);
+  virtual bool isShared(MeshEntity* e);
   Mesh* mesh;
 private:
   size_t getNeighborCount(int peer);

apf/apfNumbering.cc
@@ -219,6 +219,7 @@ struct NoSharing : public Sharing
 {
   bool isOwned(MeshEntity*) {return true;}
   virtual void getCopies(MeshEntity*, CopyArray&) {}
+  bool isShared(MeshEntity*) {return false;}
 };
 
 Numbering* numberNodes(

phasta/phLinks.cc
@@ -45,12 +45,16 @@ struct PhastaSharing : public apf::Sharing {
         copies[i++] = copies[j];
     copies.setSize(i);
   }
+  bool isShared(apf::MeshEntity* e)
+  {
+    return helper->isShared(e);
+  }
   apf::Mesh* mesh;
   apf::Sharing* helper;
 };
 
 /* this algorithm is essential to parallel
-   scalability: generate local inter-part 
+   scalability: generate local inter-part
    communication arrays describing shared entities.
 
    for every partition model face, each part

commit 53890b3ead4a9a5ae5802f3547c59c17076a2ad4

fix backwards logic in new isSharing
this should fix a bug introduced by 429e410
still working towards issue #45

apf/apfMesh.cc
@@ -909,8 +909,8 @@ bool MatchedSharing::isShared(MeshEntity* e) {
   this->getCopies(e, copies);
   APF_ITERATE(CopyArray, copies, it)
     if (it->peer != PCU_Comm_Self())
-      return false;
-  return true;
+      return true;
+  return false;
 }
 
 Sharing* getSharing(Mesh* m)