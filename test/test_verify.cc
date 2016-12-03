5bbd8ffc6e235e46efba0b04f89b93523b774f81
Brian Granzow <brian.granzow@gmail.com>
2016-12-03 16:11:50 -0500

    test: try to make verify test comprehensive

--- test/test_verify.cc
+++ test/test_verify.cc
@@ -16,13 +16,15 @@ int main(int argc, char** argv)
   assert(argc == 3);
   gmi_register_mesh();
   apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
-  apf::DynamicArray<apf::Field*> fields(8);
-  apf::FieldShape* shapes[8] = {
+  apf::DynamicArray<apf::Field*> fields(10);
+  apf::FieldShape* shapes[10] = {
     apf::getLagrange(1),
     apf::getSerendipity(),
     apf::getHierarchic(2),
     apf::getIPShape(3,1),
     apf::getIPShape(3,2),
+    apf::getVoronoiShape(3,1),
+    apf::getVoronoiShape(3,2),
     apf::getIPFitShape(3,1),
     apf::getIPFitShape(3,2),
     apf::getConstant(3) };
@@ -34,6 +36,8 @@ int main(int argc, char** argv)
     apf::zeroField(fields[i]);
   }
   apf::verify(m);
+  m->changeShape(shapes[1]);
+  apf::verify(m);
   for (size_t i=0; i < fields.size(); ++i)
     apf::destroyField(fields[i]);
   m->destroyNative();
@@ -41,4 +45,3 @@ int main(int argc, char** argv)
   PCU_Comm_Free();
   MPI_Finalize();
 }
-
