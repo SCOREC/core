f06738d1881f019fe17e0c6be01fb23f944710d5
Fan Yang <yangf4@rpi.edu>
2016-12-09 15:58:22 -0500

    use new-added function in ph_adapt

--- sam/sam.h
+++ sam/sam.h
@@ -9,6 +9,8 @@ apf::Field* specifiedIso(apf::Mesh* m, const char* fieldName, const unsigned idx
 apf::Field* compareIsoSF(apf::Mesh* m, const char* desiredSzFld, int method = 0);
 apf::Field* errorThreshold(apf::Mesh* m, const char* fieldName,
     const unsigned idx, const double limit, const double factor);
+apf::Field* multipleSF(apf::Mesh* m, apf::Field* sf, double factor);
+apf::Field* mulipleSFBox(apf::Mesh* m, apf::Field* sf, double factor, double* box);
 
 }
 
