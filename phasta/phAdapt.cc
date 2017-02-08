83f7667cd75006f36a225bc13ef0e9b651ec2931
Cameron Smith <smithc11@rpi.edu>
2017-02-08 18:49:42 -0500

    phasta: sub-communicator adapt
    revert to a smaller group of processes during adaptation to
    prevent parts from being emptied by the cavity operator

--- phasta/phAdapt.cc
+++ phasta/phAdapt.cc
@@ -4,11 +4,61 @@
 #include <ma.h>
 #include <PCU.h>
 #include <sam.h>
+#include <parma.h>
 
 #include <cstdio>
 #include <cassert>
 
 namespace ph {
+
+struct AdaptCallback : public Parma_GroupCode
+{
+  apf::Mesh2* mesh;
+  ma::Input* in;
+  void run(int) {
+    ma::adapt(in);
+  }
+};
+
+static double getAveragePartDensity(apf::Mesh* m) {
+  double nElements = m->count(m->getDimension());
+  nElements = PCU_Add_Double(nElements);
+  return nElements / PCU_Comm_Peers();
+}
+
+static int getShrinkFactor(apf::Mesh* m, double minPartDensity) {
+  double partDensity = getAveragePartDensity(m);
+  int factor = 1;
+  while (partDensity < minPartDensity) {
+    if (factor >= PCU_Comm_Peers())
+      break;
+    factor *= 2;
+    partDensity *= 2;
+  }
+  assert(PCU_Comm_Peers() % factor == 0);
+  return factor;
+}
+
+static void warnAboutShrinking(int factor) {
+  int nprocs = PCU_Comm_Peers() / factor;
+  if (!PCU_Comm_Self()) {
+    fprintf(stderr,"sensing mesh is spread too thin: "
+                   "adapting with %d procs\n", nprocs);
+  }
+}
+
+void adaptShrunken(apf::Mesh2* m, double minPartDensity,
+                   Parma_GroupCode& callback) {
+  int factor = getShrinkFactor(m, minPartDensity);
+  if (!PCU_Comm_Self())
+    fprintf(stderr,"adaptShrunken factor computed as %d\n", factor);
+  if (factor == 1) {
+    callback.run(0);
+  } else {
+    warnAboutShrinking(factor);
+    Parma_ShrinkPartition(m, factor, callback);
+  }
+}
 
 void setupPreBalance(Input& in, ma::Input* ma_in) {
   if ( in.preAdaptBalanceMethod == "parma" ) {
@@ -93,7 +143,10 @@ namespace chef {
     ma_in->shouldRunPreZoltan = true;
     if (m->hasMatching())
       ph::setupMatching(*ma_in);
-    ma::adapt(ma_in);
+    ph::AdaptCallback acb;
+    acb.mesh = m;
+    acb.in = ma_in;
+    adaptShrunken(m,200000,acb);
   }
 
   void adapt(apf::Mesh2* m, apf::Field* szFld, ph::Input& in) {
@@ -106,7 +159,10 @@ namespace chef {
     ma_in->maximumIterations = in.maxAdaptIterations;
     if (m->hasMatching())
       ph::setupMatching(*ma_in);
-    ma::adapt(ma_in);
+    ph::AdaptCallback acb;
+    acb.mesh = m;
+    acb.in = ma_in;
+    adaptShrunken(m,200000,acb);
   }
 
   void uniformRefinement(ph::Input& in, apf::Mesh2* m)
