42317da2f76073e4c28d7c733cbe5b3b18957f59
Morteza H. Siboni <hakimi1364@gmail.com>
2017-05-26 07:58:22 -0400

    Destroying the temp wireMesh after write to vtk.
    in crv::writeCurvedWireFrame the temporary wireMesh had to be destroyed
    after it is written to vtk.

--- crv/crvVtk.cc
+++ crv/crvVtk.cc
@@ -1025,6 +1025,8 @@ void writeCurvedWireFrame(apf::Mesh* m, int n, const char* prefix)
   std::stringstream ss;
   ss << prefix << "_wire";
   apf::writeVtkFiles(ss.str().c_str(),wireMesh);
+  wireMesh->destroyNative();
+  apf::destroyMesh(wireMesh);
 }
 
 
