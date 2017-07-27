743e886d4c57144093993a790d7311d5d1b521bb
Fan Yang <yangf4@rpi.edu>
2017-07-26 22:46:08 -0400

    [phasta] phasta calls to core to check if model entity is in closure

--- phasta/phSnap.f
+++ phasta/phSnap.f
@@ -2,14 +2,25 @@
       use iso_c_binding
 c
       interface
+c
         subroutine sim_get_pos_on_surf ( dx, dy, dz, numnp, px, py, pz )
      &    bind(C, NAME='sim_get_pos_on_surf')
         use iso_c_binding
           integer(c_int),value :: numnp
-          real(c_double),intent(out),dimension(:) ::
-     &                               dx(numnp), dy(numnp), dz(numnp)
           real(c_double),intent(in),dimension(:) ::
+     &                               dx(numnp), dy(numnp), dz(numnp)
+          real(c_double),intent(out),dimension(:) ::
      &                               px(numnp), py(numnp), pz(numnp)
         end subroutine
+c
+        subroutine sim_is_in_closure ( e_dim, e_tag,
+     &                                 t_dim, t_tag, answer )
+     &    bind(C, NAME='sim_is_in_closure')
+        use iso_c_binding
+          integer(c_int),intent(in),value :: e_dim, e_tag, t_dim, t_tag
+          integer(c_int),intent(out) :: answer
+        end subroutine
+c
       end interface
+c
       end module
--- phasta/phSnap.h
+++ phasta/phSnap.h
@@ -10,8 +10,11 @@ extern "C" {
 #endif
 
 void pass_mesh_to_phasta(apf::Mesh* mesh);
+
 void sim_get_pos_on_surf (double dx[], double dy[], double dz[], int numnp,
                           double px[], double py[], double pz[]);
+
+void sim_is_in_closure (int e_dim, int e_tag, int t_dim, int t_tag, int& answer);
 
 #ifdef __cplusplus
 }
