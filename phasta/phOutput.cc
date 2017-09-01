7e96966f6b753af30b7b984a849d876efeb91ebc
Fan Yang <yangf4@rpi.edu>
2017-08-31 21:28:59 -0400

    [phasta] pass regression tests

--- phasta/phOutput.cc
+++ phasta/phOutput.cc
@@ -139,7 +139,7 @@ static void getInterior(Output& o, BCs& bcs, apf::Numbering* n)
   Blocks& bs = o.blocks.interior;
   int*** ien     = new int**[bs.getSize()];
   int**  mattype = 0;
-  int*  gmattype = new int[m->count(m->getDimension())];
+  int*  gmattype = 0;
   if (bcs.fields.count("material type"))
     mattype = new int* [bs.getSize()];
   apf::NewArray<int> js(bs.getSize());
@@ -150,6 +150,8 @@ static void getInterior(Output& o, BCs& bcs, apf::Numbering* n)
     js[i] = 0;
   }
   int iel = 0;
+  if (mattype)
+    gmattype = new int[m->count(m->getDimension())];
   gmi_model* gm = m->getModel();
   apf::MeshEntity* e;
   apf::MeshIterator* it = m->begin(m->getDimension());
