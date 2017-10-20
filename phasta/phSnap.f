d58415f524be9eed374688caa1304e6644d55e7b
Fan Yang <yangf4@rpi.edu>
2017-10-20 14:06:58 -0400

    [phasta] use more general name for functions used by phasta

--- phasta/phSnap.f
+++ phasta/phSnap.f
@@ -3,8 +3,8 @@
 c
       interface
 c
-        subroutine sim_get_pos_on_surf ( dx, dy, dz, numnp, px, py, pz )
-     &    bind(C, NAME='sim_get_pos_on_surf')
+        subroutine core_get_pos_on_surf ( dx, dy, dz, numnp, px, py, pz )
+     &    bind(C, NAME='core_get_pos_on_surf')
         use iso_c_binding
           integer(c_int),value :: numnp
           real(c_double),intent(in),dimension(:) ::
@@ -13,9 +13,9 @@ c
      &                               px(numnp), py(numnp), pz(numnp)
         end subroutine
 c
-        subroutine sim_is_in_closure ( e_dim, e_tag,
+        subroutine core_is_in_closure ( e_dim, e_tag,
      &                                 t_dim, t_tag, answer )
-     &    bind(C, NAME='sim_is_in_closure')
+     &    bind(C, NAME='core_is_in_closure')
         use iso_c_binding
           integer(c_int),intent(in),value :: e_dim, e_tag, t_dim, t_tag
           integer(c_int),intent(out) :: answer
