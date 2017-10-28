fd209c9bc0cfa66329148b704a2f215923b7fb81
Fan Yang <yangf4@rpi.edu>
2017-10-28 15:46:30 -0400

    [phasta] implicitly read sim license

--- phasta/chef.cc
+++ phasta/chef.cc
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
 #ifdef HAVE_SIMMETRIX
   MS_init();
   SimModel_start();
-  Sim_readLicenseFile("/net/common/meshSim/license/license.txt");
+  Sim_readLicenseFile(0);
   SimPartitionedMesh_start(0, 0);
 #ifdef HAVE_SIMADVMESHING
   SimAdvMeshing_start();
