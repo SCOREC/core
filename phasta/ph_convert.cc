fb4b82f64c08651b186813cc2d25117e2d5052df
Cameron Smith <smithc11@rpi.edu>
2017-11-07 15:09:48 -0500

    ph_convert: fix segfault after mpi_finalize
    ph::output was not being initialized

--- phasta/ph_convert.cc
+++ phasta/ph_convert.cc
@@ -270,6 +270,8 @@ int main(int argc, char** argv)
   phIn.displacementMigration = false;
   phIn.dwalMigration = false;
   phIn.buildMapping = true;
+  ph::BCs bcs;
+  ph::generateOutput(phIn, bcs, mesh, phOut);
   ph::detachAndWriteSolution(phIn, phOut, mesh, restartPath);
 
   mesh->destroyNative();
