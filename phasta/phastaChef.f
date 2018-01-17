08d54034a94fe0097be7d63eb1dcdf1c62b66d30
Cameron Smith <smithc11@rpi.edu>
2018-01-17 15:32:45 +0000

    fortran: xl didn't like the line continuation position

--- phasta/phastaChef.f
+++ phasta/phastaChef.f
@@ -1,46 +1,41 @@
       module core_snap
       use iso_c_binding
-c
       interface
-c
-        subroutine core_get_pos_on_surf (dx, dy, dz, numnp, px, py, pz)
-     &    bind(C, NAME='core_get_pos_on_surf')
+
+        subroutine core_get_pos_on_surf (dx, dy, dz, numnp, px, py, pz) &
+          bind(C, NAME='core_get_pos_on_surf')
         use iso_c_binding
           integer(c_int),value :: numnp
-          real(c_double),intent(in),dimension(:) ::
-     &                               dx(numnp), dy(numnp), dz(numnp)
-          real(c_double),intent(out),dimension(:) ::
-     &                               px(numnp), py(numnp), pz(numnp)
+          real(c_double),intent(in),dimension(:) :: &
+                                    dx(numnp), dy(numnp), dz(numnp)
+          real(c_double),intent(out),dimension(:) :: &
+                                    px(numnp), py(numnp), pz(numnp)
         end subroutine
-c
-        subroutine core_is_in_closure ( e_dim, e_tag,
-     &                                 t_dim, t_tag, answer )
-     &    bind(C, NAME='core_is_in_closure')
+
+        subroutine core_is_in_closure ( e_dim, e_tag, &
+                                       t_dim, t_tag, answer ) &
+          bind(C, NAME='core_is_in_closure')
         use iso_c_binding
           integer(c_int),intent(in),value :: e_dim, e_tag, t_dim, t_tag
           integer(c_int),intent(out) :: answer
         end subroutine
-c
+
       end interface
-c
       end module
-c-------------------------------------------------------------------------
-c
-c-------------------------------------------------------------------------
+
+
       module core_mesh_quality
       use iso_c_binding
-c
       interface
-c
-        subroutine core_measure_mesh(x1, x2, x3, numnp, minq)
-     &    bind(C, NAME='core_measure_mesh')
+
+        subroutine core_measure_mesh(x1, x2, x3, numnp, minq) &
+          bind(C, NAME='core_measure_mesh')
         use iso_c_binding
           integer(c_int),value :: numnp
-          real(c_double),intent(in),dimension(:) ::
-     &                               x1(numnp), x2(numnp), x3(numnp)
+          real(c_double),intent(in),dimension(:) :: &
+                                     x1(numnp), x2(numnp), x3(numnp)
           real(c_double),intent(out) :: minq
         end subroutine
-c
+
       end interface
-c
       end module
