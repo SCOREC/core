76e961b5f5450f5e7ad1a1d7392f68949b3f916d
Cameron Smith <smithc11@rpi.edu>
2018-01-19 07:34:37 -0500

    Revert "fortran: xl didn't like the line continuation position"
    This reverts commit 08d54034a94fe0097be7d63eb1dcdf1c62b66d30.

--- phasta/phastaChef.f
+++ phasta/phastaChef.f
@@ -1,41 +1,46 @@
       module core_snap
       use iso_c_binding
+c
       interface
-
-        subroutine core_get_pos_on_surf (dx, dy, dz, numnp, px, py, pz) &
-          bind(C, NAME='core_get_pos_on_surf')
+c
+        subroutine core_get_pos_on_surf (dx, dy, dz, numnp, px, py, pz)
+     &    bind(C, NAME='core_get_pos_on_surf')
         use iso_c_binding
           integer(c_int),value :: numnp
-          real(c_double),intent(in),dimension(:) :: &
-                                    dx(numnp), dy(numnp), dz(numnp)
-          real(c_double),intent(out),dimension(:) :: &
-                                    px(numnp), py(numnp), pz(numnp)
+          real(c_double),intent(in),dimension(:) ::
+     &                               dx(numnp), dy(numnp), dz(numnp)
+          real(c_double),intent(out),dimension(:) ::
+     &                               px(numnp), py(numnp), pz(numnp)
         end subroutine
-
-        subroutine core_is_in_closure ( e_dim, e_tag, &
-                                       t_dim, t_tag, answer ) &
-          bind(C, NAME='core_is_in_closure')
+c
+        subroutine core_is_in_closure ( e_dim, e_tag,
+     &                                 t_dim, t_tag, answer )
+     &    bind(C, NAME='core_is_in_closure')
         use iso_c_binding
           integer(c_int),intent(in),value :: e_dim, e_tag, t_dim, t_tag
           integer(c_int),intent(out) :: answer
         end subroutine
-
+c
       end interface
+c
       end module
-
-
+c-------------------------------------------------------------------------
+c
+c-------------------------------------------------------------------------
       module core_mesh_quality
       use iso_c_binding
+c
       interface
-
-        subroutine core_measure_mesh(x1, x2, x3, numnp, minq) &
-          bind(C, NAME='core_measure_mesh')
+c
+        subroutine core_measure_mesh(x1, x2, x3, numnp, minq)
+     &    bind(C, NAME='core_measure_mesh')
         use iso_c_binding
           integer(c_int),value :: numnp
-          real(c_double),intent(in),dimension(:) :: &
-                                     x1(numnp), x2(numnp), x3(numnp)
+          real(c_double),intent(in),dimension(:) ::
+     &                               x1(numnp), x2(numnp), x3(numnp)
           real(c_double),intent(out) :: minq
         end subroutine
-
+c
       end interface
+c
       end module
--- phasta/phiotimer.f
+++ phasta/phiotimer.f
@@ -2,13 +2,13 @@
       use :: iso_c_binding
       public
         enum, bind(C)
-          enumerator :: GEOMBC_READ, GEOMBC_WRITE, &
-                        RESTART_READ, RESTART_WRITE, &
-                        NUM_PHASTAIO_MODES
+          enumerator :: GEOMBC_READ, GEOMBC_WRITE,
+     &                  RESTART_READ, RESTART_WRITE,
+     &                  NUM_PHASTAIO_MODES
         end enum
       interface 
-        subroutine phastaio_setfile(fileidx) &
-         bind(C, NAME='phastaio_setfile')
+        subroutine phastaio_setfile(fileidx)
+     &   bind(C, NAME='phastaio_setfile')
         use :: iso_c_binding
           integer(c_int), intent(in), value:: fileidx
         end subroutine
