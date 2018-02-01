f782c21e0a5cf2d4208c6a14a9735d5766a2b2ea
Morteza H. Siboni <hakimi1364@gmail.com>
2018-02-01 13:20:13 -0500

    config file for capstone

---
+++ capstone-config.sh
@@ -0,0 +1,22 @@
+# # # #
+# The following line is for capstone compilation
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/lore/cwsmith/projects/pettt/capstone/rhel7/lib
+# # # #
+
+flags="-g -O0"
+cmake .. \
+  -DCMAKE_BUILD_TYPE="Debug" \
+  -DCMAKE_C_COMPILER="mpicc" \
+  -DCMAKE_CXX_COMPILER="mpicxx" \
+  -DCMAKE_EXE_LINKER_FLAGS="-lpthread ${flags}" \
+  -DSCOREC_CXX_OPTIMIZE=ON \
+  -DBUILD_EXES=ON \
+  -DMDS_ID_TYPE="int" \
+  -DENABLE_CAPSTONE=ON \
+  -DIS_TESTING=ON \
+  -DMESHES="$MESHES" \
+  -DENABLE_ZOLTAN=ON \
+  -DPCU_COMPRESS=ON \
+  -DENABLE_FPP=ON
+
+
