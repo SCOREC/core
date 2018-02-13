e8641ae42ff5a59e6dadeee98c04ce8496a279f0
Morteza H. Siboni <hakimi1364@gmail.com>
2018-02-13 12:18:05 -0500

    buildElement->MeshCAP::createEntity when using cap

--- ma/maAdapt.cc
+++ ma/maAdapt.cc
@@ -436,7 +436,11 @@ Entity* buildElement(
     int type,
     Entity** verts)
 {
+#ifdef HAVE_CAPSTONE
+  return a->mesh->createEntity_(type, c, verts);
+#else
   return apf::buildElement(a->mesh,c,type,verts,a->buildCallback);
+#endif
 }
 
 Entity* rebuildElement(
