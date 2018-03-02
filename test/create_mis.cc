784b07cc846b28179bc3988c028d38768f723a56
Lucas Davis <davisl3@rpi.edu>
2018-03-02 13:31:36 -0500

    Modified create_mis.cc to output the size of each color/set

--- test/create_mis.cc
+++ test/create_mis.cc
@@ -6,6 +6,8 @@
 #include <PCU.h>
 #include <cstdlib>
 #include <apfShape.h>
+#include <iostream>
+#include <map>
 
 int main(int argc, char** argv)
 {
@@ -28,15 +30,21 @@ int main(int argc, char** argv)
 
   //Set up a MIS with primary type as elements and adjacencies as vertices.
   apf::MIS* mis = apf::initializeMIS(m,m->getDimension(),0);
+  std::map<int,int> colors1;
 
   while (apf::getIndependentSet(mis)) {
     //This for loop can be thread parallized safetly
     for (int i=0;i<mis->n;i++) {
       //Independent work can be done here
       apf::setScalar(coloring,mis->ents[i],0,mis->color);
+      ++colors1[mis->color];
     }
   }
   apf::finalizeMIS(mis);
+  std::cout << "Number of colors used in test 1: " << colors1.size() << std::endl;
+  for (std::map<int,int>::iterator itr = colors1.begin(); itr != colors1.end(); ++itr) {
+    std::cout << "Size of color " << itr->first << " : " << itr->second << std::endl;
+  }
 
   //A second coloring over the vertices
   apf::Field* coloring2 = apf::createField(m,"colors2",apf::SCALAR,
@@ -44,13 +52,19 @@ int main(int argc, char** argv)
   //Another MIS example where primary types are vertices and
   //    adjacencies are edges
   mis = apf::initializeMIS(m,0,1);
+  std::map<int,int> colors2;
 
   while (apf::getIndependentSet(mis)) {
     for (int i=0;i<mis->n;i++) {
       apf::setScalar(coloring2,mis->ents[i],0,mis->color);
+      ++colors2[mis->color];
     }
   }
   apf::finalizeMIS(mis);
+  std::cout << "Number of colors used in test 2: " << colors2.size() << std::endl;
+  for (std::map<int,int>::iterator itr = colors2.begin(); itr != colors2.end(); ++itr) {
+    std::cout << "Size of color " << itr->first << " : " << itr->second << std::endl;
+  }
 
   //Vtk files should have a coloring of the elements
   //    to prove the coloring forms independent sets
