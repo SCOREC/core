82ae8f75eba1c6ea4fff040d6ad1bcc4f453f1a1
Morteza H. Siboni <hakimi1364@gmail.com>
2018-03-02 15:43:29 -0500

    minimal cmake command for compilation
    user needs to provide the path to capstone install and the path to
    install directory in capstone-config.sh

--- capstone-config.sh
+++ capstone-config.sh
@@ -1,22 +1,13 @@
 # # # #
-# The following line is for capstone compilation
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/lore/cwsmith/projects/pettt/capstone/rhel7/lib
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/path/to/capstone/install
 # # # #
 
 flags="-g -O0"
 cmake .. \
-  -DCMAKE_BUILD_TYPE="Debug" \
+  -DCMAKE_INSTALL_PREFIX:PATH="/path/to/install/directory" \
   -DCMAKE_C_COMPILER="mpicc" \
   -DCMAKE_CXX_COMPILER="mpicxx" \
-  -DCMAKE_EXE_LINKER_FLAGS="-lpthread ${flags}" \
+  -DCMAKE_EXE_LINKER_FLAGS="${flags}" \
   -DSCOREC_CXX_OPTIMIZE=ON \
-  -DBUILD_EXES=ON \
-  -DMDS_ID_TYPE="int" \
   -DENABLE_CAPSTONE=ON \
-  -DIS_TESTING=ON \
-  -DMESHES="$MESHES" \
-  -DENABLE_ZOLTAN=ON \
-  -DPCU_COMPRESS=ON \
   -DENABLE_FPP=ON
-
-
