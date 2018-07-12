b1c9544ad584017b94fa31b87b5a64d7acb9cc24
Ajinkya Dahale <dahala@rpi.edu>
2018-07-12 02:48:39 -0400

    [ma] Add single-split-collapse functionality
    [ma][sliver-fix] Replace sliver type table with 2-D array
    
    Takes into account the fact that a vertex may be too close to an edge,
    and that we might want to use a different fixer for it.
    
    Two more types are added to that effect in `LargeAngleTetFixer`:
    `EDGE_VERT` and `VERT_VERT`. Rotation in `matchSliver` is such that edge
    v_0--v_1 is too close to v_3 for `EDGE_VERT` and v_0 and v_3 are too
    close for `VERT_VERT`.
    
    [ma][sliver-fix] Add proximity to edges in `getSliverCode`
    
    Currently, edge-vert and vert-vert cases are treated the same as face-vert.
    
    [ma][sliver-fix] Logic for flat first face
    
    Similar code structure, except using projection of third vertex of first face onto the opposite edge.
    
    [ma][sliver-fix] Using face quality condition to choose identifiers
    
    Also tweaked the threshold for closeness,
    
    [ma][sliver-fix] Insert single split collapse into build
    
    [ma][sliver-fix] Insert single-split-collapse in large-angle-fixer
    
    Also removed some outputs used for debugging.

---
+++ ma/maSingleSplitCollapse.h
@@ -0,0 +1,38 @@
+/****************************************************************************** 
+
+  Copyright 2013 Scientific Computation Research Center, 
+      Rensselaer Polytechnic Institute. All rights reserved.
+  
+  The LICENSE file included with this distribution describes the terms
+  of the SCOREC Non-Commercial License this program is distributed under.
+ 
+*******************************************************************************/
+#ifndef MA_SINGLESPLITCOLLAPSE_H
+#define MA_SINGLESPLITCOLLAPSE_H
+
+#include "maSplits.h"
+#include "maCollapse.h"
+
+namespace ma {
+
+class SingleSplitCollapse
+{
+public:
+  SingleSplitCollapse(Adapt* a);
+  void getNewElements(EntityArray& e);
+  bool didImproveQuality();
+  bool tryThisCollapse();
+  bool tryBothCollapses(Entity* e);
+  void accept();
+  bool run(Entity* edge, Entity* vert);
+  Adapt* getAdapt();
+private:
+  Entity *oldVert, *oldEdge;
+  Splits splits;
+  Collapse collapse;
+  double oldQuality;
+};
+
+}
+
+#endif
