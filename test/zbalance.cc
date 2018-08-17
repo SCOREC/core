7e7324dc7de1f1d5ec93a8a0493b893aa6cbcb52
Cameron Smith <smithc11@rpi.edu>
2018-08-17 14:00:08 -0400

    zbalance: support null and geomsim models

--- test/zbalance.cc
+++ test/zbalance.cc
@@ -2,16 +2,37 @@
 #include <apfMDS.h>
 #include <apfMesh2.h>
 #include <apfZoltan.h>
+#include <gmi_null.h>
 #include <gmi_mesh.h>
 #include <parma.h>
 #include <PCU.h>
 #include <pcu_util.h>
+#ifdef HAVE_SIMMETRIX
+#include <gmi_sim.h>
+#include <SimUtil.h>
+#include <MeshSim.h>
+#include <SimModel.h>
+#endif
+
 
 int main(int argc, char** argv)
 {
-  PCU_ALWAYS_ASSERT(argc == 4);
   MPI_Init(&argc,&argv);
   PCU_Comm_Init();
+  if ( argc != 4 ) {
+    if ( !PCU_Comm_Self() )
+      printf("Usage: %s <model> <mesh> <output mesh prefix>\n", argv[0]);
+    MPI_Finalize();
+    exit(EXIT_FAILURE);
+  }
+#ifdef HAVE_SIMMETRIX
+  MS_init();
+  SimModel_start();
+  Sim_readLicenseFile(NULL);
+  gmi_sim_start();
+  gmi_register_sim();
+#endif
+  gmi_register_null();
   gmi_register_mesh();
   //load model and mesh
   apf::Mesh2* m = apf::loadMdsMesh(argv[1],argv[2]);
@@ -26,6 +47,12 @@ int main(int argc, char** argv)
   // destroy mds
   m->destroyNative();
   apf::destroyMesh(m);
+#ifdef HAVE_SIMMETRIX
+  gmi_sim_stop();
+  Sim_unregisterAllKeys();
+  SimModel_stop();
+  MS_exit();
+#endif
   PCU_Comm_Free();
   MPI_Finalize();
 }
