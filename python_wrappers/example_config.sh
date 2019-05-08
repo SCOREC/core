ea88a5aea12efcbce145d643fbc10f80c82b3b42
Morteza HS <hakimi1364@gmail.com>
2019-05-08 15:43:42 -0400

    Updated example_config.sh

--- python_wrappers/example_config.sh
+++ python_wrappers/example_config.sh
@@ -1,6 +1,6 @@
 cmake .. \
 	-DSCOREC_PREFIX="/usr/local/core_install" \
-	-DCMAKE_INSTALL_PREFIX="/usr/local/pcu_python" \
+	-DCMAKE_INSTALL_PREFIX="/usr/local/pyCore" \
   -DCMAKE_C_COMPILER="mpicc" \
   -DCMAKE_CXX_COMPILER="mpicxx" \
   -DENABLE_PYTHON=ON
