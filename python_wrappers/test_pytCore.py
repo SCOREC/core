3d5d66d9bf868e8011482d633b7ad4d551b41a24
Morteza HS <hakimi1364@gmail.com>
2019-05-09 10:31:33 -0400

    Updated test file.

--- python_wrappers/test_pytCore.py
+++ python_wrappers/test_pytCore.py
@@ -1,17 +1,22 @@
 from mpi4py import MPI
 import pyCore
 
-# print MPI.COMM_WORLD.Get_rank()
-
+# PCU initialization
 pyCore.PCU_Comm_Init()
-print pyCore.PCU_Time()
-print pyCore.PCU_Comm_Self()
 
-
+# gmi initialization
 pyCore.gmi_register_mesh()
 
+# load the mesh and model and write the initial mesh to vtk
 mesh = pyCore.loadMdsMesh('../cube.dmg', '../cube.smb')
+pyCore.writeASCIIVtkFiles('before', mesh);
 
+# setup uniform refiner and call mesh adapt
 ma_input = pyCore.configureUniformRefine(mesh, 2);
+pyCore.adapt(ma_input);
 
+# write the adapted mesh to vtk
+pyCore.writeASCIIVtkFiles('after', mesh);
+
+# gmi finalization
 pyCore.PCU_Comm_Free()
