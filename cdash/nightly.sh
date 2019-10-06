c243a98853cb40b211cdb61cdac3570ca2db1fbe
Cameron Smith <smithc11@rpi.edu>
2019-10-06 06:35:12 -0400

    update cdash scripts

--- cdash/nightly.sh
+++ cdash/nightly.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -x
 source /etc/profile
 source /users/cwsmith/.bash_profile
 
@@ -11,9 +11,9 @@ module use /opt/scorec/spack/lmod/linux-rhel7-x86_64/Core/
 
 module load gcc/7.3.0-bt47fwr
 module load cmake/3.12.1-wfk2b7e
-module load mpich/3.2.1-niuhmad
-module load zoltan/3.83-int32-p5tdxgl
-module load simmetrix-simmodsuite/12.0-180713-6dupwm4
+module load mpich
+module load zoltan/3.83-int32-gx3prjr
+module load simmetrix-simmodsuite/12.0-190225-5euzhd5
 
 #cdash output root
 d=/lore/cwsmith/nightlyBuilds/
