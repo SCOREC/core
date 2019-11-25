02a1b0bfbe64ca1baca601bc8f2581d0a1a2804d
Morteza H. Siboni <hakimi1364@gmail.com>
2019-11-25 12:48:46 -0500

    moved comments to the beginnig of example_config

--- python_wrappers/example_config.sh
+++ python_wrappers/example_config.sh
@@ -1,9 +1,11 @@
+# DSCOREC_PREFIX should point to the location of SCOREC/core shared libs
+# DSIM_MPI should be set to relevant mpi build
 cmake .. \
-  -DSCOREC_PREFIX="/lore/hakimm2/opt/coresimso" \ # point to the location of SCOREC/core shared libs
+  -DSCOREC_PREFIX="/lore/hakimm2/opt/coresimso" \
   -DCMAKE_INSTALL_PREFIX="../install" \
   -DCMAKE_C_COMPILER="mpicc" \
   -DCMAKE_CXX_COMPILER="mpicxx" \
   -DENABLE_SIMX=ON \
-  -DSIM_MPI=mpich3.3 \ # has to be changed to the relevant openmpi build if using openmpi
+  -DSIM_MPI=mpich3.3 \
   -DSIM_PARASOLID=ON \
   -DENABLE_PYTHON=ON
