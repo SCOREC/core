5158023e137e6bb12c72472aff01f85b83e3b61d
Morteza H. Siboni <hakimi1364@gmail.com>
2019-11-25 11:03:40 -0500

    added comments to config file

--- python_wrappers/example_config.sh
+++ python_wrappers/example_config.sh
@@ -1,9 +1,9 @@
 cmake .. \
-  -DSCOREC_PREFIX="/lore/hakimm2/opt/coresimso" \
+  -DSCOREC_PREFIX="/lore/hakimm2/opt/coresimso" \ # point to the location of SCOREC/core shared libs
   -DCMAKE_INSTALL_PREFIX="../install" \
   -DCMAKE_C_COMPILER="mpicc" \
   -DCMAKE_CXX_COMPILER="mpicxx" \
   -DENABLE_SIMX=ON \
-  -DSIM_MPI=mpich3.3 \
+  -DSIM_MPI=mpich3.3 \ # has to be changed to the relevant openmpi build if using openmpi
   -DSIM_PARASOLID=ON \
   -DENABLE_PYTHON=ON
