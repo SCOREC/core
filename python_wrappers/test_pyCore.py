8b71d1db1f0910cf868ef1e75ccd85ee699c5051
Morteza <mortezah@users.noreply.github.com>
2019-12-11 15:55:10 -0500

    Python wrappers (#280)
    * Initial commit for python-wrappers
    
    This commits does the wrappers for some of the PCU APIs
    
    For now everything is done using swig. That is "swig" is call
    automatically during cmake configure process and _.so (python Module)
    files are build during the make process.
    
    In the future it might be beneficial to also include build instructions
    using the "setup.py" approach.
    
    * Renamed to python module to "pcu" to be consistent
    
    Also added wrappers for PCU_Time and PCU_ASSERTS
    
    * Moved the wrapper related things to a new folder
    
    * Updates to the wrappers after moving to a new folder
    
    * For now everything is in a single module called pyCore
    
    pyCore's intention is to expose minimal functionality of scorec core so
    ma::adapt can be called from python.
    
    In the future we might want to have a seperate python module for each
    library in SCOREC::core, i.e., pyApf for libapf, pyGmi for libgmi and so
    on.
    
    * Updated example_config.sh
    
    * Updated README.md and added a test python script
    
    * Updated test file.
    
    * Some updates to the CMakeLists.txt
    
    * test_pyCore.py now gets mesh/model location as input
    
    * Updated README to reflect changes in previous commit
    
    * helper wrappers to start/stop SimModSuite
    
    * Rest of apis needed for adapt and size field computation
    
    * Added simx helpers
    
    * Updated test with simmx
    
    * Updated README
    
    * Added PCU_Comm_Initialized
    
    * Added get vert numbering and apfShape
    
    * Optimization pragma for python wrappers + adjRegions
    
    * AdaptVerbose + getIPxyz
    
    * Minor update to simhelper
    
    * Changed verbosity level in test
    
    * python version removed from cmakelist
    
    * made FindMPI4PY with python 3
    
    * took care of __attribute__ problem in apf::fail
    
    * directives added to handle builds without simmetrix
    
    * added the install target for pyCore
    
    * install target for swig needed more than just install
    
    * made test_pyCore.py compatible with pythong 3
    
    * made python test with simx compatible with version 3
    
    * removed unused files
    
    * updated FindSimModSuite
    
    * Corrected the name of the test files
    
    * Added sample meshes and models to "input"
    
    * added comments to config file
    
    * updated the README
    
    * moved comments to the beginnig of example_config
    
    * added -DHAVE_SIMMETRIX CMAKE_SWIG_FLAGS when simx is on
    
    * Removed the #pragma s from apf/apfMatrix.h
    
    These where added since optimized build does not include the object of
    routines that are not used in the shared libraries and this would have
    caused problems when trying to build pyCore
    
    * FindMPI4PY is now consistent with python 3
    
    * Point wrapper CMakeList to correct location for modules
    
    * update README.md for python_wrappers
    
    * remove repeated cmake modules
    
    * does not add core include dirs to swig compile line
    
    * appears to work - tested without simmetrix
    
    * Includes build with simx capability for pyCore
    
    * Updates README.md and python_wrappers/README.md
    
    * Adds example config to build with python interface

---
+++ python_wrappers/test_pyCore.py
@@ -0,0 +1,47 @@
+from mpi4py import MPI
+import pyCore
+
+import sys, getopt
+
+def main(argv):
+   model = ''
+   mesh  = ''
+   try:
+      opts, args = getopt.getopt(argv,"hg:m:",["model=","mesh="])
+   except getopt.GetoptError:
+      print('test_pytCore.py -g <model> -m <mesh>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('test_pytCore.py -g <model> -m <mesh>')
+         sys.exit()
+      elif opt in ("-g", "--model"):
+         model = arg
+      elif opt in ("-m", "--mesh"):
+         mesh = arg
+   print('Model file is "', model)
+   print('Mesh  file is "', mesh)
+
+   # PCU initialization
+   pyCore.PCU_Comm_Init()
+
+   # gmi initialization
+   pyCore.gmi_register_mesh()
+
+   # load the mesh and model and write the initial mesh to vtk
+   mesh = pyCore.loadMdsMesh(model, mesh)
+   pyCore.writeASCIIVtkFiles('before', mesh);
+
+   # setup uniform refiner and call mesh adapt
+   ma_input = pyCore.configureUniformRefine(mesh, 2);
+   pyCore.adapt(ma_input);
+
+   # write the adapted mesh to vtk
+   pyCore.writeASCIIVtkFiles('after', mesh);
+
+   # gmi finalization
+   pyCore.PCU_Comm_Free()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
