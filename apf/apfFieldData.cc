96f8a528fb2b3dff003db3d92393c77359721246
Morteza H. Siboni <hakimi1364@gmail.com>
2020-04-13 14:34:31 -0400

    Treats vector shapes differently in getElementData
    Note that vector shapes for edges have to account for ordering and
    directions since even for first order nedelc elements

--- apf/apfFieldData.cc
+++ apf/apfFieldData.cc
@@ -237,16 +237,29 @@ int FieldDataOf<T>::getElementData(MeshEntity* entity, NewArray<T>& data)
       int na = mesh->getDownward(entity,d,a);
       for (int i = 0; i < na; ++i) {
         int nan = fs->countNodesOn(mesh->getType(a[i]));
-        if (nan > 1 && ed != d) { /* multiple shared nodes, check alignment */
-          order.setSize(nen); /* nen >= nan */
-          adata.setSize(nen); /* setSize is no-op for the same size */
-          es->alignSharedNodes(mesh, entity, a[i], &order[0]);
-          get(a[i], &adata[0]);
-          reorderData<T>(&adata[0], &data[n], &order[0], nc, nan);
-        } else if (nan) { /* non-zero set of nodes, either one
-                             or not shared */
-          get(a[i], &data[n]);
-        }
+        // for vector shapes (i.e., nedelec) direction matters for nan>=1
+        if (fs->isVectorShape()) {
+	  if (nan >= 1 && ed != d) {
+	    order.setSize(nen);
+	    adata.setSize(nen);
+	    es->alignSharedNodes(mesh, entity, a[i], &order[0]);
+	    get(a[i], &adata[0]);
+	    reorderData<T>(&adata[0], &data[n], &order[0], nc, nan);
+	  }
+	}
+        // for non vector shapes direction matters for nan>1
+	else {
+	  if (nan > 1 && ed != d) { /* multiple shared nodes, check alignment */
+	    order.setSize(nen); /* nen >= nan */
+	    adata.setSize(nen); /* setSize is no-op for the same size */
+	    es->alignSharedNodes(mesh, entity, a[i], &order[0]);
+	    get(a[i], &adata[0]);
+	    reorderData<T>(&adata[0], &data[n], &order[0], nc, nan);
+	  } else if (nan) { /* non-zero set of nodes, either one
+			      or not shared */
+	    get(a[i], &data[n]);
+	  }
+	}
         n += nc * nan;
       }
     }
