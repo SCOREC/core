ba84108043941ccca7fb690f1abba10aa9a1017e
Samiullah Malik <maliks2@portal2.scorec.rpi.edu>
2020-05-01 01:43:52 -0400

    Initial commit for Error estimator.
    Builds edge based patches and assembles LHS topology matrices on
    both the interior and the boundary edge patches.

--- em/emResidualFunctionals.cc
+++ em/emResidualFunctionals.cc
@@ -4,23 +4,194 @@
  * This work is open source software, licensed under the terms of the
  * BSD license as described in the LICENSE file in the top-level directory.
  */
+#include <iostream>
 
 #include <PCU.h>
 #include <lionPrint.h>
 
-#include "spr.h"
+#include "em.h"
 
 #include <apfMesh.h>
 #include <apfShape.h>
+#include <apfField.h>
 #include <apfCavityOp.h>
+#include "crv.h"
+#include "crvShape.h"
+
+#include <mthQR.h>
+#include <mth.h>
+#include <mth_def.h>
 
 #include <limits>
 
+#include <set>
+#include <pcu_util.h>
+
 namespace em {
 
+/* overall information useful during equilibration */
+struct Equilibration {
+  apf::Mesh* mesh;
+  /* mesh dimension, so far handling 3 only */
+  int dim;
+  /* polynomial order of Nedelec space */
+  int order;
+  /* input scalar field containing Nedelec dofs for electric field */
+  apf::Field* ef;
+};
+
+static void setupEquilibration(Equilibration* eq, apf::Field* f)
+{
+  eq->mesh = apf::getMesh(f);
+  eq->dim = eq->mesh->getDimension();
+  eq->ef = f;
+  eq->order = f->getShape()->getOrder();
+}
+
+struct QRDecomp {
+  mth::Matrix<double> Q;
+  mth::Matrix<double> R;
+};
+
+typedef std::set<apf::MeshEntity*> EntitySet;
+
+struct EdgePatch {
+  apf::Mesh* mesh;
+  Equilibration* equilibration;
+  /* the entity around which the patch
+     is centered. a patch collects entities
+     (faces & tets) around this edge entity */
+  apf::MeshEntity* entity;
+  EntitySet tets;
+  EntitySet faces;
+  mth::Matrix<double> A;
+  QRDecomp qr;
+};
+
+static void setupEdgePatch(EdgePatch* p, Equilibration* eq)
+{
+  p->mesh = eq->mesh;
+  p->equilibration = eq;
+  p->entity = 0;
+}
+
+static void startEdgePatch(EdgePatch* p, apf::MeshEntity* e)
+{
+  p->tets.clear();
+  p->faces.clear();
+  p->entity = e;
+}
+
+static void addEntityToPatch(EdgePatch* p, apf::MeshEntity* e)
+{
+  if(p->mesh->getType(e) == apf::Mesh::TRIANGLE)
+    p->faces.insert(e);
+  if(p->mesh->getType(e) == apf::Mesh::TET)
+    p->tets.insert(e);
+}
+
+static void addEntitiesToPatch(EdgePatch* p, apf::DynamicArray<apf::MeshEntity*>& es)
+{
+  for (std::size_t i=0; i < es.getSize(); ++i)
+    addEntityToPatch(p, es[i]);
+}
+
+static bool getInitialEdgePatch(EdgePatch* p, apf::CavityOp* o)
+{
+  if ( ! o->requestLocality(&p->entity,1))
+    return false;
+  apf::DynamicArray<apf::MeshEntity*> adjacent;
+  p->mesh->getAdjacent(p->entity, 3, adjacent);
+  addEntitiesToPatch(p, adjacent);
+  p->mesh->getAdjacent(p->entity, 2, adjacent);
+  addEntitiesToPatch(p, adjacent);
+  return true;
+}
+
+static bool buildEdgePatch(EdgePatch* p, apf::CavityOp* o)
+{
+  if (!getInitialEdgePatch(p, o)) return false;
+  return true;
+}
+
+static void assembleLHS(EdgePatch* p)
+{
+  int ne = p->tets.size();
+  int nf = p->faces.size();
+  printf("ne %d nf %d \n", ne, nf); // TODO remove
+  if( crv::isBoundaryEntity(p->mesh, p->entity) ) {
+    p->A.resize(ne+nf, ne+nf);
+    p->A.zero();
+    for (int i = 0; i < nf; i++)
+      p->A(i,i) = 2.;
+    for (int i = 0; i < ne-1; i++) {
+      p->A(i+nf,i) = 1.; p->A(i+nf,i+1) = -1.;
+      p->A(i,i+nf) = 1.; p->A(i+1,i+nf) = -1.;
+    }
+    p->A(ne+nf-1, ne-1) = 1.; p->A(ne+nf-1, ne) = 1.;
+    p->A(ne-1, ne+nf-1) = 1.; p->A(ne, ne+nf-1) = 1.;
+
+    std::cout << "boundary" << std::endl; // TODO remove
+    std::cout << p->A << std::endl; // TODO remove
+  }
+  else if( ! crv::isBoundaryEntity(p->mesh, p->entity) ) {
+    mth::Matrix<double> m(ne, nf);
+    m.zero();
+    for (int i = 0; i < ne-1; i++) {
+      m(i,i) = 1.;  m(i,i+1) = -1.;
+    }
+    m(ne-1,0) = -1.; m(ne-1,ne-1) = 1.;
+
+    // m is singular so do (m*mt) + 1.0
+    // to pick a particular solution
+    mth::Matrix<double> mt(nf,ne);
+    mth::transpose(m, mt);
+    mth::multiply(m, mt, p->A);
+
+    for (int i = 0; i < ne; i++)
+      for (int j = 0; j < ne; j++)
+        p->A(i,j) += 1.;
+    std::cout << "interior" << std::endl; // TODO remove
+    std::cout << p->A << std::endl; // TODO remove
+  }
+}
+
+static void runErm(EdgePatch* p)
+{
+  assembleLHS(p);
+}
 
 
+class EdgePatchOp : public apf::CavityOp
+{
+public:
+  EdgePatchOp(Equilibration* eq):
+    apf::CavityOp(eq->mesh)
+  {
+    setupEdgePatch(&edgePatch, eq);
+  }
+  virtual Outcome setEntity(apf::MeshEntity* e)
+  {
+    startEdgePatch(&edgePatch, e);
+    if ( ! buildEdgePatch(&edgePatch, this))
+      return REQUEST;
+    return OK;
+    return SKIP;
+  }
+  virtual void apply()
+  {
+    runErm(&edgePatch);
+  }
+  EdgePatch edgePatch;
+};
 
+void equilibrateResiduals(apf::Field* f)
+{
+  Equilibration equilibration;
+  setupEquilibration(&equilibration, f);
+  EdgePatchOp op(&equilibration);
+  op.applyToDimension(1); // edges
+}
 
 
 
