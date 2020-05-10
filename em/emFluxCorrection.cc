c90015ac54d5457e10e9de1cf13a748fc6366111
Samiullah Malik <maliks2@portal2.scorec.rpi.edu>
2020-05-10 12:43:12 -0400

    Adds computeFaceMassMatrix and fixes bug.

--- em/emFluxCorrection.cc
+++ em/emFluxCorrection.cc
@@ -37,6 +37,66 @@ struct QRDecomp {
   mth::Matrix<double> R;
 };
 
+static void assembleFaceMassMatrix(apf::Mesh* mesh,apf::MeshEntity* e,
+    apf::Field* f, mth::Matrix<double>& elmat)
+{
+  apf::FieldShape* fs = f->getShape();
+  int type = mesh->getType(e);
+  int nd = apf::countElementNodes(fs, type);
+  int dim = apf::getDimension(mesh, e);
+  double w;
+
+  apf::NewArray<apf::Vector3> vectorshape(nd);
+  elmat.resize(nd,nd);
+
+  apf::MeshElement* me = apf::createMeshElement(mesh, e);
+  apf::Element* el = apf::createElement(f, me);
+  int order = 2 * fs->getOrder();
+  int np = apf::countIntPoints(me, order); // int points required
+
+  apf::Downward edges;
+  int ned =  mesh->getDownward(e, 1, edges);
+  PCU_ALWAYS_ASSERT(ned == 3);
+  int which, rotate; bool flip;
+
+  elmat.zero();
+  apf::Vector3 p;
+  for (int i = 0; i < np; i++) {
+    apf::getIntPoint(me, order, i, p);
+    double weight = apf::getIntWeight(me, order, i);
+    apf::Matrix3x3 J;
+    apf::getJacobian(me, p, J);
+    double jdet = apf::getJacobianDeterminant(J, dim);
+    w = weight * jdet;
+
+    apf::getVectorShapeValues(el, p, vectorshape);
+    mth::Matrix<double> vectorShape (nd, dim);
+    for (int j = 0; j < nd; j++)
+      for (int k = 0; k < dim; k++)
+        vectorShape(j,k) = vectorshape[j][k];
+
+    // negate negatve dof indices
+    for (int ei = 0; ei < ned; ei++) {
+      apf::getAlignment(mesh, e, edges[ei], which, flip, rotate);
+      if(flip) {
+        for (int j = 0; j < dim; j++)
+          vectorShape(ei, j) = -1*vectorShape(ei, j);
+      }
+    }
+
+    mth::Matrix<double> vectorShapeT (dim, nd);
+    mth::transpose(vectorShape, vectorShapeT);
+    mth::Matrix<double> M (nd,nd);
+    M.zero();
+    mth::multiply(vectorShape, vectorShapeT, M);
+    M *= w;
+    elmat += M;
+  }
+  apf::destroyElement(el);
+  apf::destroyMeshElement(me);
+}
+
+
 // This function takes the NedelecField and computes
 // correction vectors on faces and stores them in a field
 apf::Field* computeFluxCorrection(apf::Field* ef, apf::Field* g)
@@ -62,7 +122,7 @@ apf::Field* computeFluxCorrection(apf::Field* ef, apf::Field* g)
 
     // assemlbe face mass matrix
     mth::Matrix<double> M;
-    assembleVectorMassElementMatrix(
+    assembleFaceMassMatrix(
         apf::getMesh(g), face, faceNedelecField, M);
 
     // solve the system
