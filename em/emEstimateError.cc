fe5931b32ac26f1f410555438f0b405dab3ee799
Samiullah-Malik <39503305+Samiullah-Malik@users.noreply.github.com>
2020-05-13 18:30:57 -0400

    Applies DBCs to BVPs & computes per-element l2-err

--- em/emEstimateError.cc
+++ em/emEstimateError.cc
@@ -281,16 +281,181 @@ static void computeLambdaVector(apf::Mesh* mesh, apf::MeshEntity* e,
   } // end face loop
 }
 
+static void getEssentialBdrElementNDDofs(apf::Mesh* mesh, apf::MeshEntity* e,
+    apf::Field* f, apf::NewArray<int>& ess_dofs, apf::NewArray<int>& uness_dofs)
+{
+  apf::FieldShape* fs = f->getShape();
+  int etype = mesh->getType(e);
+  PCU_ALWAYS_ASSERT(etype == apf::Mesh::TET);
+  int nedofs = apf::countElementNodes(fs, etype);
+
+  apf::NewArray<int> marker_list (nedofs);
+  for (int i = 0; i < nedofs; i++)
+    marker_list[i] = 0;
+
+  // populate marker list by iterating over downward edges and faces
+  apf::Downward edges;
+  int nedges = mesh->getDownward(e, 1, edges);
+  PCU_ALWAYS_ASSERT(nedges == 6);
+  for (int i = 0; i < nedges; i++) {
+    int nodesOnEdge = fs->countNodesOn(mesh->getType(edges[i]));
+    if ( crv::isBoundaryEntity(mesh, edges[i]) ) {
+      for (int n = 0; n < nodesOnEdge; n++) {
+        marker_list[(i*nodesOnEdge)+n] = -1;
+      }
+    }
+  }
+  int nodesOnEdges = fs->countNodesOn(apf::Mesh::TRIANGLE) * nedges;
+
+  apf::Downward faces;
+  int nfaces = mesh->getDownward(e, 2, faces);
+  PCU_ALWAYS_ASSERT(nedges == 4);
+  for (int i = 0; i < nfaces; i++) {
+    int nodesOnFace = fs->countNodesOn(mesh->getType(faces[i]));
+    if ( crv::isBoundaryEntity(mesh, faces[i]) ) {
+      for (int n = 0; n < nodesOnEdge; n++) {
+        marker_list[(nodesOnEdges + i*nodesOnFace)+n] = -1;
+      }
+    }
+  }
+
+  int num_marked = 0;
+  for (int i = 0; i < nedofs; i++) {
+    if (marker_list[i])
+      num_marked++;
+  }
+
+  // use marker list to get ess_dofs list
+  ess_dofs.allocated() ? ess_dofs.resize(num_marked)
+    : ess_dofs.allocate(num_marked);
+  uness_dofs.allocated() ? uness_dofs.resize(nedofs-num_marked)
+    : uness_dofs.allocate(nedofs-num_marked);
+  int ess_dof_counter = 0;
+  int uness_dof_counter = 0;
+  for (int i = 0; i < nedofs; i++) {
+    if(marker_list[i])
+      ess_dofs[ess_dof_counter++] = i;
+    else
+      uness_dofs[uness_dofs_counter++] = i;
+  }
+}
+ 
+/**
+ * Inputs: Matrix A, Vector X, Vector B, essential dofs, not essential dofs.
+ * Output: reduced matrix A, reduced rhs B.
+ */
+static void eliminateDBCs(
+    mth::Matrix<double> const A,
+    mth::Vector<double> const X,
+    mth::Vector<double> const B, 
+    apf::NewArray<int> ess_dofs,
+    apf::NewArray<int> uness_dofs,
+    mth::Matrix<double>& Anew,
+    mth::Vector<double>& Bnew)
+{
+  int num_ess_dofs = ess_dofs.size();
+  int num_uness_dofs = uness_dofs.size();
+
+  // 1. Remove rows and cols of A corresponding to
+	// ess_dofs by copying into Anew
+  Anew.resize(num_uness_dofs, num_uness_dofs);
+	for(int rr = 0; rr < num_uness_dofs; rr++) {
+    int i = uness_dofs[rr];
+    for(int cc = 0; cc < num_uness_dofs; cc++) {
+      int j = uness_dofs[cc];
+      Anew(rr,cc) = A(i,j);
+    }    
+  }
+
+	// 2. Assemble new B
+	Bnew.resize(num_uness_dofs, num_uness_dofs);
+  for(int i = 0; i < num_uness_dofs; i++) {
+    Bnew(i) = B(uness_dofs[i]);
+  }
+
+	// 3. Subtract from Bnew: (Bnew -= Ae*Xe)
+  mth::Matrix<double> Ae(num_uness_dofs, num_ess_dofs);
+  for(int rr = 0; rr < num_uness_dofs; rr++) {
+    int i = uness_dofs[rr];
+    for(int cc = 0; cc < num_ess_dofs; cc++) {
+      int j = ess_dofs[cc];
+      Ae(rr,cc) = A(i,j);
+    }    
+  }
+
+  mth::Vector<double> Xe(num_ess_dofs);
+  for(int i = 0; i < num_ess_dofs; i++) {
+    Xe(i) = X(ess_dofs[i]);
+  }
+
+  mth::Vector<double> temp;
+  mth::multiply(Ae, Xe, temp);
+  Bnew -= temp;	
+}
+
+static double computeL2Error(apf::Mesh* mesh, apf::MeshEntity* e,
+	apf::Field* f, mth::Vector<double> const error_dofs)
+{
+	double error = 0.0;
+
+  apf::FieldShape* fs = f->getShape();
+  int type = mesh->getType(e);
+	PCU_ALWAYS_ASSERT(type == apf::Mesh::TET);
+  int nd = apf::countElementNodes(fs, type);
+  int dim = apf::getDimension(mesh, e);
+  double w;
+
+  apf::MeshElement* me = apf::createMeshElement(mesh, e);
+  apf::Element* el = apf::createElement(f, me);
+  int order = 2 * fs->getOrder();
+  int np = apf::countIntPoints(me, order);
+
+  apf::NewArray<apf::Vector3> vectorshape(nd);
+
+  for (int i = 0; i < np; i++) {
+    apf::getIntPoint(me, order, i, p);
+    double weight = apf::getIntWeight(me, order, i);
+    apf::Matrix3x3 J;
+    apf::getJacobian(me, p, J);
+    double jdet = apf::getJacobianDeterminant(J, dim);
+    w = weight * jdet;
+	
+    apf::getVectorShapeValues(el, p, vectorshape);
+		// TODO take care of negative dof values
+    mth::Matrix<double> vectorShape (nd, dim);
+    for (int j = 0; j < nd; j++)
+      for (int k = 0; k < dim; k++)
+        vectorShape(j,k) = vectorshape[j][k];
+
+		apf::Matrix<double> vectorShapeT (dim, nd);
+    mth::transpose(vectorShape, vectorShapeT);
+		
+		mth::Vector<double> err_func;
+		mth::multiply(vectorShapeT, error_dofs, err_func);
+
+		error += w * (err_func * err_func);
+	}
+	if (error < 0.0)
+		error = -error;
+
+	return sqrt(error);
+}
+
 // @ apf::Field* ef --> input p order ND electric field
 // @ apf::Field* ef --> input theta corrected flux field
 // *** p+1 order field is created inside this function
 apf::Field* emEstimateError(apf::Field* ef, apf::Field* correctedFlux)
 {
-  // 1. Create one order higher ND field
+
+	// 1. Create per-element SCALAR error field
+  apf::Field* error_field = apf::createIPField(
+		apf::getMesh(ef), "residual_error_field", apf::SCALAR, 1); // TODO maybe use getConstant here
+	
+  // 2. Create one order higher ND field
   int order = ef->getShape()->getOrder();
   int orderp1 = order+1;
-  apf::Field* efp1 = apf::createField(
-      apf::getMesh(ef), "higher_order_nedelec_field", apf::SCALAR, apf::getNedelec(orderp1));
+  apf::Field* efp1 = apf::createField(apf::getMesh(ef),
+	 "orderp1_nedelec_field", apf::SCALAR, apf::getNedelec(orderp1));
   apf::zeroField(efp1);
 
   // 2. iterate over all elements of the mesh
@@ -298,8 +463,8 @@ apf::Field* emEstimateError(apf::Field* ef, apf::Field* correctedFlux)
   apf::MeshIterator* it = apf::getMesh(ef)->begin(3);
   while ((el = apf::getMesh(ef)->iterate(it))) {
     // 2(a). Assemble LHS element matrix
-    mth::Matrix<double> lhs;
-    assembleElementMatrix( apf::getMesh(ef), el, efp1, lhs);
+    mth::Matrix<double> A;
+    assembleElementMatrix( apf::getMesh(ef), el, efp1, A);
     // TODO Take care of negative dofs in lhs
 
     // 2(b). Compute Bilinear Form Vector 
@@ -316,13 +481,45 @@ apf::Field* emEstimateError(apf::Field* ef, apf::Field* correctedFlux)
     mth::Vector<double> lambda;
     computeLambdaVector(apf::getMesh(efp1), e, ef, efp1, correctedFlux, lambda);
     // TODO Take care of negative dofs in lambda
-   
-     
+    
+    // 2(e). Assemble RHS element vector = blf - lf - lambda
+    mth::Vector<double> B = blf - lf - lambda; // TODO syntax
 
+    // 2(f). Get List of Essential Dofs
+    apf::NewArray<int> ess_dofs, uness_dofs;
+    getEssentialBdrElementNDDofs(
+        apf::getMesh(efp1), e, efp1, ess_dofs, uness_dofs);
 
+    // 2(g). eliminate Dirichlet (Essential) Boundary Conditions
+		mth::Vector<double> X, Anew, Bnew;
+		X.resize(B.size());
+		X.zero(); // initialize X with exact DBC (e = 0.0)
+		eliminateDBCs(A, X, B, ess_dofs, uness_dofs, Anew, Bnew);
+
+		// 2(h). Solve the reduced system
+	  mth::Matrix<double> Q, R;
+		mth::decompose(Anew, Q, R);
+		mth::Vector<double> Xnew;
+		mth::solveFromQR(Q, R, Bnew, Xnew);
+
+		// 2(i). Recover the solution
+		mth::Vector<double> error_dofs(B.size());
+    for(int i = 0; i < ess_dofs.size(); i++) {
+    	int index = ess_dofs[i];
+      error_dofs(index) = X(index);
+    }
+    for(int i = 0; i < uness_dofs.size(); i++) {
+      int index = uness_dofs[i];
+      error_dofs(index) = Xnew(i);
+    }
+
+		// 2(j). Compute L2 Norm Error
+		double l2_error = computeL2Error(mesh, e, efp1, error_dofs);
+		apf::setScalar(error_field, e, 0, l2_error);
   }
   apf::getMesh(ef)->end(it);
 
+	apf::destroyField(efp1);
 
-
+	return error_field;
 }
