202fcca13469bb36bf373e92211021943cb695a9
Morteza H. Siboni <hakimi1364@gmail.com>
2020-10-29 19:37:37 -0400

    Updates testing.cmake to include the models

--- test/testing.cmake
+++ test/testing.cmake
@@ -520,19 +520,19 @@ mpi_test(nedelec 1
 set(MDIR ${MESHES}/cube/pumi670)
 mpi_test(l2_shape_tet_serial 1
   ./L2Shapes
-  ".null"
+  "${MDIR}/../cube.dmg"
   "${MDIR}/cube.smb")
 mpi_test(l2_shape_tet_parallel 4
   ./L2Shapes
-  ".null"
+  "${MDIR}/../cube.dmg"
   "${MDIR}/4/cube.smb")
 mpi_test(h1_shape_serial 1
   ./H1Shapes
-  ".null"
+  "${MDIR}/../cube.dmg"
   "${MDIR}/cube.smb")
 mpi_test(h1_shape_parallel 4
   ./H1Shapes
-  ".null"
+  "${MDIR}/../cube.dmg"
   "${MDIR}/4/cube.smb")
 set(MDIR ${MESHES}/cube/pumi24)
 mpi_test(pumiLoadMesh-1p 1
