7bc29a3d2aba20bb04dd006e52cf25e52310491f
Samiullah Malik <maliks2@romulus.scorec.rpi.edu>
2020-12-03 15:36:11 -0500

    Adds residual based error estimation test

---
+++ test/residualErrorEstimation_test.cc
@@ -0,0 +1,51 @@
+#include <em.h>
+#include <apf.h>
+#include <gmi_mesh.h>
+#include <apfMDS.h>
+#include <apfShape.h>
+#include <PCU.h>
+#include <lionPrint.h>
+#ifdef HAVE_SIMMETRIX
+#include <gmi_sim.h>
+#include <SimUtil.h>
+#include <MeshSim.h>
+#include <SimModel.h>
+#endif
+#include <pcu_util.h>
+
+
+int main(int argc, char** argv)
+{
+  PCU_ALWAYS_ASSERT(argc==3);
+  const char* modelFile = argv[1];
+  const char* meshFile = argv[2];
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  lion_set_verbosity(1);
+#ifdef HAVE_SIMMETRIX
+  MS_init();
+  SimModel_start();
+  Sim_readLicenseFile(0);
+  gmi_sim_start();
+  gmi_register_sim();
+#endif
+  gmi_register_mesh();
+  ma::Mesh* m = apf::loadMdsMesh(modelFile,meshFile);
+  m->verify();
+
+  apf::Field* electric_field = m->getField(0);
+  apf::Field* implicit_error_field = em::estimateError(electric_field);
+
+  m->destroyNative();
+  apf::destroyMesh(m);
+#ifdef HAVE_SIMMETRIX
+  gmi_sim_stop();
+  Sim_unregisterAllKeys();
+  SimModel_stop();
+  MS_exit();
+#endif
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
+
+
