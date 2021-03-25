2e505f6ba61cb8258a7bf821e81536f4bbfd431d
Tucker Babcock <tbabcock03@gmail.com>
2021-03-24 17:12:47 -0700

    using cmake's configure_file to define the locations of the egads and egadslite libraries

--- cmake/FindEGADS.cmake
+++ cmake/FindEGADS.cmake
@@ -3,6 +3,7 @@
 #  EGADS_FOUND - System has EGADS
 #  EGADS_INCLUDE_DIRS - The EGADS include directories
 #  EGADS_LIBRARIES - The libraries needed to use EGADS
+#  EGADSLITE_LIBRARIES - The libraries needed to use EGADSlite
 #  EGADS_DEFINITIONS - Compiler switches required for using EGADS
 
 #set(EGADS_PREFIX "${EGADS_PREFIX_DEFAULT}" CACHE STRING "EGADS install directory")
@@ -11,27 +12,25 @@
 #endif()
 
 find_path(EGADS_INCLUDE_DIR egads.h PATHS "${EGADS_DIR}/include")
-
-option(EGADS_LITE "Use EGADS_LITE" OFF)
-if (EGADS_LITE)
-  message(STATUS "Using EGADSlite")
-  find_library(EGADS_LIBRARY NAMES egadslite PATHS "${EGADS_DIR}/lib")
-else()
-  message(STATUS "Using EGADS")
-  find_library(EGADS_LIBRARY NAMES egads libegads.so libegads.dylib PATHS "${EGADS_DIR}/lib")
-endif()
-
-set(EGADS_LIBRARIES ${EGADS_LIBRARY} )
 set(EGADS_INCLUDE_DIRS ${EGADS_INCLUDE_DIR} )
+
+find_library(EGADS_LIBRARY
+  NAMES egads libegads.so libegads.dylib
+  PATHS "${EGADS_DIR}/lib")
+set(EGADS_LIBRARIES ${EGADS_LIBRARY})
+
+find_library(EGADSLITE_LIBRARY
+  NAMES egadslite libegadslite.so libegadslite.dylib
+  PATHS "${EGADS_DIR}/lib")
+set(EGADSLITE_LIBRARIES ${EGADSLITE_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set EGADS_FOUND to TRUE
 # if all listed variables are TRUE
-message(STATUS "beep boop: ${EGADS_LIBRARY}")
 find_package_handle_standard_args(
     EGADS
     DEFAULT_MSG
-    EGADS_LIBRARY EGADS_INCLUDE_DIR
+    EGADS_INCLUDE_DIR EGADS_LIBRARY EGADSLITE_LIBRARY
 )
 
-mark_as_advanced(EGADS_INCLUDE_DIR EGADS_LIBRARY)
+mark_as_advanced(EGADS_INCLUDE_DIR EGADS_LIBRARY EGADSLITE_LIBRARY)
