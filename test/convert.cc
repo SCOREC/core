95126d85f7d03ace81164556c1af1b6f07cff99c
Kenneth E. Jansen <Kenneth.Jansen@colorado.edu>
2022-10-09 18:24:34 -0600

    compiles but wont link...retreat to see if develop has same issue

--- test/convert.cc
+++ test/convert.cc
@@ -2,6 +2,7 @@
 #include <lionPrint.h>
 #include <MeshSim.h>
 #include <SimPartitionedMesh.h>
+#include <SimAdvMeshing.h>
 #include <SimUtil.h>
 #include <apfSIM.h>
 #include <apfMDS.h>
@@ -11,14 +12,30 @@
 #include <apfConvert.h>
 #include <apfMesh2.h>
 #include <apfNumbering.h>
+#include <apfShape.h>
 #include <ma.h>
 #include <pcu_util.h>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <cassert>
 
 #include <getopt.h>
 
+apf::Field* convert_my_tag(apf::Mesh* m, apf::MeshTag* t) {
+  apf::MeshEntity* vtx;
+  apf::MeshIterator* it = m->begin(0);
+  apf::Field* f = apf::createFieldOn(m, "fathers2D_field", apf::SCALAR);
+  int vals[1];
+  double vals_d;
+  while ((vtx = m->iterate(it))) {
+    m->getIntTag(vtx, t, vals);
+    vals_d = vals[0];
+    apf::setScalar(f, vtx, 0, vals_d);
+  }
+  m->end(it);
+  return f;
+}
 
 static void attachOrder(apf::Mesh* m)
 {
@@ -54,6 +71,7 @@ const char* smb_path = NULL;
 int should_log = 0;
 int should_fix_pyramids = 1;
 int should_attach_order = 0;
+int ExtruRootId =0;
 bool found_bad_arg = false;
 
 void getConfig(int argc, char** argv) {
@@ -64,6 +82,7 @@ void getConfig(int argc, char** argv) {
     {"no-pyramid-fix", no_argument, &should_fix_pyramids, 0},
     {"attach-order", no_argument, &should_attach_order, 1},
     {"enable-log", no_argument, &should_log, 2},
+    {"model-face-root", required_argument, 0, 'e'},
     {"native-model", required_argument, 0, 'n'},
     {0, 0, 0, 0}  // terminate the option array
   };
@@ -74,6 +93,7 @@ void getConfig(int argc, char** argv) {
     "  --no-pyramid-fix                Disable quad-connected pyramid tetrahedronization\n"
     "  --attach-order                  Attach the Simmetrix element order as a Numbering\n"
     "  --enable-log                    Enable Simmetrix logging\n"
+    "  --model-face-root               Model face that is root of extrusion from SimModeler\n"
     "  --native-model=/path/to/model   Load the native Parasolid or ACIS model that the GeomSim model uses\n";
 
   int option_index = 0;
@@ -84,6 +104,9 @@ void getConfig(int argc, char** argv) {
       case 0: // pyramid fix flag
       case 1: // attach order flag
       case 2: // enable simmetrix logging
+        break;
+      case 'e':
+        ExtruRootId = atoi(optarg);
         break;
       case 'n':
         gmi_native_path = optarg;
@@ -110,8 +133,8 @@ void getConfig(int argc, char** argv) {
   smb_path = argv[i++];
 
   if (!PCU_Comm_Self()) {
-    printf ("fix_pyramids %d attach_order %d enable_log %d\n",
-            should_fix_pyramids, should_attach_order, should_log);
+    printf ("fix_pyramids %d attach_order %d enable_log %d ExtruRootId %d\n",
+            should_fix_pyramids, should_attach_order, should_log, ExtruRootId);
     printf ("native-model \'%s\' model \'%s\' simmetrix mesh \'%s\' output mesh \'%s\'\n",
       gmi_native_path, gmi_path, sms_path, smb_path);
   }
@@ -123,6 +146,7 @@ int main(int argc, char** argv)
   PCU_Comm_Init();
   lion_set_verbosity(1);
   MS_init();
+  SimAdvMeshing_start(); //for fancy BL/extrusion queries
   SimModel_start();
   Sim_readLicenseFile(NULL);
   SimPartitionedMesh_start(&argc,&argv);
@@ -153,7 +177,183 @@ int main(int argc, char** argv)
   double t1 = PCU_Time();
   if(!PCU_Comm_Self())
     fprintf(stderr, "read and created the simmetrix mesh in %f seconds\n", t1-t0);
+// put the extrude tagging here which 1) loops over the mesh faces classified on the model face that is the root of the extrude
+// create a tag on vertices fathers
+// get the list of mesh rootfaces classified on the source geometric model face
+// for each srcFace in rootfaces
+// get the ids of downward adjacent vertices, store that as an array of size 3
+// get the upward adjacent region srcRgn
+// call Extrusion_3DRegionsAndLayerFaces(srcRgn,...)
+// for each face in the returned list of faces
+// get the downward adjacent vertices of face - they will be in the same order as the srcFace ids
+// set the fathers tag
+// assert that the x,y coordinates of each vertex matches the srcFace vertex coordinates within some relaxed tolerance - sanity check my assumption that face-to-vtx adjaceny is always the same order
+  
+  // create a tag on vertices fathers   TODO
+  pMeshDataId myFather = MD_newMeshDataId( "fathers2D");
+  
+  pPList listV,listVn,regions,faces;
+  pFace face;
+  pRegion region;
+  pVertex vrts[4];
+//  pVertex vrtsN[4]; 
+  int dir, err;
+  int count2D=0;
+//  int ExtruRootId=6176;
+  pGFace gface;
+  pGFace ExtruRootFace=NULL;
+//  pEntity ent;
+  pVertex entV;
+  pMesh meshP= PM_mesh	(sim_mesh, 0 );	
+  
+  //find the root face of the extrusion
+  GFIter gfIter=GM_faceIter(simModel);
+  while ( (gface=GFIter_next(gfIter))) {
+    int id = GEN_tag(gface); 
+    if(id==ExtruRootId) ExtruRootFace=gface;
+  }
+  assert(ExtruRootFace != NULL);
+
+//  GEN_regions(); //FIXME - needed?
+//  GEN_faces(); //FIXME - need face iterator, then call int id = GEN_tag(face); if(id==wantedId) ExtruRootFace=face);
+//  pGEntity ExtruRootFace=??  // is this a model tag number and if yes how do I find it....is it the face ID from SimModeler?
+  // ExtruRootFace  needs to be read in but not sure how yet
+// get the list of mesh rootfaces classified on the source geometric model face
+// assume this iterator will do?
+
+
+  char coordfilename[64];
+  char cnnfilename[64];
+  sprintf(coordfilename, "geom.crd");
+  sprintf(cnnfilename, "geom.cnn");
+  FILE* fcr = fopen(coordfilename, "w");
+  FILE* fcn = fopen(cnnfilename, "w");
+
+  FIter fIter = M_classifiedFaceIter( meshP, ExtruRootFace, 0 ); // 0 says I don't want closure	  
+  while ((face = FIter_next(fIter))) {
+    dir=1;
+// get the ids of downward adjacent vertices, store that as an array of size 3
+//    int F_verticesArray	(face, v,  dir); // guessing zero as I think the element will number such that that the curl point inward
+//OR	
+    listV= F_vertices(face, dir); 
+    void *iter = 0;        // Must initialize to 0
+    int i=0;
+    while ((entV =(pVertex)PList_next(listV, &iter))) { //loop over plist of vertices
+      // Process each item in list
+      vrts[i] = (pVertex)entV;
+      i++;
+    }
+    int nvert=i;
+    PList_delete(listV);
+
+    //FIXME DELETE the plist
+
+//  We missed a step where we check to see if a 2D father has already been created or not and use previousy created ones for any of these
+//  three that already exist.  I suppose this is best accomplished by building a map of the 3D father nodes to a 2D father node number and
+//  increment that 2D father node counter as  we encounter nodes not yet on the list. This can aso take care of the fact that 3D father node
+//  needs to point a 2D fther node as well
+    double coordNewPt[nvert][3];
+    for(i=0; i< nvert ; i++) { //FIXME logic for quads
+       int* markedData;
+       if(!EN_getDataPtr((pEntity)vrts[i],myFather,(void**)&markedData)){  // not sure about marked yet
+         count2D++;
+         int* vtxData = new int[1];
+         vtxData[0] = count2D;
+         EN_attachDataPtr((pEntity)vrts[i],myFather,(void*)vtxData);
+         V_coord(vrts[i],coordNewPt[i]);
+
+//Worked         pGEntity pei=V_whatIn(vrts[i]);	
+//         if ((V_whatInType(vrts[i])==2) ;	
+//pGEntity V_whatIn	(	pVertex 	vertex	)	
+//Returns the model entity on which vertex is classified.
+
+//gType V_whatInType	(	pVertex 	vertex	)	
+         
+                    
+//           apfSIM::modelEntity* pei=meshP->toModel(vrts[i]);
+//         int mtag = meshP->getModelTag(meshP->toModel(vrts[i]));
+        fprintf ( fcr, "%.15E %.15E %d \n", coordNewPt[i][0],coordNewPt[i][1], V_whatInType(vrts[i]));
+       }  
+    }
+
+    double coordFather[nvert][3];
+    int fatherIds[4]; //store the ids of the fathers (vertices) on the root face 
+    for(i=0; i< nvert ; i++) { //FIXME logic for quads
+       int* fatherIdPtr;
+       const int exists = EN_getDataPtr((pEntity)vrts[i],myFather,(void**)&fatherIdPtr);
+       assert(exists);
+       fatherIds[i] = fatherIdPtr[0];
+       V_coord(vrts[i],coordFather[i]);
+       fprintf ( fcn, "%d ", fatherIds[i]);
+    }
+    fprintf ( fcn, "\n");
+//    fprintf ( fcn, "%d,%d,%d,\n", fatherIds[0],fatherIds[1],fatherIds[2]);
+
+   dir=0;  // 1 fails
+ // get the upward adjacent region srcRgn
+    region = F_region(face, dir );  // 0 is the negative normal which I assume for a face on the boundary in is interior. 	
+
+// call Extrusion_3DRegionsAndLayerFaces(srcRgn,...)
+    regions=PList_new();
+    faces=PList_new();
+   err = Extrusion_3DRegionsAndLayerFaces(region, regions, faces, 1); 
+   if(err!=1 && !PCU_Comm_Self())
+    fprintf(stderr, "Extrusion_3DRegionsAndLayerFaces returned %d for err \n", err);
+    
+// for each face in the returned list of faces
+// presumably there is a pPList iterator somewhere for faces??   assume this gives me a faceN 
+   iter=0;
+   //pEntity sonFace;
+   pFace sonFace;
+   int iface=0;
+   dir=0;
+   while( (sonFace = (pFace)PList_next(faces, &iter)) ) { //loop over plist of vertices
+    if(iface !=0) {  // root face is in the stack but we already took care of it above
+// get the downward adjacent vertices of face - they will be in the same order as the srcFace ids
+     listVn= F_vertices(sonFace, dir);
+     void *iter2=0; // Must initialize to 0
+     i=0;
+     int my2Dfath;
+     pVertex  sonVtx;
+     double dist, dx, dy, distMin;
+     double coordSon[3];
+     int iMin;
+     while( (sonVtx = (pVertex)PList_next(listVn, &iter2)) ) { //loop over plist of vertices
+        V_coord(sonVtx,coordSon);
+        distMin=1.0e7;
+        for(i=0; i< nvert; i++){
+          dx=coordSon[0]-coordFather[i][0];
+          dy=coordSon[1]-coordFather[i][1];
+          dist=dx*dx+dy*dy;
+          if(dist < distMin) {
+             iMin=i;
+             distMin=dist;
+          }
+        }
+//FAIL        my2Dfath=fatherIds[i];  FAIL would have worked if Simmetrix Extrusions followed root ordering with dir 0 or 1 but they don't
+        my2Dfath=fatherIds[iMin];
+        int* vtxData = new int[1];
+        vtxData[0] = my2Dfath;
+        EN_attachDataPtr((pEntity)sonVtx,myFather,(void*)vtxData);
+//FAIL        i++;
+     }
+    PList_delete(listVn);
+     //FIXME DELETE the plist
+    }
+    iface++;
+   }
+   //FIXME DELETE the plist
+    PList_delete(faces);
+
+// set the fathers tag  TODO
+// assert that the x,y coordinates of each vertex matches the srcFace vertex coordinates within some relaxed tolerance - sanity check my assump    tion that face-to-vtx adjaceny is always the same order
+  } //end root face iterator
+
+
+
   apf::Mesh* simApfMesh = apf::createMesh(sim_mesh);
+  apf::MeshSIM* cake = reinterpret_cast<apf::MeshSIM*>(simApfMesh);
+  cake->createIntTag("fathers2D", myFather, 1);
   double t2 = PCU_Time();
   if(!PCU_Comm_Self())
     fprintf(stderr, "created the apf_sim mesh in %f seconds\n", t2-t1);
@@ -171,6 +371,10 @@ int main(int argc, char** argv)
   if (should_fix_pyramids) fixPyramids(mesh);
   mesh->verify();
   mesh->writeNative(smb_path);
+
+  auto ft = mesh->findTag("fathers2D");
+  convert_my_tag(mesh, ft);
+//  apf::writeVtkFiles("foo", mesh);
 
   mesh->destroyNative();
   apf::destroyMesh(mesh);
