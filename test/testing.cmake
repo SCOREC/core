fd4ae56ec094775c28bdb249c820e4173a044657
Aiden Woodruff <woodra@rpi.edu>
2024-04-30 12:50:15 -0400

    Do not use mpirun for tests with SCOREC_NO_MPI
    Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

--- test/testing.cmake
+++ test/testing.cmake
@@ -2,10 +2,14 @@ set(MESHES ""
     CACHE STRING
     "Extracted http://scorec.rpi.edu/pumi/pumi_test_meshes.tar.gz")
 function(mpi_test TESTNAME PROCS EXE)
+  if(SCOREC_NO_MPI)
+  add_test(NAME ${TESTNAME} COMMAND ${VALGRIND} ${VALGRIND_ARGS} ${EXE} ${ARGN})
+  else()
   add_test(
     NAME ${TESTNAME}
     COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} ${PROCS} ${VALGRIND} ${VALGRIND_ARGS} ${EXE} ${ARGN}
   )
+  endif()
 endfunction(mpi_test)
 
 mpi_test(shapefun 1 ./shapefun)
