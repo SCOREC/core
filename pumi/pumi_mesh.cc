SCOREC/core

commit 46be470617fc2bf41d9d4a50cde65c88002e92c2

replace PCU_Comm_Split with PCU::Split in tests
- parma/group/parma_group.cc: use PCU::Split.
- phasta/phCook: use PCU::Split.
- pumi/pumi_mesh.cc(split_comm): remove function and use PCU::Split
  instead.
- (merge_comm): remove function and use delete PCUObj instead.
- (pumi_mesh_loadSerial): since num_target_part is equal to number of
  peers, I simplified isMaster and split_comm to just use Self() as the
  color and 0 as key for automatic keying.
- replaced merge_comm with switchPCU and delete.
- (pumi_mesh_load): added pcu_obj alias for pumi::instance()->getPCU().
- use modulus and division for color and key because of num_target_part.
- use switchPCU and delete.
- (pumi_mesh_loadAll): use Split and switchPCU and delete.
- test/repartition.cc: use PCU::Split.
- test/split.cc: use PCU::Split.
- test/zsplit.cc: use PCU::Split.

Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

pumi/pumi_mesh.cc
@@ -177,26 +177,6 @@ apf::Migration* getPlan(apf::Mesh* m, int num_target_part)
   return plan;
 }
 
-void split_comm(int num_out_comm, pcu::PCU &PCUObj)
-{
-  int self = PCUObj.Self();
-  int group_id = self % num_out_comm;
-  int in_group_rank = self / num_out_comm;
-  PCU_Comm groupComm;
-  PCU_Comm_Split(PCUObj.GetMPIComm(), group_id, in_group_rank, &groupComm);
-  PCUObj.SwitchMPIComm(groupComm);
-}
-
-
-void merge_comm(PCU_Comm oldComm, pcu::PCU &PCUObj)
-{
-  PCU_Comm prevComm = PCUObj.GetMPIComm();
-  PCUObj.SwitchMPIComm(oldComm);
-  PCU_Comm_Free_One(&prevComm);
-}
-
-
-
 pGeom pumi_mesh_getGeom(pMesh)
 {
   return pumi::instance()->model;
@@ -211,14 +191,15 @@ pMesh pumi_mesh_loadSerial(pGeom g, const char* filename, const char* mesh_type)
     if (!pumi::instance()->getPCU()->Self()) std::cout<<"[PUMI ERROR] "<<__func__<<" failed: invalid mesh type "<<mesh_type<<"\n";
     return NULL;
   }
-  PCU_Comm prevComm = pumi::instance()->getPCU()->GetMPIComm();
-  int num_target_part = pumi::instance()->getPCU()->Peers();
-  bool isMaster = ((pumi::instance()->getPCU()->Self() % num_target_part) == 0);
+  bool isMaster = pumi::instance()->getPCU()->Self() == 0;
   pMesh m = 0;
-  split_comm(num_target_part, *pumi::instance()->getPCU());
+  pcu::PCU *split_comm = pumi::instance()->getPCU()->Split(
+    pumi::instance()->getPCU()->Self(), 0
+  );
   if (isMaster) 
-    m = apf::loadMdsMesh(g->getGmi(), filename, pumi::instance()->getPCU());
-  merge_comm(prevComm, *pumi::instance()->getPCU());
+    m = apf::loadMdsMesh(g->getGmi(), filename, split_comm);
+  if (m != nullptr) m->switchPCU(pumi::instance()->getPCU());
+  delete split_comm;
   pumi::instance()->mesh = expandMdsMesh(m, g->getGmi(), 1, m->getPCU());
   return pumi::instance()->mesh;
 }
@@ -232,28 +213,31 @@ pMesh pumi_mesh_load(pMesh m)
 
 pMesh pumi_mesh_load(pGeom g, const char* filename, int num_in_part, const char* mesh_type)
 {
+  pcu::PCU *pcu_obj = pumi::instance()->getPCU();
   if (strcmp(mesh_type,"mds"))
   {
-    if (!pumi::instance()->getPCU()->Self()) std::cout<<"[PUMI ERROR] "<<__func__<<" failed: invalid mesh type "<<mesh_type<<"\n";
+    if (!pcu_obj->Self()) std::cout<<"[PUMI ERROR] "<<__func__<<" failed: invalid mesh type "<<mesh_type<<"\n";
     return NULL;
   }
-  if (num_in_part==1 && pumi::instance()->getPCU()->Peers()>1) // do static partitioning
+  if (num_in_part==1 && pcu_obj->Peers()>1) // do static partitioning
   {
-    PCU_Comm prevComm = pumi::instance()->getPCU()->GetMPIComm();
-    int num_target_part = pumi::instance()->getPCU()->Peers()/num_in_part;
-    bool isMaster = ((pumi::instance()->getPCU()->Self() % num_target_part) == 0);
+    int num_target_part = pcu_obj->Peers()/num_in_part;
+    bool isMaster = ((pcu_obj->Self() % num_target_part) == 0);
     pMesh m = 0;
     apf::Migration* plan = 0;   
-    split_comm(num_target_part, *pumi::instance()->getPCU());
+    pcu::PCU *split_comm = pcu_obj->Split(
+      pcu_obj->Self() % num_target_part, pcu_obj->Self() / num_target_part
+    );
     if (isMaster) {
-      m = apf::loadMdsMesh(g->getGmi(), filename, pumi::instance()->getPCU());
+      m = apf::loadMdsMesh(g->getGmi(), filename, split_comm);
       plan = getPlan(m, num_target_part);
     }
-    merge_comm(prevComm, *pumi::instance()->getPCU());
-    pumi::instance()->mesh = apf::repeatMdsMesh(m, g->getGmi(), plan, num_target_part, pumi::instance()->getPCU());
+    if (m != nullptr) m->switchPCU(split_comm);
+    delete split_comm;
+    pumi::instance()->mesh = apf::repeatMdsMesh(m, g->getGmi(), plan, num_target_part, pcu_obj);
   }
   else
-    pumi::instance()->mesh = apf::loadMdsMesh(g->getGmi(), filename, pumi::instance()->getPCU());
+    pumi::instance()->mesh = apf::loadMdsMesh(g->getGmi(), filename, pcu_obj);
   pumi_mesh_print(pumi::instance()->mesh);
   return pumi::instance()->mesh;
 }
@@ -289,12 +273,13 @@ pMesh pumi_mesh_loadAll(pGeom g, const char* filename, bool stitch_link)
   else
   {
     double t0 = pcu::Time();
-    PCU_Comm prevComm = pumi::instance()->getPCU()->GetMPIComm();
-    int num_target_part = pumi::instance()->getPCU()->Peers();
-    split_comm(num_target_part, *pumi::instance()->getPCU());
+    pcu::PCU *split_comm = pumi::instance()->getPCU()->Split(
+      pumi::instance()->getPCU()->Self(), 0
+    );
     // no pmodel & remote links setup
-    pumi::instance()->mesh = apf::loadSerialMdsMesh(g->getGmi(), filename, pumi::instance()->getPCU()); 
-    merge_comm(prevComm, *pumi::instance()->getPCU());
+    pumi::instance()->mesh = apf::loadSerialMdsMesh(g->getGmi(), filename, split_comm); 
+    pumi::instance()->mesh->switchPCU(pumi::instance()->getPCU());
+    delete split_comm;
     if (!pumi::instance()->getPCU()->Self())
       lion_oprint(1,"serial mesh %s loaded in %f seconds\n", filename, pcu::Time() - t0);
   }

parma/group/parma_group.cc
@@ -48,9 +48,9 @@ static void runInGroups(
   int groupRank = inMap(self);
   int group = groupMap(self);
   
-  PCU_Comm groupComm;
-  PCU_Comm_Split(expandedPCU->GetMPIComm(), group, groupRank, &groupComm);
-  auto groupedPCU = std::unique_ptr<pcu::PCU>(new pcu::PCU(groupComm));
+  auto groupedPCU = std::unique_ptr<pcu::PCU>(
+    expandedPCU->Split(group, groupRank)
+  );
   if (m){
     m->switchPCU(groupedPCU.get());
     apf::remapPartition(m, inMap);

phasta/phCook.cc
@@ -51,9 +51,7 @@ pcu::PCU* createGroupComm(int splitFactor, pcu::PCU *PCUObj)
   int self = PCUObj->Self();
   int groupRank = self / splitFactor;
   int group = self % splitFactor;
-  PCU_Comm groupComm;
-  PCU_Comm_Split(PCUObj->GetMPIComm(), group, groupRank, &groupComm);
-  return new pcu::PCU(groupComm);
+  return PCUObj->Split(group, groupRank);
 }
 
 void loadCommon(ph::Input& in, ph::BCs& bcs, gmi_model*& g, pcu::PCU *PCUObj)

test/repartition.cc
@@ -45,11 +45,9 @@ CreateGroupCommResult createGroupComm(pcu::PCU *PCUObj)
        no need to spend time computing a good contiguous number */
     groupRank = 0;
   }
-  PCU_Comm groupComm;
-  PCU_Comm_Split(PCUObj->GetMPIComm(), group, groupRank, &groupComm);
   CreateGroupCommResult result;
   result.isOriginal = isOriginal;
-  result.group_pcu_obj = new pcu::PCU(groupComm);
+  result.group_pcu_obj = PCUObj->Split(group, groupRank);
   return result;
 }
 

test/split.cc
@@ -42,9 +42,7 @@ pcu::PCU* getGroupedPCU(pcu::PCU *PCUObj)
   int self = PCUObj->Self();
   int groupRank = self / partitionFactor;
   int group = self % partitionFactor;
-  PCU_Comm groupComm;
-  PCU_Comm_Split(PCUObj->GetMPIComm(), group, groupRank, &groupComm);
-  return new pcu::PCU(groupComm);
+  return PCUObj->Split(group, groupRank);
 }
 
 void getConfig(int argc, char** argv, pcu::PCU *PCUObj)

test/ugrid.cc
@@ -23,9 +23,7 @@ pcu::PCU* getGroupedPCU(const int partitionFactor, pcu::PCU *PCUObj)
   int self = PCUObj->Self();
   int groupRank = self / partitionFactor;
   int group = self % partitionFactor;
-  PCU_Comm groupComm;
-  PCU_Comm_Split(PCUObj->GetMPIComm(), group, groupRank, &groupComm);
-  return new pcu::PCU(groupComm);
+  return PCUObj->Split(group, groupRank);
 }
 
 int main(int argc, char** argv)

test/zsplit.cc
@@ -44,9 +44,7 @@ pcu::PCU* getGroupedPCU(pcu::PCU *PCUObj)
   int self = PCUObj->Self();
   int groupRank = self / partitionFactor;
   int group = self % partitionFactor;
-  PCU_Comm groupComm;
-  PCU_Comm_Split(PCUObj->GetMPIComm(), group, groupRank, &groupComm);
-  return new pcu::PCU(groupComm);
+  return PCUObj->Split(group, groupRank);
 }
 
 void getConfig(int argc, char** argv, pcu::PCU *PCUObj)

commit 006947468405b1e31a3a22407d10f38fa09c8236

fix pumi_mesh switchPCU
- pumi/pumi_mesh.cc (pumi_mesh_load): switch back to original PCU object
  instead of split (fixing error introduced by 46be470).

Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

pumi/pumi_mesh.cc
@@ -231,7 +231,7 @@ pMesh pumi_mesh_load(pGeom g, const char* filename, int num_in_part, const char*
       m = apf::loadMdsMesh(g->getGmi(), filename, split_comm.get());
       plan = getPlan(m, num_target_part);
     }
-    if (m != nullptr) m->switchPCU(split_comm.get());
+    if (m != nullptr) m->switchPCU(pcu_obj);
     pumi::instance()->mesh = apf::repeatMdsMesh(m, g->getGmi(), plan, num_target_part, pcu_obj);
   }
   else