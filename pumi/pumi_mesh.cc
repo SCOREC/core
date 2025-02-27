SCOREC/core

commit 36b09bd7cf624c7dd953c5cab9b82d5b77a27fee

remove PCU_Comm_Allreduce
- example/mpi-nompi/hello.cc: use PCU::Add.
- example/mpi-nompi/CMakeLists.txt: update regex. escape parentheses.
- pcu/PCU.h: remove PCU_Comm_Allreduce.
- pcu/pcu_c.cc: remove PCU_Comm_Allreduce.
- pcu/pcu_pmpi.c: remove pcu_pmpi_allreduce.
- pcu/pcu_pmpi.h: remove pcu_pmpi_allreduce.
- pcu/pcu_pnompi.c: remove pcu_pmpi_allreduce.
- pcu/pcu_pnompi.h: remove pcu_pmpi_allreduce.
- pumi/pumi_ghost.cc: use PCU::Add.
- pumi/pumi_mesh.cc: use PCU::Add.
- test/pumi.cc: use PCU::ADD.

Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

pumi/pumi_mesh.cc
@@ -347,8 +347,9 @@ void pumi_mesh_setCount(pMesh m, pOwnership o)
       m->end(it);
       pumi::instance()->num_own_ent[dim] = n;
     }
+    pumi::instance()->num_global_ent = pumi::instance()->num_own_ent;
   }
-  PCU_Comm_Allreduce(pumi::instance()->num_own_ent, pumi::instance()->num_global_ent, 4, MPI_INT, MPI_SUM, m->getPCU()->GetMPIComm());
+  m->getPCU()->Add(pumi::instance()->num_global_ent, 4);
 #ifdef DEBUG
   if (!pumi_rank()) std::cout<<"[PUMI INFO] "<<__func__<<" end\n";
 #endif
@@ -436,12 +437,17 @@ void pumi_mesh_print (pMesh m, bool print_ent)
   int* global_local_entity_count = new int[4*m->getPCU()->Peers()]; 
   int* global_own_entity_count = new int[4*m->getPCU()->Peers()]; 
 
-  PCU_Comm_Allreduce(local_entity_count, global_local_entity_count, 4*m->getPCU()->Peers(), 
-                MPI_INT, MPI_SUM, m->getPCU()->GetMPIComm());
+  memcpy(
+    global_local_entity_count, local_entity_count,
+    sizeof(int) * 4 * m->getPCU()->Peers()
+  );
+  m->getPCU()->Add(global_local_entity_count, 4);
 
-  PCU_Comm_Allreduce(own_entity_count, global_own_entity_count, 4*m->getPCU()->Peers(), 
-                MPI_INT, MPI_SUM, m->getPCU()->GetMPIComm());
-
+  memcpy(
+    global_own_entity_count, own_entity_count,
+    sizeof(int) * 4 * m->getPCU()->Peers()
+  );
+  m->getPCU()->Add(global_own_entity_count, 4);
  
   if (!m->getPCU()->Self())
   {

example/mpi-nompi/CMakeLists.txt
@@ -20,15 +20,15 @@ add_test(NAME hello1 COMMAND mpirun -np 1 $<TARGET_FILE:hello>)
 set_property(TEST hello1 PROPERTY PASS_REGULAR_EXPRESSION 
   "MPI size: 1\; PCU size: 1
 Hello from MPI rank: 0\; PCU rank: 0
-MPI(0) sum: 1; PCU sum: 1"
+MPI\\(0\\) sum: 1\; PCU sum: 1"
 )
 add_test(NAME hello2 COMMAND mpirun -np 2 $<TARGET_FILE:hello>)
 set_property(TEST hello2 PROPERTY PASS_REGULAR_EXPRESSION 
   "MPI size: 2\; PCU size: 1
 Hello from MPI rank: 0\; PCU rank: 0
 Hello from MPI rank: 1\; PCU rank: 0
-MPI(0) sum: 3; PCU sum: 1
-MPI(1) sum: 3; PCU sum: 1"
+MPI\\(0\\) sum: 3\; PCU sum: 1
+MPI\\(1\\) sum: 3\; PCU sum: 2"
 )
 add_test(NAME hello4 COMMAND mpirun -np 4 $<TARGET_FILE:hello>)
 set_property(TEST hello4 PROPERTY PASS_REGULAR_EXPRESSION 
@@ -37,9 +37,9 @@ Hello from MPI rank: 0\; PCU rank: 0
 Hello from MPI rank: 1\; PCU rank: 0
 Hello from MPI rank: 2\; PCU rank: 0
 Hello from MPI rank: 3\; PCU rank: 0
-MPI(0) sum: 10\; PCU sum: 1
-MPI(1) sum: 10\; PCU sum: 1
-MPI(2) sum: 10\; PCU sum: 1
-MPI(3) sum: 10\; PCU sum: 1"
+MPI\\(0\\) sum: 10\; PCU sum: 1
+MPI\\(1\\) sum: 10\; PCU sum: 2
+MPI\\(2\\) sum: 10\; PCU sum: 3
+MPI\\(3\\) sum: 10\; PCU sum: 4"
 )
 

example/mpi-nompi/hello.cc
@@ -31,7 +31,7 @@ int main(int argc, char** argv) {
   int val = rank + 1, sum;
   MPI_Allreduce(&val, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
   int pval = rank + 1, psum;
-  PCU_Comm_Allreduce(&pval, &psum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  psum = PCUObj->Add(pval);
   for (int i = 0; i < size; ++i) {
     if (rank == i)
       std::cout << "MPI(" << rank << ") sum: " << sum <<

pcu/PCU.h
@@ -26,7 +26,6 @@ struct pcu_mpi_struct;
 extern "C" {
 int PCU_Comm_Free_One(MPI_Comm* com);
 int PCU_Comm_Split(MPI_Comm oldCom, int color, int key, MPI_Comm* newCom);
-int PCU_Comm_Allreduce(const void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm);
 int PCU_Comm_Allgather(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm);
 }
 namespace pcu {

pcu/pcu_c.cc
@@ -35,12 +35,6 @@ int PCU_Comm_Split(MPI_Comm oldCom, int color, int key, MPI_Comm* newCom) {
   return PCU_SUCCESS;
 }
 
-int PCU_Comm_Allreduce(const void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
-{
-  pcu_pmpi_allreduce(sendbuf,recvbuf,count,datatype,op,comm);
-  return PCU_SUCCESS;
-}
-
 int PCU_Comm_Allgather(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm)
 {
   pcu_pmpi_allgather(sendbuf,sendcount,sendtype,recvbuf,recvcount,recvtype,comm);

pcu/pcu_pmpi.c
@@ -114,11 +114,6 @@ bool pcu_pmpi_receive2(const pcu_mpi_t* self, pcu_message* m, int tag, MPI_Comm 
   return true;
 }
 
-int pcu_pmpi_allreduce( const void* sendbuf, void* recvbuf, int count,
-  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm) {
-  return MPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
-}
-
 int  pcu_pmpi_allgather(const void *sendbuf, int sendcount,
   MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype,
   MPI_Comm comm) {

pcu/pcu_pmpi.h
@@ -26,10 +26,6 @@ bool pcu_pmpi_done(const pcu_mpi_t *, pcu_message *m);
 
 int pcu_pmpi_split(MPI_Comm comm, int color, int key, MPI_Comm* newcomm);
 int pcu_pmpi_free(MPI_Comm* comm);
-int pcu_pmpi_allreduce(
-  const void* sendbuf, void* recvbuf, int count,
-  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm
-);
 int pcu_pmpi_allgather(
   const void *sendbuf, int sendcount, MPI_Datatype sendtype,
   void *recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm

pcu/pcu_pnompi.c
@@ -185,12 +185,6 @@ bool pcu_pmpi_receive2(const pcu_mpi_t *self, pcu_message* m, int tag,
   return true;
 }
 
-int pcu_pmpi_allreduce(const void* a, void* b, int c,
-  MPI_Datatype d, MPI_Op e, MPI_Comm f) {
-  (void) a, (void) b, (void) c, (void) d, (void) e, (void) f;
-  return 0;
-}
-
 int  pcu_pmpi_allgather(const void * a, int b, MPI_Datatype c, void * d, int e,
   MPI_Datatype f, MPI_Comm g) {
   (void) a, (void) b, (void) c, (void) d, (void) e, (void) f, (void) g;

commit 07232e81062b5fe7fdea11950f8c9fcadbdf9568

fix pumi_mesh.cc errors introduced by 36b09bd7c
- pumi/pumi_mesh.cc (pumi_mesh_setCount): set array elements, not array
  pointer.
- (pumi_mesh_print): fix PCU::Add array sizes.
- replace memcpy with for loops.

Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

pumi/pumi_mesh.cc
@@ -329,7 +329,7 @@ void pumi_mesh_setCount(pMesh m, pOwnership o)
       m->end(it);
       pumi::instance()->num_own_ent[dim] = n;
     }
-    pumi::instance()->num_global_ent = pumi::instance()->num_own_ent;
+    pumi::instance()->num_global_ent[dim] = pumi::instance()->num_own_ent[dim];
   }
   m->getPCU()->Add(pumi::instance()->num_global_ent, 4);
 #ifdef DEBUG
@@ -419,17 +419,12 @@ void pumi_mesh_print (pMesh m, bool print_ent)
   int* global_local_entity_count = new int[4*m->getPCU()->Peers()]; 
   int* global_own_entity_count = new int[4*m->getPCU()->Peers()]; 
 
-  memcpy(
-    global_local_entity_count, local_entity_count,
-    sizeof(int) * 4 * m->getPCU()->Peers()
-  );
-  m->getPCU()->Add(global_local_entity_count, 4);
-
-  memcpy(
-    global_own_entity_count, own_entity_count,
-    sizeof(int) * 4 * m->getPCU()->Peers()
-  );
-  m->getPCU()->Add(global_own_entity_count, 4);
+  for (int i = 0; i < 4 * m->getPCU()->Peers(); ++i) {
+    global_local_entity_count[i] = local_entity_count[i];
+    global_own_entity_count[i] = own_entity_count[i];
+  }
+  m->getPCU()->Add(global_local_entity_count, 4*m->getPCU()->Peers());
+  m->getPCU()->Add(global_own_entity_count, 4*m->getPCU()->Peers());
  
   if (!m->getPCU()->Self())
   {