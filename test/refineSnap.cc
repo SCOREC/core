5624760b1003b0a19e6661a655c3e4e078e209aa
Angelyr <scardking@gmail.com>
2025-04-09 16:22:32 -0400

    fixed test

--- test/refineSnap.cc
+++ test/refineSnap.cc
@@ -86,98 +86,81 @@ int main(int argc, char** argv)
   gmi_register_mesh();
   gmi_register_null();
 
-  std::string meshFile(argv[1]);
-  apf::Mesh2* mesh;
-  if (meshFile.substr(meshFile.find_last_of(".") + 1) == ".smb")
+  const char* meshFile = argv[1];
+  // load capstone mesh
+  // create an instance of the Capstone Module activating CREATE/CREATE/CREATE
+  // for the Geometry/Mesh/Attribution databases
+  /* const std::string gdbName("Geometry Database : Create");// Switch Create with SMLIB for CAD */
+  const std::string gdbName("Geometry Database : SMLIB");// Switch Create with SMLIB for CAD
+  const std::string mdbName("Mesh Database : Create");
+  const std::string adbName("Attribution Database : Create");
+
+  CapstoneModule  cs("the_module", gdbName.c_str(), mdbName.c_str(), adbName.c_str());
+
+  GeometryDatabaseInterface     *g = cs.get_geometry();
+  MeshDatabaseInterface         *m = cs.get_mesh();
+  AppContext                    *c = cs.get_context();
+
+  PCU_ALWAYS_ASSERT(g);
+  PCU_ALWAYS_ASSERT(m);
+  PCU_ALWAYS_ASSERT(c);
+
+  v_string filenames;
+  filenames.push_back(meshFile);
+
+  M_GModel gmodel = cs.load_files(filenames);
+
+  int numbreps = 0;
+  MG_CALL(g->get_num_breps(numbreps));
+  std::cout << "number of b reps is " << numbreps << std::endl;
+  if(numbreps == 0)
+      error(HERE, ERR_INVALID_INPUT, "Model is empty");
+
+  M_MModel mmodel;
+  // Pick the volume mesh model from associated mesh models to this geom model
+  std::vector<M_MModel> mmodels;
+  MG_API_CALL(m, get_associated_mesh_models(gmodel, mmodels));
+  for(std::size_t i = 0; i < mmodels.size(); ++i)
   {
-    if (argc < 3) {
-      if(PCUObj.Self()==0) std::cerr << "usage: "<< argv[0] << " <.smb file> <model file>\n";
-      return EXIT_FAILURE;
-    }
-    const char* modelFile = argv[2];
-    mesh = apf::loadMdsMesh(modelFile, meshFile.c_str(), &PCUObj);
+      M_MModel ammodel = mmodels[i];
+      std::size_t numregs = 0;
+      std::size_t numfaces = 0;
+      std::size_t numedges = 0;
+      std::size_t numverts = 0;
+      MG_API_CALL(m, set_current_model(ammodel));
+      MG_API_CALL(m, get_num_topos(TOPO_REGION, numregs));
+      MG_API_CALL(m, get_num_topos(TOPO_FACE, numfaces));
+      MG_API_CALL(m, get_num_topos(TOPO_EDGE, numedges));
+      MG_API_CALL(m, get_num_topos(TOPO_VERTEX, numverts));
+      std::cout << "num regions is " << numregs << std::endl;
+      std::cout << "num faces   is " << numfaces << std::endl;
+      std::cout << "num edges   is " << numedges << std::endl;
+      std::cout << "num verts   is " << numverts << std::endl;
+      std::cout << "-----------" << std::endl;
+      if(numregs > 0)
+      {
+    mmodel = ammodel;
+    break;
+      }
   }
-  else if (meshFile.substr(meshFile.find_last_of(".") + 1) == ".cre")
-  {
-    // load capstone mesh
-    // create an instance of the Capstone Module activating CREATE/CREATE/CREATE
-    // for the Geometry/Mesh/Attribution databases
-    /* const std::string gdbName("Geometry Database : Create");// Switch Create with SMLIB for CAD */
-    const std::string gdbName("Geometry Database : SMLIB");// Switch Create with SMLIB for CAD
-    const std::string mdbName("Mesh Database : Create");
-    const std::string adbName("Attribution Database : Create");
 
-    CapstoneModule  cs("the_module", gdbName.c_str(), mdbName.c_str(), adbName.c_str());
+  /* SET THE ADJACENCIES */
+  MG_API_CALL(m, set_adjacency_state(REGION2FACE|
+                                    REGION2EDGE|
+                                    REGION2VERTEX|
+                                    FACE2EDGE|
+                                    FACE2VERTEX));
+  MG_API_CALL(m, set_reverse_states());
+  MG_API_CALL(m, set_adjacency_scope(TOPO_EDGE, SCOPE_FULL));
+  MG_API_CALL(m, set_adjacency_scope(TOPO_FACE, SCOPE_FULL));
+  MG_API_CALL(m, compute_adjacency());
 
-    GeometryDatabaseInterface     *g = cs.get_geometry();
-    MeshDatabaseInterface         *m = cs.get_mesh();
-    AppContext                    *c = cs.get_context();
+  gmi_cap_start();
+  gmi_register_cap();
 
-    PCU_ALWAYS_ASSERT(g);
-    PCU_ALWAYS_ASSERT(m);
-    PCU_ALWAYS_ASSERT(c);
+  // convert the mesh to apf/mds mesh
 
-    v_string filenames;
-    filenames.push_back(meshFile);
-
-    M_GModel gmodel = cs.load_files(filenames);
-
-    int numbreps = 0;
-    MG_CALL(g->get_num_breps(numbreps));
-    std::cout << "number of b reps is " << numbreps << std::endl;
-    if(numbreps == 0)
-        error(HERE, ERR_INVALID_INPUT, "Model is empty");
-
-    M_MModel mmodel;
-    // Pick the volume mesh model from associated mesh models to this geom model
-    std::vector<M_MModel> mmodels;
-    MG_API_CALL(m, get_associated_mesh_models(gmodel, mmodels));
-    for(std::size_t i = 0; i < mmodels.size(); ++i)
-    {
-        M_MModel ammodel = mmodels[i];
-        std::size_t numregs = 0;
-        std::size_t numfaces = 0;
-        std::size_t numedges = 0;
-        std::size_t numverts = 0;
-        MG_API_CALL(m, set_current_model(ammodel));
-        MG_API_CALL(m, get_num_topos(TOPO_REGION, numregs));
-        MG_API_CALL(m, get_num_topos(TOPO_FACE, numfaces));
-        MG_API_CALL(m, get_num_topos(TOPO_EDGE, numedges));
-        MG_API_CALL(m, get_num_topos(TOPO_VERTEX, numverts));
-        std::cout << "num regions is " << numregs << std::endl;
-        std::cout << "num faces   is " << numfaces << std::endl;
-        std::cout << "num edges   is " << numedges << std::endl;
-        std::cout << "num verts   is " << numverts << std::endl;
-        std::cout << "-----------" << std::endl;
-        if(numregs > 0)
-        {
-      mmodel = ammodel;
-      break;
-        }
-    }
-
-    /* SET THE ADJACENCIES */
-    MG_API_CALL(m, set_adjacency_state(REGION2FACE|
-                                      REGION2EDGE|
-                                      REGION2VERTEX|
-                                      FACE2EDGE|
-                                      FACE2VERTEX));
-    MG_API_CALL(m, set_reverse_states());
-    MG_API_CALL(m, set_adjacency_scope(TOPO_EDGE, SCOPE_FULL));
-    MG_API_CALL(m, set_adjacency_scope(TOPO_FACE, SCOPE_FULL));
-    MG_API_CALL(m, compute_adjacency());
-
-    gmi_cap_start();
-    gmi_register_cap();
-
-    // convert the mesh to apf/mds mesh
-
-    mesh = apf::createMesh(m,g,&PCUObj);
-  }
-  else {
-    std::cerr << "unsupported file type\n";
-    return EXIT_FAILURE;
-  }
+  apf::Mesh2* mesh = apf::createMesh(m,g,&PCUObj);
   mesh->verify();
   apf::writeVtkFiles("before_snap",mesh);
 
