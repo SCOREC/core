5f2e701455e95e23904e232e2f08ec26473dbf0c
Aiden Woodruff <woodra@rpi.edu>
2025-06-18 01:50:02 -0400

    add gmi_cap documentation and update cmake
    - gmi_cap/CMakeLists.txt: update link libraries.
    - gmi_cap/gmi_cap.h: add function documentation.
    - add doxygen group
    
    Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

--- gmi_cap/gmi_cap.h
+++ gmi_cap/gmi_cap.h
@@ -9,7 +9,22 @@
 *******************************************************************************/
 #ifndef GMI_CAP_H
 #define GMI_CAP_H
-
+/**
+ * \file gmi_cap.h
+ * \brief Concrete Capstone GMI implementation.
+ *
+ * This interface can be used in two ways:
+ *
+ * 1. to import an existing Capstone model; and
+ * 2. to load the model and (optionally) meshes from a CRE file by wrapping
+ *    Capstone calls.
+ *
+ * The first option results in a non-owned model (gmi_cap_start does not need
+ * to be called). The second option results in an owned model (gmi_cap_start
+ * MUST be called) for which destruction is handled by gmi_destroy. Both
+ * options require gmi_register_cap to be called. It is recommended to call
+ * gmi_cap_start (if desired) before gmi_register_cap.
+ */
 
 #include "gmi.h"
 #ifdef __cplusplus
@@ -20,9 +35,49 @@
 extern "C" {
 #endif
 
+/**
+ * \defgroup gmi_cap Capstone geometric model interface
+ *
+ * gmi_cap implements gmi. The interface in gmi_cap.h includes additional
+ * functions to initialize the library and simplify loading of Capstone models.
+ *
+ * \{
+ */
+
+/**
+ * \brief Initialize gmi_cap library.
+ *
+ * \note This call is required before calling any other gmi_cap functions.
+ */
 void gmi_cap_start(void);
+/**
+ * \brief Finalize gmi_cap library.
+ *
+ * \note No gmi_cap functions except gmi_cap_import and gmi_cap_export should
+ * bec called after this function.
+ */
 void gmi_cap_stop(void);
+/**
+ * \brief Register gmi_cap in GMI's global list.
+ *
+ * \note This call is required before every gmi_cap function except
+ * gmi_cap_start.
+ *
+ * This call registers gmi_cap with GMI. It also registers the ".cre" extension
+ * with the Create loader so that gmi_load can be used with ".cre" files.
+ */
 void gmi_register_cap(void);
+/**
+ * \brief Load a gmi_model from a CRE file.
+ *
+ * This method is called by gmi_load when the input file has a ".cre"
+ * extension. It loads the file with all associated mesh models. Use
+ * gmi_cap_load_some to only load certain meshes.
+ *
+ * \param creFileName the name of the CRE file
+ * \pre gmi_cap_stop must be called before this method.
+ * \pre creFileName ends in ".cre"
+ */
 struct gmi_model* gmi_cap_load(const char* creFileName);
 
 #ifdef __cplusplus
@@ -41,8 +96,29 @@ struct gmi_model* gmi_cap_load_some(
 gmi_ent* toGmiEntity(CreateMG::M_GTopo topo);
 CreateMG::M_GTopo fromGmiEntity(gmi_ent* g);
 
+/**
+ * \brief Import a non-owned Capstone model.
+ *
+ * Loads the current geometry model on the interface.
+ *
+ * \param gi Geometry interface to load.
+ * \pre gi has a current model.
+ */
 struct gmi_model* gmi_import_cap(CreateMG::GDBI* gi);
-struct gmi_model* gmi_import_cap(CreateMG::GDBI* gi, CreateMG::M_GModel);
+/**
+ * \brief Import a non-owned, specific Capstone model.
+ *
+ * \param gi Geometry interface to load from.
+ * \param gmodel The existing geometry model to load.
+ */
+struct gmi_model* gmi_import_cap(
+  CreateMG::GDBI* gi, CreateMG::M_GModel gmodel
+);
+/**
+ * \brief Expose the underlying Capstone geometry interface from a gmi_model.
+ *
+ * \param m Loaded or imported gmi_model.
+ */
 CreateMG::GDBI* gmi_export_cap(struct gmi_model* m);
 
 #endif // __cplusplus
@@ -51,6 +127,8 @@ CreateMG::GDBI* gmi_export_cap(struct gmi_model* m);
 #ifndef __cplusplus
 } // extern "C"
 #endif
+
+/** \} */
 
 #endif // GMI_CAP_H
 
