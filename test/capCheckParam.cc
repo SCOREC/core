32c246e252b77d093eb173fdc1981e27607eef4a
Aiden Woodruff <woodra@rpi.edu>
2025-07-01 12:40:03 -0400

    test: update capCheckParam
    - test/capCheckParam.cc: use apf APIs to call the same underlying
      Capstone ones.
    - reformat table.
    - check edges as well as faces.
    - enable lion error messages
    - test/CMakeLists.txt: remove obsolete capVol include dir.
    - remove obsolete capCheckParam link libraries.
    
    Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

--- test/capCheckParam.cc
+++ test/capCheckParam.cc
@@ -1,16 +1,15 @@
 #include <iostream>
+#include <iomanip>
 
 #include <PCU.h>
 #include <apf.h>
 #include <apfCAP.h>
 #include <apfMesh2.h>
 #include <gmi_cap.h>
+#include <lionPrint.h>
 #include <pcu_util.h>
 
-#include <CreateMG_Framework_Geometry.h>
-#include <CreateMG_Framework_Mesh.h>
-
-void checkParametrization(CreateMG::MDBI* mdb, CreateMG::GDBI* gdb);
+void checkParametrization(apf::Mesh* mesh);
 
 int main(int argc, char** argv)
 {
@@ -26,6 +25,7 @@ int main(int argc, char** argv)
 
   const char* creFileName = argv[1];
 
+  lion_set_verbosity(1);
   gmi_cap_start();
   gmi_register_cap();
 
@@ -33,53 +33,58 @@ int main(int argc, char** argv)
   apf::Mesh2* mesh = apf::createCapMesh(model, &PCUobj);
 
   // check parametrization using capstone apis
-  checkParametrization(apf::exportCapNative(mesh), gmi_export_cap(model));
+  checkParametrization(mesh);
   apf::destroyMesh(mesh);
   gmi_cap_stop();
   } // pcu object scope
   pcu::Finalize();
 }
 
-void checkParametrization(CreateMG::MDBI* mdb, CreateMG::GDBI* gdb) {
-  using namespace CreateMG;
-  using namespace CreateMG::Mesh;
-  MeshSmartIterator miter(mdb);
-  mdb->get_topo_iterator(TOPO_VERTEX, miter);
-  int count = 0;
+void checkParametrization(apf::Mesh* mesh) {
+  int count[2] = {0, 0};
   double sum = 0.0;
-  for(mdb->iterator_begin(miter); !mdb->iterator_end(miter); mdb->iterator_next(miter)) {
-    M_MTopo vert = mdb->iterator_value(miter);
-    M_GTopo geom;
-    GeometryTopoType gtype;
-    mdb->get_geom_entity(vert, gtype, geom);
-    if (!gdb->is_face(geom)) continue;
-    double range_u[2];
-    double range_v[2];
-    gdb->get_parametrization_range(geom, 0, range_u[0], range_u[1]);
-    gdb->get_parametrization_range(geom, 1, range_v[0], range_v[1]);
-    GeometryTopoType gtype1;
-     double u,v;
-    mdb->get_vertex_uv_parameters(vert, u, v, gtype1);
-    PCU_ALWAYS_ASSERT(gtype1 == gtype);
-
+  std::cout <<
+    "ct, (        u,         v), (     umin,      umax), "
+    "(     vmin,      vmax), diff\n";
+  apf::MeshIterator* it = mesh->begin(0);
+  for (apf::MeshEntity* vtx; (vtx = mesh->iterate(it));) {
+    apf::ModelEntity* me = mesh->toModel(vtx);
+    int dim = mesh->getModelType(me);
+    if (dim != 1 && dim != 2) continue;
+    double range_u[2], range_v[2];
+    mesh->getPeriodicRange(me, 0, range_u);
+    mesh->getPeriodicRange(me, 1, range_v);
+    apf::Vector3 xi;
+    mesh->getParam(vtx, xi);
     // coordinate from mesh
     apf::Vector3 coord;
-    mdb->get_vertex_coord(vert, &(coord[0]));
-
+    mesh->getPoint(vtx, 0, coord);
     // coordinate from surface
-    vec3d x;
-    gdb->get_point(geom, vec3d(u, v, 0.0), x);
-    apf::Vector3 pcoord(x[0], x[1], x[2]);
+    apf::Vector3 pcoord;
+    mesh->snapToModel(me, xi, pcoord);
+    apf::Vector3 diff = coord - pcoord;
 
-    if (count < 50) {
-      std::cout << count << ", "
-        << u << ", " << v << ", "
-        << range_u[0] << ", " << range_u[1]
-        << ", " << range_v[0] << ", " << range_v[1]
-        << ", " << (coord - pcoord).getLength() << std::endl;
+    constexpr int dim_printmax = 25;
+    if (count[dim - 1] < dim_printmax) {
+      std::cout << std::setw(2) << count[dim - 1] << ", "
+        << std::setprecision(3) << std::scientific
+        << '(' << std::setw(8) << xi.x() << ", "
+        << std::setw(8) << xi.y() << "), "
+        << '(' << std::setw(4) << range_u[0] << ", "
+        << std::setw(4) << range_u[1] << "), ";
+      if (dim == 2)
+        std::cout << '(' << std::setw(4) << range_v[0] << ", "
+          << std::setw(4) << range_v[1] << "), ";
+      else
+        std::cout << "----------------------, ";
+      std::cout << std::setw(4) << std::defaultfloat << diff.getLength()
+        << std::endl;
+    } else if (count[dim - 1] == dim_printmax) {
+      std::cout << "Skipping printing remaining "
+        << (dim == 1 ? "edges" : "faces") << std::endl;
     }
-    sum += (coord-pcoord) * (coord-pcoord);
-    count++;
+    sum += diff * diff;
+    count[dim - 1]++;
   }
   std::cout << "norm of the difference vector is " << std::sqrt(sum)
     << std::endl;
