19aee8ea24f56f4f3b380afa26b5e7bea615415d
Angelyr <scardking@gmail.com>
2025-07-02 19:58:13 -0400

    single coarsen

--- ma/ma.cc
+++ ma/ma.cc
@@ -27,7 +27,7 @@ void adapt(Input* in)
   validateInput(in);
   Adapt* a = new Adapt(in);
   preBalance(a);
-  coarsenMultiple(a);
+  coarsen(a);
   for (int i = 0; i < in->maximumIterations; ++i)
   {
     print(a->mesh->getPCU(), "iteration %d", i);
