41e827e445f2974e0838af6f628cd1849d926e7c
Aditya Joshi <joshia5@rpi.edu>
2025-08-28 10:50:37 -0400

    apply coarsening first, revert changes

--- ma/ma.cc
+++ ma/ma.cc
@@ -27,15 +27,14 @@ void adapt(Input* in)
   validateInput(in);
   Adapt* a = new Adapt(in);
   preBalance(a);
-  coarsen(a);
   for (int i = 0; i < in->maximumIterations; ++i)
   {
     print(a->mesh->getPCU(), "iteration %d", i);
+    coarsen(a);
+    coarsenLayer(a);
     midBalance(a);
     refine(a);
     snap(a);
-    coarsen(a);
-    coarsenLayer(a);
   }
   allowSplitCollapseOutsideLayer(a);
   fixElementShapes(a);
