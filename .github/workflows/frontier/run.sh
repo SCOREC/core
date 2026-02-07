37580aa9e47230915f8881bc38cf8d3dcc9d28d6
Angelyr <scardking@gmail.com>
2026-02-07 18:04:54 -0500

    updated allocation

--- .github/workflows/frontier/run.sh
+++ .github/workflows/frontier/run.sh
@@ -2,10 +2,10 @@
 
 name=core
 
-cd /lustre/orion/phy122/scratch/castia5/globus-compute/$name-test
+cd /lustre/orion/csc679/scratch/castia5/globus-compute/$name-test
 
 module load PrgEnv-amd
 
 cd build-$name
-salloc --account=PHY122 --time=00:20:00 -q debug --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus-per-task=1 --gpus=1 ctest
+salloc --account=csc679 --time=00:20:00 -q debug --nodes=1 --ntasks=1 --cpus-per-task=1 --gpus-per-task=1 --gpus=1 ctest
 cat $PWD/Testing/Temporary/LastTest.log
