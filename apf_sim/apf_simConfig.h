6f3e6b63f84de338ef429312571c34131f4497fe
Cameron Smith <smithc11@rpi.edu>
2026-04-20 09:41:40 -0400

    restore old header, mark deprecated

---
+++ apf_sim/apf_simConfig.h
@@ -0,0 +1,8 @@
+#include <simModSuiteConfig.h>
+
+#if defined(__GNUC__) || defined(__clang__) || defined(__INTEL_COMPILER) || defined(__INTEL_LLVM_COMPILER)
+  #warning "apf_simConfig.h is deprecated. Please use simModSuiteConfig.h instead."
+#elif defined(_MSC_VER)
+  #pragma message("warning: apf_simConfig.h is deprecated. Please use simModSuiteConfig.h instead.")
+#endif
+
