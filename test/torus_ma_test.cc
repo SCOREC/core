5886170a7f173a46b1e7bf91896e88db8016c09e
Angelyr <scardking@gmail.com>
2026-06-13 22:40:57 -0400

    fixed torus ma test

--- test/torus_ma_test.cc
+++ test/torus_ma_test.cc
@@ -56,11 +56,6 @@ int main(int argc, char** argv)
   apf::writeVtkFiles("torus_before",m);
   CylindricalShock sf(m);
   ma::Input* in = ma::makeAdvanced(ma::configure(m, &sf));
-  // Note that the optimal number of iterations will be set
-  // inside the call to ma::configure above. However for this
-  // test we override this value to 3 to reduce the run time of
-  // test.
-  in->maximumIterations = 3;
   in->shouldRunPreZoltan = true;
   in->shouldRunMidParma = true;
   in->shouldRunPostParma = true;
